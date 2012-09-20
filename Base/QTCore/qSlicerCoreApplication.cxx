Slicer/SlicerGitSVNArchive

commit fbc9a2aa7cef6ba3506034d58e98f1b914722c72

BUG: Extensions directory is not created on Macs.
See http://na-mic.org/Mantis/view.php?id=2358

Partial fix to #2358

git-svn-id: http://svn.slicer.org/Slicer4/trunk@20996 3bd1e089-480b-0410-8dfb-8563597acbee

Base/QTCore/qSlicerCoreApplication.cxx
@@ -250,6 +250,12 @@ void qSlicerCoreApplicationPrivate::init()
 
 #ifdef Slicer_BUILD_EXTENSIONMANAGER_SUPPORT
 
+# ifdef Q_OS_MAC
+  this->createDirectory(this->defaultExtensionsInstallPathForMacOSX(), "extensions"); // Make sure the path exists
+  q->addLibraryPath(this->defaultExtensionsInstallPathForMacOSX());
+  q->setExtensionsInstallPath(this->defaultExtensionsInstallPathForMacOSX());
+# endif
+
   this->createDirectory(q->extensionsInstallPath(), "extensions"); // Make sure the path exists
 
   qSlicerExtensionsManagerModel * model = new qSlicerExtensionsManagerModel(q);
@@ -264,11 +270,6 @@ void qSlicerCoreApplicationPrivate::init()
     {
     qDebug() << "Successfully uninstalled extension" << extensionName;
     }
-
-# ifdef Q_OS_MAC
-  q->addLibraryPath(this->defaultExtensionsInstallPathForMacOSX());
-  q->setExtensionsInstallPath(this->defaultExtensionsInstallPathForMacOSX());
-# endif
 
 #endif
 }
@@ -544,9 +545,7 @@ QString qSlicerCoreApplicationPrivate::defaultExtensionsInstallPathForMacOSX()co
     {
     QDir slicerHomeDir(q->slicerHome());
     slicerHomeDir.cdUp();
-    slicerHomeDir.cd("Contents");
-    slicerHomeDir.cd(Slicer_BUNDLE_EXTENSIONS_DIRNAME);
-    return slicerHomeDir.absolutePath();
+    return slicerHomeDir.absolutePath() + "Contents/" Slicer_BUNDLE_EXTENSIONS_DIRNAME;
     }
   else
     {

commit 37cba000cc25bd482742e3d27ffa3d9c5a60208d

BUG: Fix MacOSX startup crash related to ExtensionsModelManager
Calling "q->setExtensionsInstallPath()" expects the model to be
instantiated.

Fixes issue introduced by commit r20996

See http://na-mic.org/Mantis/view.php?id=2358

Partial fix to #2358

git-svn-id: http://svn.slicer.org/Slicer4/trunk@21018 3bd1e089-480b-0410-8dfb-8563597acbee

Base/QTCore/qSlicerCoreApplication.cxx
@@ -250,6 +250,11 @@ void qSlicerCoreApplicationPrivate::init()
 
 #ifdef Slicer_BUILD_EXTENSIONMANAGER_SUPPORT
 
+  qSlicerExtensionsManagerModel * model = new qSlicerExtensionsManagerModel(q);
+  model->setLauncherSettingsFilePath(q->launcherSettingsFilePath());
+  model->setSlicerRequirements(q->repositoryRevision(), q->os(), q->arch());
+  q->setExtensionManagerModel(model);
+
 # ifdef Q_OS_MAC
   this->createDirectory(this->defaultExtensionsInstallPathForMacOSX(), "extensions"); // Make sure the path exists
   q->addLibraryPath(this->defaultExtensionsInstallPathForMacOSX());
@@ -258,10 +263,6 @@ void qSlicerCoreApplicationPrivate::init()
 
   this->createDirectory(q->extensionsInstallPath(), "extensions"); // Make sure the path exists
 
-  qSlicerExtensionsManagerModel * model = new qSlicerExtensionsManagerModel(q);
-  model->setLauncherSettingsFilePath(q->launcherSettingsFilePath());
-  model->setSlicerRequirements(q->repositoryRevision(), q->os(), q->arch());
-  q->setExtensionManagerModel(model);
   model->updateModel();
 
   QStringList uninstalledExtensions;
@@ -1071,6 +1072,7 @@ void qSlicerCoreApplication::setExtensionsInstallPath(const QString& path)
   Q_ASSERT(appSettings);
   appSettings->setValue("Extensions/InstallPath", path);
 #ifdef Slicer_BUILD_EXTENSIONMANAGER_SUPPORT
+  Q_ASSERT(this->extensionManagerModel());
   this->extensionManagerModel()->updateModel();
 #endif
 }