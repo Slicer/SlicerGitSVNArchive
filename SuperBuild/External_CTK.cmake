Slicer/SlicerGitSVNArchive

commit f8b64dc6df4ee55504520a132dce54127aac77eb

ENH: Update CTK project to include CMake configuration speedup
On Ubuntu 13.10 using CMake 2.8.11.2, an improvement of ~10%
can be observed.

Without CTK tweaks:

real	2m32.996s
user	2m26.225s
sys	0m6.820s

With CTK tweak:

real	2m19.957s
user	2m15.908s
sys	0m3.768s


$ git shortlog 233d87..909073a --no-merges
Jean-Christophe Fillion-Robin (18):
      Remove unused FindCTK.cmake module
      Ensure CTK_ENABLE_Python_Wrapping remain set when CTK_BUILD_ALL is ON
      Print message if CTK_{APP, LIB, PLUGIN} options set to ON because of BUILD_ALL options
      Fix dependency of Scripting/Python/{Core, Widgets} options
      Remove code referencing unexisting variable. See #449
      Remove 'CTK_WRAP_PYTHONQT_FULL' obsolete option. See #449
      Remove setting of INCLUDE_DIRS variable specific to PythonQtGenerator
      Move all PythonQt wrapping code into one script. See #449
      Optimize ctkMacroWrapPythonQt creating required source list in a single loop.
      Configure module init file at configure times. See #449
      Simplify how path of generated file are specified. See #449
      Simplify moc'ing of generated PythonQt wrapper. See #449
      Remove Light wrapping CMake script and directly invoke python executable
      Do not log PythonQt wrapping output. See #449
      Optimize QT4_GENERATE_MOCS macro getting moc flags only when needed
      Simplify QT4_GENERATE_MOCS. See #449
      Re-create python compile script only if needed and fix dependency. See #449
      Fix python script compilation removing inputs from custom target deps. See #449

Julien Finet (2):
      Fix ctkPushButton text alignment with no icon
      Emit ctkSearchBox::textEdited() signal when clear icon is clicked

git-svn-id: http://svn.slicer.org/Slicer4/trunk@23100 3bd1e089-480b-0410-8dfb-8563597acbee

SuperBuild/External_CTK.cmake
@@ -58,7 +58,7 @@ if(NOT DEFINED CTK_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
   ExternalProject_Add(${proj}
     ${${proj}_EP_ARGS}
     GIT_REPOSITORY "${git_protocol}://github.com/commontk/CTK.git"
-    GIT_TAG "233d87b70db2ce7f00bc71be93d009d8b2ee063d"
+    GIT_TAG "909073a04221bb10d17966928840d7ba0c7f1a88"
     SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
     BINARY_DIR ${proj}-build
     CMAKE_CACHE_ARGS

commit 23befd1ddbd4da181042809de233955416926fed

COMP: Fix CTK compilation error expecting moc_ file to have cpp/cxx extension
This commit fixes issue introduced by r23100

$ git shortlog 909073..2f1ff44 --no-merges
Jean-Christophe Fillion-Robin (1):
      Fix compilation error introduced by 130deabb4. See #449

git-svn-id: http://svn.slicer.org/Slicer4/trunk@23101 3bd1e089-480b-0410-8dfb-8563597acbee

SuperBuild/External_CTK.cmake
@@ -58,7 +58,7 @@ if(NOT DEFINED CTK_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
   ExternalProject_Add(${proj}
     ${${proj}_EP_ARGS}
     GIT_REPOSITORY "${git_protocol}://github.com/commontk/CTK.git"
-    GIT_TAG "909073a04221bb10d17966928840d7ba0c7f1a88"
+    GIT_TAG "2f1ff4481104b6e9047a07ac418b25a3b1a233da"
     SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
     BINARY_DIR ${proj}-build
     CMAKE_CACHE_ARGS