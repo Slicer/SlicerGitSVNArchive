Slicer/SlicerGitSVNArchive

commit f301707f4f44c08e2aa6fed6aa86e1c3d3fd0c9e

ENH: Editor: Effects are checkable and updated based on the parameter node
This commit ensures the button effect are checked or unchecked based
on the 'effect' property stored in the parameter node.

It also extends the EditUtil API adding function allowing to more easily
set and get the 'EraseLabel' state.

git-svn-id: http://svn.slicer.org/Slicer4/trunk@23636 3bd1e089-480b-0410-8dfb-8563597acbee

Modules/Scripted/EditorLib/EditBox.py
@@ -3,6 +3,7 @@ from __main__ import qt
 from EditOptions import *
 import EditUtil
 import EditorLib
+from slicer.util import VTKObservationMixin
 
 #########################################################
 #
@@ -28,8 +29,10 @@ class EditBox(VTKObservationMixin):
     self.effects = []
     self.effectButtons = {}
     self.effectCursors = {}
-    self.effectMapper = qt.QSignalMapper()
-    self.effectMapper.connect('mapped(const QString&)', self.selectEffect)
+    self.effectActionGroup = qt.QActionGroup(parent)
+    self.effectActionGroup.connect('triggered(QAction*)', self._onEffectActionTriggered)
+    self.effectActionGroup.exclusive = True
+    self.currentEffect = None
     self.editUtil = EditUtil.EditUtil()
     self.undoRedo = EditUtil.UndoRedo()
     self.undoRedo.stateChangedCallback = self.updateUndoRedoButtons
@@ -87,6 +90,9 @@ class EditBox(VTKObservationMixin):
     interactionNode = slicer.app.applicationLogic().GetInteractionNode()
     self.addObserver(interactionNode, interactionNode.InteractionModeChangedEvent, self.onInteractionModeChanged)
 
+    # Listen for changed on the Parameter node
+    self.addObserver(self.editUtil.getParameterNode(), vtk.vtkCommand.ModifiedEvent, self._onParameterNodeModified)
+
   def __del__(self):
     self.removeObservers()
 
@@ -94,6 +100,12 @@ class EditBox(VTKObservationMixin):
     if caller.IsA('vtkMRMLInteractionNode'):
       if caller.GetCurrentInteractionMode() != caller.ViewTransform:
         self.defaultEffect()
+
+  def _onParameterNodeModified(self, caller, event=-1):
+    self._onEffectChanged(caller.GetParameter("effect"))
+    self.editUtil.setEraseEffectEnabled(self.editUtil.isEraseEffectEnabled())
+    self.actions["EraseLabel"].checked = self.editUtil.isEraseEffectEnabled()
+    self.actions[self.editUtil.getCurrentEffect()].checked = True
 
   #
   # Public lists of the available effects provided by the editor
@@ -201,12 +213,28 @@ class EditBox(VTKObservationMixin):
           b.setToolTip(EditBox.displayNames[effect])
         hbox.addWidget(b)
 
-        # Setup the mapping between button and its associated effect name
-        self.effectMapper.setMapping(self.buttons[effect], effect)
-        # Connect button with signal mapper
-        self.buttons[effect].connect('clicked()', self.effectMapper, 'map()')
+        if effect not in ('EraseLabel', 'PreviousCheckPoint', 'NextCheckPoint'):
+          # Mapping between action and its associated effect, is done
+          # in function'_onEffectActionTriggered' by retrieving the 'effectName'
+          # property.
+          a.checkable = True
+          a.setProperty('effectName', effect)
+          self.effectActionGroup.addAction(a)
+        elif effect == 'EraseLabel':
+          a.checkable = True
+          a.connect('triggered(bool)', self._onEraseLabelActionTriggered)
+        elif effect == 'PreviousCheckPoint':
+          a.connect('triggered(bool)', self.undoRedo.undo)
+        elif effect == 'NextCheckPoint':
+          a.connect('triggered(bool)', self.undoRedo.redo)
 
     hbox.addStretch(1)
+
+  def _onEffectActionTriggered(self, action):
+    self.selectEffect(action.property('effectName'))
+
+  def _onEraseLabelActionTriggered(self, enabled):
+    self.editUtil.setEraseEffectEnabled(enabled)
 
   # create the edit box
   def create(self):
@@ -258,6 +286,7 @@ class EditBox(VTKObservationMixin):
     vbox.addStretch(1)
 
     self.updateUndoRedoButtons()
+    self._onParameterNodeModified(self.editUtil.getParameterNode())
 
   def setActiveToolLabel(self,name):
     if EditBox.displayNames.has_key(name):
@@ -270,23 +299,16 @@ class EditBox(VTKObservationMixin):
   def defaultEffect(self):
     self.selectEffect("DefaultTool")
 
+  def selectEffect(self, effectName):
+      self.editUtil.setCurrentEffect(effectName)
+
   #
   # manage the editor effects
   #
-  def selectEffect(self, effectName):
+  def _onEffectChanged(self, effectName):
 
-    if effectName != "EraseLabel":
-        self.editUtil.restoreLabel()
-
-    if effectName ==  "EraseLabel":
-        self.editUtil.toggleLabel()
-        return
-    elif effectName ==  "PreviousCheckPoint":
-        self.undoRedo.undo()
-        return
-    elif effectName == "NextCheckPoint":
-        self.undoRedo.redo()
-        return
+    if self.currentEffect == effectName:
+      return
 
     #
     # If there is no background volume or label map, do nothing
@@ -295,6 +317,13 @@ class EditBox(VTKObservationMixin):
       return
     if not self.editUtil.getLabelVolume():
       return
+
+    self.currentEffect = effectName
+
+    self.editUtil.restoreLabel()
+
+    # Update action
+    self.actions[effectName].checked = True
 
     #
     # an effect was selected, so build an options GUI

Modules/Scripted/EditorLib/EditUtil.py
@@ -1,6 +1,8 @@
 import vtk
 import slicer
 
+from slicer.util import NodeModify
+
 #########################################################
 #
 #
@@ -43,11 +45,25 @@ class EditUtil(object):
     node.SetSingletonTag( "Editor" )
     node.SetModuleName( "Editor" )
     node.SetParameter( "label", "1" )
+    node.SetParameter( "effect", "DefaultTool" )
     slicer.mrmlScene.AddNode(node)
     # Since we are a singleton, the scene won't add our node into the scene,
     # but will instead insert a copy, so we find that and return it
     node = self._findParameterNodeInScene()
     return node
+
+  def getCurrentEffect(self):
+    """return effect associated with the editor parameter node.
+    """
+    return self.getParameterNode().GetParameter('effect')
+
+  def setCurrentEffect(self, name):
+    """set current effect on the editor parameter node.
+    """
+    if name != 'EraseLabel':
+      self.getParameterNode().SetParameter('effect', name)
+    else:
+      self.toggleLabel()
 
   def getCompositeNode(self,layoutName='Red'):
     """ use the Red slice composite node to define the active volumes """
@@ -128,11 +144,22 @@ class EditUtil(object):
 
   def toggleLabel(self):
     """toggle the current label map in the editor parameter node"""
-    if self.getLabel() == 0:
+    if self.isEraseEffectEnabled():
       self.restoreLabel()
     else:
       self.backupLabel()
       self.setLabel(0)
+
+  def isEraseEffectEnabled(self):
+      return self.getLabel() == 0;
+
+  def setEraseEffectEnabled(self, enabled):
+    with NodeModify(self.getParameterNode()):
+      if enabled and not self.isEraseEffectEnabled():
+        self.backupLabel()
+        self.setLabel(0)
+      elif not enabled and self.isEraseEffectEnabled():
+        self.restoreLabel()
 
   def getLabelColor(self):
     """returns rgba tuple for the current paint color """

commit 1c10718f30b8f15a5f6c0602e96add70c1898df6

BUG: EditBox: Ensure state variable are initialized before calling "create()"
This commit fixes regression introduced by r23636.

The following tests are now passing:
 * py_StandaloneEditorWidgetTest
 * py_ThresholdThreadingTest
 * py_NeurosurgicalPlanningTutorialMarkupsSelfTest

The associated error was:

Traceback (most recent call last):
  File "/path/to/Slicer-build/lib/Slicer-4.3/qt-scripted-modules/Editor.py", line 244, in setup
    self.createEditBox()
  File "/path/to/Slicer-build/lib/Slicer-4.3/qt-scripted-modules/Editor.py", line 280, in createEditBox
    self.toolsBox = EditorLib.EditBox(self.editBoxFrame, optionsFrame=self.effectOptionsFrame)
  File "/path/to/Slicer-build/lib/Slicer-4.3/qt-scripted-modules/EditorLib/EditBox.py", line 74, in __init__
    self.create()
  File "/path/to/Slicer-build/lib/Slicer-4.3/qt-scripted-modules/EditorLib/EditBox.py", line 289, in create
    self._onParameterNodeModified(self.editUtil.getParameterNode())
  File "/path/to/Slicer-build/lib/Slicer-4.3/qt-scripted-modules/EditorLib/EditBox.py", line 105, in _onParameterNodeModified
    self._onEffectChanged(caller.GetParameter("effect"))
  File "/path/to/Slicer-build/lib/Slicer-4.3/qt-scripted-modules/EditorLib/EditBox.py", line 336, in _onEffectChanged
    if self.currentOption:
AttributeError: 'EditBox' object has no attribute 'currentOption'

git-svn-id: http://svn.slicer.org/Slicer4/trunk@23639 3bd1e089-480b-0410-8dfb-8563597acbee

Modules/Scripted/EditorLib/EditBox.py
@@ -64,15 +64,6 @@ class EditBox(VTKObservationMixin):
     self.editorBuiltins["FastMarchingEffect"] = EditorLib.FastMarchingEffect
     self.editorBuiltins["WandEffect"] = EditorLib.WandEffect
 
-    if not parent:
-      self.parent = qt.QFrame()
-      self.parent.setLayout( qt.QVBoxLayout() )
-      self.create()
-      self.parent.show()
-    else:
-      self.parent = parent
-      self.create()
-
     # frame that holds widgets specific for each effect
     if not optionsFrame:
       self.optionsFrame = qt.QFrame(self.parent)
@@ -92,6 +83,15 @@ class EditBox(VTKObservationMixin):
 
     # Listen for changed on the Parameter node
     self.addObserver(self.editUtil.getParameterNode(), vtk.vtkCommand.ModifiedEvent, self._onParameterNodeModified)
+
+    if not parent:
+      self.parent = qt.QFrame()
+      self.parent.setLayout( qt.QVBoxLayout() )
+      self.create()
+      self.parent.show()
+    else:
+      self.parent = parent
+      self.create()
 
   def __del__(self):
     self.removeObservers()