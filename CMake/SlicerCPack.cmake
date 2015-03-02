Slicer/SlicerGitSVNArchive

commit deb57c4f5579761160cd816598c518366b1df2f6

ENH: Reduce package size by not installing export libraries. Fixes #2397
This commit explicitly installs the needed component and skips the
development component if not explicitly requested.

Since on DLL platform, export libraries (.lib) are considered as archive [1]
and are associated with the development component, they won't be installed
in the package.

[1] http://www.cmake.org/cmake/help/v2.8.12/cmake.html#command:install

git-svn-id: http://svn.slicer.org/Slicer4/trunk@23158 3bd1e089-480b-0410-8dfb-8563597acbee

CMake/SlicerCPack.cmake
@@ -125,7 +125,12 @@ foreach(additional_project ${additional_projects})
 endforeach()
 
 # Install Slicer
-set(CPACK_INSTALL_CMAKE_PROJECTS "${CPACK_INSTALL_CMAKE_PROJECTS};${Slicer_BINARY_DIR};Slicer;ALL;/")
+set(CPACK_INSTALL_CMAKE_PROJECTS "${CPACK_INSTALL_CMAKE_PROJECTS};${Slicer_BINARY_DIR};Slicer;Runtime;/")
+set(CPACK_INSTALL_CMAKE_PROJECTS "${CPACK_INSTALL_CMAKE_PROJECTS};${Slicer_BINARY_DIR};Slicer;RuntimeLibraries;/")
+set(CPACK_INSTALL_CMAKE_PROJECTS "${CPACK_INSTALL_CMAKE_PROJECTS};${Slicer_BINARY_DIR};Slicer;RuntimePlugins;/")
+if(NOT Slicer_INSTALL_NO_DEVELOPMENT)
+  set(CPACK_INSTALL_CMAKE_PROJECTS "${CPACK_INSTALL_CMAKE_PROJECTS};${Slicer_BINARY_DIR};Slicer;Development;/")
+endif()
 
 # -------------------------------------------------------------------------
 # Package properties

commit 0d6d48727b4759f299058b60b089435c1d124ac9

COMP: Fix redistributable system libraries in packaging. Fixes #3972
This commit fixes a regression introduced by r23158 (Explicitly installs
the needed component and skips the development component if not explicitly
requested.). When Slicer switched to using component for packaging, the VS
redistributable libraries were not included anymore in the package. This
is explained by the fact the CMake module "InstallRequiredSystemLibraries"
does not support components.

Waiting the issue reported on CMake bug tracker [1] is resolved and the
minimum required CMake version is updated, this commit duplicates the logic
installing the redistributable libraries specifying the "RuntimeLibraries"
component.

[1] http://www.cmake.org/Bug/view.php?id=15428)

Co-authored-by: Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>

git-svn-id: http://svn.slicer.org/Slicer4/trunk@24010 3bd1e089-480b-0410-8dfb-8563597acbee

CMake/SlicerCPack.cmake
@@ -45,6 +45,24 @@ if(NOT APPLE)
     include(${Slicer_CMAKE_DIR}/SlicerBlockInstallLibArchive.cmake)
   endif()
   include(InstallRequiredSystemLibraries)
+
+  # XXX: Remove this once CMake minimum version has been updated.
+  #      See Slicer issue #3972 and CMake issue #15428
+  if(CMAKE_INSTALL_SYSTEM_RUNTIME_LIBS)
+    if(NOT CMAKE_INSTALL_SYSTEM_RUNTIME_LIBS_SKIP)
+      if(NOT CMAKE_INSTALL_SYSTEM_RUNTIME_DESTINATION)
+        if(WIN32)
+          set(CMAKE_INSTALL_SYSTEM_RUNTIME_DESTINATION bin)
+        else()
+          set(CMAKE_INSTALL_SYSTEM_RUNTIME_DESTINATION lib)
+        endif()
+      endif()
+      install(PROGRAMS ${CMAKE_INSTALL_SYSTEM_RUNTIME_LIBS}
+        DESTINATION ${CMAKE_INSTALL_SYSTEM_RUNTIME_DESTINATION}
+        COMPONENT RuntimeLibraries)
+    endif()
+  endif()
+
   include(${Slicer_CMAKE_DIR}/SlicerBlockInstallCMakeProjects.cmake)
 
 else()