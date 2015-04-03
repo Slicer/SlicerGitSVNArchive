Slicer/SlicerGitSVNArchive

commit f17c8acf7a957011a612d6f197610e5130bbba7d

BUG: Ensure "qSlicerScriptedUtils::executeFile()" set "__file__" property.
This is a hack. Instead the "imp" module should be used as outline
in issue #3549 (Import scripted module as python module).

git-svn-id: http://svn.slicer.org/Slicer4/trunk@24149 3bd1e089-480b-0410-8dfb-8563597acbee

Base/QTCore/qSlicerScriptedUtils.cxx
@@ -20,6 +20,9 @@
 
 #include "qSlicerScriptedUtils_p.h"
 
+// Qt includes
+#include <QFileInfo>
+
 // PythonQt includes
 #include <PythonQt.h>
 
@@ -30,6 +33,11 @@ bool qSlicerScriptedUtils::executeFile(const QString& fileName, PyObject * globa
     {
     PyDict_SetItemString(global_dict, "__name__", PyString_FromString(className.toLatin1()));
     }
+
+  // Backup current __file__ value
+  QString savedFile = PyString_AsString(PyDict_GetItemString(global_dict, "__file__"));
+  // Set new __file__ value
+  PyDict_SetItemString(global_dict, "__file__", PyString_FromString(QFileInfo(fileName).fileName().toLatin1()));
 
   PyObject* pyRes = 0;
   if (fileName.endsWith(".py"))
@@ -47,6 +55,8 @@ bool qSlicerScriptedUtils::executeFile(const QString& fileName, PyObject * globa
     {
     PythonQt::self()->handleError();
     qCritical() << "setPythonSource - Failed to execute file" << fileName << "!";
+    // Restore __file__ value
+    PyDict_SetItemString(global_dict, "__file__", PyString_FromString(savedFile.toLatin1()));
     return false;
     }
   Py_DECREF(pyRes);
@@ -54,6 +64,8 @@ bool qSlicerScriptedUtils::executeFile(const QString& fileName, PyObject * globa
     {
     PyDict_SetItemString(global_dict, "__name__", PyString_FromString("__main__"));
     }
+  // Restore __file__ value
+  PyDict_SetItemString(global_dict, "__file__", PyString_FromString(savedFile.toLatin1()));
   return true;
 }
 

Base/QTCore/Testing/Python/qSlicerModuleGenericTest.py.in
@@ -8,6 +8,12 @@ class qSlicer@MODULENAME@ModuleGenericTest(unittest.TestCase):
     self.assertTrue(hasattr(slicer.modules, '@MODULENAME_LC@'))
     self.module = slicer.modules.@MODULENAME_LC@
 
+  def test_file_attribute(self):
+    self.assertRegexpMatches(__file__, r'qSlicer@MODULENAME@ModuleGenericTest.py[c]?$')
+
+  def test_name_attribute(self):
+    self.assertEqual(__name__, 'qSlicer@MODULENAME@ModuleGenericTest')
+
   def test_module_valid(self):
     self.assertNotEqual(self.module, None)
     self.assertEqual(self.module.name, '@MODULENAME@')

commit 1d8eda98636a64462dcfa974190098ee10350892

BUG: Fix qSlicerScriptedLoadableModuleWidgetTest broken by previous commit
This commit fixes a regression introduced by previous
commit r24149 (BUG: Ensure "qSlicerScriptedUtils::executeFile()"
set "__file__" property.

git-svn-id: http://svn.slicer.org/Slicer4/trunk@24150 3bd1e089-480b-0410-8dfb-8563597acbee

Base/QTCore/qSlicerScriptedUtils.cxx
@@ -35,7 +35,11 @@ bool qSlicerScriptedUtils::executeFile(const QString& fileName, PyObject * globa
     }
 
   // Backup current __file__ value
-  QString savedFile = PyString_AsString(PyDict_GetItemString(global_dict, "__file__"));
+  QString savedFile;
+  if (PyObject_HasAttrString(global_dict, "__file__"))
+    {
+    PyString_AsString(PyDict_GetItemString(global_dict, "__file__"));
+    }
   // Set new __file__ value
   PyDict_SetItemString(global_dict, "__file__", PyString_FromString(QFileInfo(fileName).fileName().toLatin1()));
 