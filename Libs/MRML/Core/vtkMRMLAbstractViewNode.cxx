Slicer/SlicerGitSVNArchive

commit e322dc93ba9e5f2a385786fe7cf8602b61c2d729

STYLE: Add abstract class for all view nodes
vtkMRMLViewNode, vbtkMRMLSliceNode and vtkMRMLChartViewNode share some
common properties: they are views of a layout.

git-svn-id: http://svn.slicer.org/Slicer4/trunk@21034 3bd1e089-480b-0410-8dfb-8563597acbee

Libs/MRML/Core/vtkMRMLAbstractViewNode.cxx
@@ -1,0 +1,238 @@
+/*==============================================================================
+
+  Program: 3D Slicer
+
+  Copyright (c) Kitware Inc.
+
+  See COPYRIGHT.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+  This file was originally developed by Julien Finet, Kitware Inc.
+  and was partially funded by NIH grant 3P41RR013218-12S1
+
+==============================================================================*/
+
+// MRML includes
+#include "vtkMRMLAbstractViewNode.h"
+#include "vtkMRMLScene.h"
+
+// STD includes
+#include <sstream>
+
+//----------------------------------------------------------------------------
+vtkMRMLAbstractViewNode::vtkMRMLAbstractViewNode()
+{
+  this->LayoutLabel = NULL;
+  this->Active = 0;
+  this->Visibility = 0;
+
+  double black[3] = {0.,0.,0.};
+  memcpy(this->BackgroundColor, black, 3 * sizeof(double));
+  memcpy(this->BackgroundColor2, black, 3 * sizeof(double));
+
+  this->SetLayoutLabel("1");
+  this->SetHideFromEditors(0);
+ }
+
+//----------------------------------------------------------------------------
+vtkMRMLAbstractViewNode::~vtkMRMLAbstractViewNode()
+{
+  if ( this->LayoutLabel )
+    {
+    delete [] this->LayoutLabel;
+    }
+}
+
+//----------------------------------------------------------------------------
+void vtkMRMLAbstractViewNode::WriteXML(ostream& of, int nIndent)
+{
+  // Write all attributes not equal to their defaults
+
+  this->Superclass::WriteXML(of, nIndent);
+
+  vtkIndent indent(nIndent);
+
+  if (this->GetLayoutLabel())
+    {
+    of << indent << " layoutLabel=\"" << this->GetLayoutLabel() << "\"";
+    }
+  if (this->GetLayoutName())
+    {
+    of << indent << " layoutName=\"" << this->GetLayoutName() << "\"";
+    }
+
+  of << indent << " active=\"" << (this->Active ? "true" : "false") << "\"";
+  of << indent << " visibility=\"" << (this->Visibility ? "true" : "false") << "\"";
+
+  // background color
+  of << indent << " backgroundColor=\"" << this->BackgroundColor[0] << " "
+     << this->BackgroundColor[1] << " " << this->BackgroundColor[2] << "\"";
+
+  of << indent << " backgroundColor2=\"" << this->BackgroundColor2[0] << " "
+     << this->BackgroundColor2[1] << " " << this->BackgroundColor2[2] << "\"";
+}
+
+//----------------------------------------------------------------------------
+void vtkMRMLAbstractViewNode::ReadXMLAttributes(const char** atts)
+{
+  bool isBackgroundColor2Set = false;
+  int disabledModify = this->StartModify();
+
+  this->Superclass::ReadXMLAttributes(atts);
+
+  const char* attName;
+  const char* attValue;
+  while (*atts != NULL) 
+    {
+    attName = *(atts++);
+    attValue = *(atts++);
+    if (!strcmp(attName, "layoutLabel")) 
+      {
+      this->SetLayoutLabel( attValue );
+      }
+    else if (!strcmp(attName, "layoutName"))
+      {
+      this->SetLayoutName( attValue );
+      }
+    else if (!strcmp(attName, "backgroundColor"))
+      {
+      std::stringstream ss;
+      ss << attValue;
+      double val;
+      ss >> val;
+      this->BackgroundColor[0] = val;
+      ss << attValue;
+      ss >> val;
+      this->BackgroundColor[1] = val;
+      ss << attValue;
+      ss >> val;
+      this->BackgroundColor[2] = val;
+      }
+    else if (!strcmp(attName, "backgroundColor2"))
+      {
+      isBackgroundColor2Set = true;
+      std::stringstream ss;
+      ss << attValue;
+      double val;
+      ss >> val;
+      this->BackgroundColor2[0] = val;
+      ss << attValue;
+      ss >> val;
+      this->BackgroundColor2[1] = val;
+      ss << attValue;
+      ss >> val;
+      this->BackgroundColor2[2] = val;
+      }
+    else if (!strcmp(attName, "active")) 
+      {
+      if (!strcmp(attValue,"true")) 
+        {
+        this->Active = 1;
+        }
+      else
+        {
+        this->Active = 0;
+        }
+      }
+    else if (!strcmp(attName, "visibility")) 
+      {
+      if (!strcmp(attValue,"true")) 
+        {
+        this->Visibility = 1;
+        }
+      else
+        {
+        this->Visibility = 0;
+        }
+      }
+    }
+#if MRML_SUPPORT_VERSION < 0x040000
+  if (!isBackgroundColor2Set)
+    {
+    this->BackgroundColor2[0] = this->BackgroundColor[0];
+    this->BackgroundColor2[1] = this->BackgroundColor[1];
+    this->BackgroundColor2[2] = this->BackgroundColor[2];
+    }
+#endif
+#if MRML_SUPPORT_VERSION < 0x040200
+  // vtkMRMLAbstractViewNodes where not singletons before 4.2
+  if (!this->GetLayoutName() || strlen(this->GetLayoutName()))
+    {
+    const char* layoutName = 0;
+    if (this->GetID() &&
+        strncmp(this->GetID(), this->GetClassName(), strlen(this->GetClassName())) == 0)
+      {
+      layoutName = this->GetID() + strlen(this->GetClassName());
+      }
+    if (!layoutName || strlen(layoutName) == 0)
+      {
+      layoutName = "1";
+      }
+    this->SetLayoutName(layoutName);
+    }
+#endif
+  this->EndModify(disabledModify);
+}
+
+//----------------------------------------------------------------------------
+// Copy the node's attributes to this object.
+// Does NOT copy: ID, FilePrefix, Name, ID
+void vtkMRMLAbstractViewNode::Copy(vtkMRMLNode *anode)
+{
+  int disabledModify = this->StartModify();
+
+  Superclass::Copy(anode);
+  vtkMRMLAbstractViewNode *node = (vtkMRMLAbstractViewNode *) anode;
+
+  this->SetLayoutLabel(node->GetLayoutLabel());
+  this->SetBackgroundColor ( node->GetBackgroundColor ( ) );
+  this->SetBackgroundColor2 ( node->GetBackgroundColor2 ( ) );
+  // Important: do not use SetActive or RemoveActiveFlagInScene will be called
+  this->Active = node->GetActive();
+  this->Visibility = node->GetVisibility();
+
+  this->EndModify(disabledModify);
+}
+
+//----------------------------------------------------------------------------
+void vtkMRMLAbstractViewNode::PrintSelf(ostream& os, vtkIndent indent)
+{
+  Superclass::PrintSelf(os,indent);
+
+  os << indent << "LayoutLabel: " << (this->LayoutLabel ? this->LayoutLabel : "(null)") << std::endl;
+  os << indent << "Active:        " << this->Active << "\n";
+  os << indent << "Visibility:        " << this->Visibility << "\n";
+  os << indent << "BackgroundColor:       " << this->BackgroundColor[0] << " "
+     << this->BackgroundColor[1] << " "
+     << this->BackgroundColor[2] <<"\n";
+  os << indent << "BackgroundColor2:       " << this->BackgroundColor2[0] << " "
+     << this->BackgroundColor2[1] << " "
+     << this->BackgroundColor2[2] <<"\n";
+}
+
+//----------------------------------------------------------------------------
+void vtkMRMLAbstractViewNode::RemoveActiveFlagInScene()
+{
+  if (this->Scene == NULL)
+    {
+    return;
+    }
+
+  vtkMRMLAbstractViewNode *node = NULL;
+  int nnodes = this->Scene->GetNumberOfNodesByClass("vtkMRMLAbstractViewNode");
+  for (int n=0; n<nnodes; n++)
+    {
+    node = vtkMRMLAbstractViewNode::SafeDownCast (
+       this->Scene->GetNthNodeByClass(n, "vtkMRMLAbstractViewNode"));
+    if (node != this)
+      {
+      node->SetActive(0);
+      }
+    }
+}

Libs/MRML/Core/CMakeLists.txt
@@ -81,6 +81,7 @@ set(MRMLCore_SRCS
   vtkImageBimodalAnalysis.cxx
   vtkDataFileFormatHelper.cxx
   vtkMRMLLogic.cxx
+  vtkMRMLAbstractViewNode.cxx
   vtkMRMLCameraNode.cxx
   vtkMRMLChartNode.cxx
   vtkMRMLChartViewNode.cxx

Libs/MRML/Core/vtkMRMLAbstractViewNode.h
@@ -1,0 +1,138 @@
+/*==============================================================================
+
+  Program: 3D Slicer
+
+  Copyright (c) Kitware Inc.
+
+  See COPYRIGHT.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+  This file was originally developed by Julien Finet, Kitware Inc.
+  and was partially funded by NIH grant 3P41RR013218-12S1
+
+==============================================================================*/
+
+#ifndef __vtkMRMLAbstractViewNode_h
+#define __vtkMRMLAbstractViewNode_h
+
+// MRML includes
+#include "vtkMRMLNode.h"
+
+/// \brief Abstract MRML node to represent a view.
+/// The class holds the properties common to any view type (3D, slice, chart..)
+/// Views are not hidden from editors by default (HideFromEditor is 0)
+class VTK_MRML_EXPORT vtkMRMLAbstractViewNode
+  : public vtkMRMLNode
+{
+public:
+  vtkTypeMacro(vtkMRMLAbstractViewNode,vtkMRMLNode);
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+  //--------------------------------------------------------------------------
+  /// MRMLNode methods
+  //--------------------------------------------------------------------------
+
+  ///
+  /// Read node attributes from XML file
+  virtual void ReadXMLAttributes( const char** atts);
+
+  ///
+  /// Write this node's information to a MRML file in XML format.
+  virtual void WriteXML(ostream& of, int indent);
+
+  ///
+  /// Copy the node's attributes to this object
+  virtual void Copy(vtkMRMLNode *node);
+
+  ///
+  /// Name of the layout. Must be unique between all the view nodes of the
+  /// same type because it is used as a singleton tag.
+  /// Typical names can be colors "Red", "Green", "Yellow",...
+  /// or numbers "1", "2"... to uniquely define the node.
+  /// No name (i.e. "") by default.
+  /// \sa SetSingletonTag(), SetViewLabel()
+  inline void SetLayoutName(const char *layoutName);
+  inline const char *GetLayoutName();
+
+  ///
+  /// Label for the view. Usually a 1 character label, e.g. R, 1, 2, etc.
+  /// \sa SetLayoutName()
+  vtkSetStringMacro(LayoutLabel);
+  vtkGetStringMacro(LayoutLabel);
+
+  ///
+  /// Indicates whether or not the view is active
+  vtkGetMacro (Active, int );
+  vtkSetMacro (Active, int );
+
+  ///
+  /// Indicates whether or not the view is visible (if it is not visible,
+  /// then the view is not shown in any of the view layouts, but can be privately
+  /// used by modules)
+  vtkGetMacro(Visibility, int);
+  vtkSetMacro(Visibility, int);
+
+  ///
+  /// 1st background color of the view.
+  /// Black (0,0,0) by default.
+  /// \sa SetBackgroundColor2()
+  vtkGetVector3Macro (BackgroundColor, double);
+  vtkSetVector3Macro (BackgroundColor, double);
+
+  ///
+  /// 2nd background color of the view
+  /// Black (0,0,0) by default.
+  /// \sa SetBackgroundColor2()
+  vtkGetVector3Macro (BackgroundColor2, double);
+  vtkSetVector3Macro (BackgroundColor2, double);
+
+protected:
+  vtkMRMLAbstractViewNode();
+  ~vtkMRMLAbstractViewNode();
+
+  vtkMRMLAbstractViewNode(const vtkMRMLAbstractViewNode&);
+  void operator=(const vtkMRMLAbstractViewNode&);
+
+  ///
+  /// Label to show for the view (shortcut for the name)
+  char * LayoutLabel;
+
+  ///
+  /// Indicates whether or not the View is visible.
+  /// Invisible (0) by default.
+  int Visibility;
+
+  ///
+  /// Indicates whether or not the View is active.
+  /// Inactive (1) by default.
+  int Active;
+
+  ///
+  /// When a view is set Active, make other views inactive.
+  virtual void RemoveActiveFlagInScene();
+
+  ///
+  /// Background colors
+  double BackgroundColor[3];
+  double BackgroundColor2[3];
+};
+
+//------------------------------------------------------------------------------
+void vtkMRMLAbstractViewNode::SetLayoutName(const char *layoutName)
+{
+  this->SetSingletonTag(layoutName);
+}
+
+//------------------------------------------------------------------------------
+const char *vtkMRMLAbstractViewNode::GetLayoutName()
+{
+  return this->GetSingletonTag();
+}
+
+#endif

Libs/MRML/Core/vtkMRMLChartViewNode.cxx
@@ -1,10 +1,26 @@
+/*=auto=========================================================================
 
-#include <sstream>
+  Portions (c) Copyright 2005 Brigham and Women's Hospital (BWH) All Rights Reserved.
 
-#include "vtkObjectFactory.h"
+  See COPYRIGHT.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
 
+  Program:   3D Slicer
+  Module:    $RCSfile: vtkMRMLChartNode.h,v $
+  Date:      $Date: 2006/03/19 17:12:29 $
+  Version:   $Revision: 1.3 $
+
+=========================================================================auto=*/
+
+// MRML includes
 #include "vtkMRMLChartViewNode.h"
 #include "vtkMRMLScene.h"
+
+// VTK includes
+#include <vtkObjectFactory.h>
+
+// STD includes
+#include <sstream>
 
 //----------------------------------------------------------------------------
 vtkMRMLNodeNewMacro(vtkMRMLChartViewNode);
@@ -12,11 +28,9 @@ vtkMRMLNodeNewMacro(vtkMRMLChartViewNode);
 //vtkCxxSetReferenceStringMacro(vtkMRMLChartViewNode, ChartNodeID);
 
 //----------------------------------------------------------------------------
-vtkMRMLChartViewNode::vtkMRMLChartViewNode() : vtkMRMLNode()
+vtkMRMLChartViewNode::vtkMRMLChartViewNode()
 {
   this->ChartNodeID = 0;
-  this->ViewLabel = new char[2];
-  strcpy(this->ViewLabel, "1");
 }
 
 //----------------------------------------------------------------------------
@@ -25,10 +39,6 @@ vtkMRMLChartViewNode::~vtkMRMLChartViewNode()
   if (this->ChartNodeID)
     {
     this->SetChartNodeID(0);
-    }
-  if ( this->ViewLabel )
-    {
-    delete [] this->ViewLabel;
     }
 }
 
@@ -49,14 +59,6 @@ void vtkMRMLChartViewNode::WriteXML(ostream& of, int nIndent)
     {
     of << " chart=\"" << this->ChartNodeID << "\"";
     }
-  if (this->GetViewLabel())
-    {
-    of << indent << " layoutLabel=\"" << this->GetViewLabel() << "\"";
-    }
-  if (this->GetLayoutName() != NULL)
-    {
-    of << indent << " layoutName=\"" << this->GetLayoutName() << "\"";
-    }
 }
 
 //----------------------------------------------------------------------------
@@ -72,15 +74,7 @@ void vtkMRMLChartViewNode::ReadXMLAttributes(const char** atts)
     {
     attName = *(atts++);
     attValue = *(atts++);
-    if (!strcmp(attName, "layoutLabel")) 
-      {
-      this->SetViewLabel( attValue );
-      }
-    else if (!strcmp(attName, "layoutName")) 
-      {
-      this->SetLayoutName( attValue );
-      }
-    else if (!strcmp(attName, "chart")) 
+    if (!strcmp(attName, "chart"))
       {
       this->SetChartNodeID(attValue);
       }
@@ -98,9 +92,8 @@ void vtkMRMLChartViewNode::Copy(vtkMRMLNode *anode)
 
   int disabledModify = this->StartModify();
 
-  Superclass::Copy(anode);
+  this->Superclass::Copy(anode);
 
-  this->SetViewLabel(achartviewnode->GetViewLabel());
   this->SetChartNodeID(achartviewnode->GetChartNodeID());
 
   this->EndModify(disabledModify);
@@ -109,10 +102,8 @@ void vtkMRMLChartViewNode::Copy(vtkMRMLNode *anode)
 //----------------------------------------------------------------------------
 void vtkMRMLChartViewNode::PrintSelf(ostream& os, vtkIndent indent)
 {
-  
-  Superclass::PrintSelf(os,indent);
+  this->Superclass::PrintSelf(os,indent);
 
-  os << indent << "ViewLabel: " << (this->ViewLabel ? this->ViewLabel : "(null)") << std::endl;
   os << indent << "ChartNodeID: " << 
    (this->ChartNodeID ? this->ChartNodeID : "(none)") << "\n";
 }

Libs/MRML/Core/vtkMRMLChartViewNode.h
@@ -15,16 +15,16 @@
 #ifndef __vtkMRMLChartViewNode_h
 #define __vtkMRMLChartViewNode_h
 
-#include "vtkMRMLNode.h"
+#include "vtkMRMLAbstractViewNode.h"
 
 /// \brief MRML node to represent chart view parameters.
 ///
 /// ChartViewNodes are associated one to one with a ChartWidget.
-class VTK_MRML_EXPORT vtkMRMLChartViewNode : public vtkMRMLNode
+class VTK_MRML_EXPORT vtkMRMLChartViewNode : public vtkMRMLAbstractViewNode
 {
 public:
   static vtkMRMLChartViewNode *New();
-  vtkTypeMacro(vtkMRMLChartViewNode,vtkMRMLNode);
+  vtkTypeMacro(vtkMRMLChartViewNode, vtkMRMLAbstractViewNode);
   void PrintSelf(ostream& os, vtkIndent indent);
   
   //--------------------------------------------------------------------------
@@ -50,15 +50,6 @@ public:
   /// Get node XML tag name (like Volume, Model)
   virtual const char* GetNodeTagName();
 
-  /// 
-  /// Name of the layout
-  void SetLayoutName(const char *layoutName) {
-    this->SetSingletonTag(layoutName);
-  }
-  char *GetLayoutName() {
-    return this->GetSingletonTag();
-  }
-
   ///
   /// Set the Chart node id displayed in this Chart View
   void SetChartNodeID(const char *);
@@ -66,10 +57,6 @@ public:
   ///
   /// Get the Chart node id displayed in this Chart View
   vtkGetStringMacro(ChartNodeID);
-
-  /// Label for the view. Usually a 1 character label, e.g. 1, 2, etc.
-  vtkSetStringMacro(ViewLabel);
-  vtkGetStringMacro(ViewLabel);
 
   /// 
   /// Updates this node if it depends on other nodes 
@@ -94,7 +81,6 @@ protected:
   void operator=(const vtkMRMLChartViewNode&);
 
   char* ChartNodeID;
-  char* ViewLabel;
 };
 
 #endif

Libs/MRML/Core/vtkMRMLSliceNode.cxx
@@ -106,11 +106,11 @@ vtkMRMLSliceNode::vtkMRMLSliceNode()
 
   this->IsUpdatingMatrices = 0;
 
-  this->LayoutLabel = new char[1];
-  strcpy(this->LayoutLabel, "");
   this->LayoutColor[0] = vtkMRMLSliceNode::grayColor()[0];
   this->LayoutColor[1] = vtkMRMLSliceNode::grayColor()[1];
   this->LayoutColor[2] = vtkMRMLSliceNode::grayColor()[2];
+
+  this->SetLayoutLabel("");
 }
 
 //----------------------------------------------------------------------------
@@ -144,11 +144,6 @@ vtkMRMLSliceNode::~vtkMRMLSliceNode()
     {
     delete [] this->OrientationReference;
     }
-  if ( this->LayoutLabel )
-    {
-    delete [] this->LayoutLabel;
-    }
-  this->SetLayoutName(NULL);
 }
 
 //-----------------------------------------------------------
@@ -660,14 +655,6 @@ void vtkMRMLSliceNode::WriteXML(ostream& of, int nIndent)
       }
     }
   of << indent << " sliceToRAS=\"" << ss.str().c_str() << "\"";
-  if (this->GetLayoutName())
-    {
-    of << indent << " layoutName=\"" << this->GetLayoutName() << "\"";
-    }
-  if (this->GetLayoutLabel())
-    {
-    of << indent << " layoutLabel=\"" << this->GetLayoutLabel() << "\"";
-    }
   of << indent << " layoutColor=\"" << this->LayoutColor[0] << " "
      << this->LayoutColor[1] << " " << this->LayoutColor[2] << "\"";
   if (this->OrientationString)
@@ -708,7 +695,7 @@ void vtkMRMLSliceNode::ReadXMLAttributes(const char** atts)
     attValue = *(atts++);
     if (!strcmp(attName, "layoutLabel")) 
       {
-      this->SetLayoutLabel( attValue );
+      // layout label is set in Superclass
       layoutLabelFound = true;
       }
     else if (!strcmp(attName, "layoutColor")) 
@@ -1019,7 +1006,6 @@ void vtkMRMLSliceNode::Copy(vtkMRMLNode *anode)
   Superclass::Copy(anode);
   vtkMRMLSliceNode *node = vtkMRMLSliceNode::SafeDownCast(anode);
 
-  this->SetLayoutLabel(node->GetLayoutLabel());
   this->SetLayoutColor(node->GetLayoutColor());
 
   this->SetSliceVisible(node->GetSliceVisible());
@@ -1077,7 +1063,6 @@ void vtkMRMLSliceNode::PrintSelf(ostream& os, vtkIndent indent)
   int idx;
   
   Superclass::PrintSelf(os,indent);
-  os << indent << "LayoutLabel: " << (this->LayoutLabel ? this->LayoutLabel : "(null)") << std::endl;
   os << indent << "LayoutColor: " << this->LayoutColor[0] << " "
                                   << this->LayoutColor[1] << " "
                                   << this->LayoutColor[2] << std::endl;

Libs/MRML/Core/vtkMRMLSliceNode.h
@@ -15,9 +15,11 @@
 #ifndef __vtkMRMLSliceNode_h
 #define __vtkMRMLSliceNode_h
 
-#include "vtkMRMLNode.h"
+// MRML includes
+#include "vtkMRMLAbstractViewNode.h"
 class vtkMRMLVolumeNode;
 
+// VTK includes
 class vtkMatrix4x4;
 
 /// \brief MRML node for storing a slice through RAS space.
@@ -26,11 +28,11 @@ class vtkMatrix4x4;
 /// the desired slice plane.
 /// \li SliceToRAS is the matrix that rotates and translates the slice plane
 /// \li FieldOfView tells the size of  slice plane
-class VTK_MRML_EXPORT vtkMRMLSliceNode : public vtkMRMLNode
+class VTK_MRML_EXPORT vtkMRMLSliceNode : public vtkMRMLAbstractViewNode
 {
   public:
   static vtkMRMLSliceNode *New();
-  vtkTypeMacro(vtkMRMLSliceNode,vtkMRMLNode);
+  vtkTypeMacro(vtkMRMLSliceNode,vtkMRMLAbstractViewNode);
   void PrintSelf(ostream& os, vtkIndent indent);
 
   virtual vtkMRMLNode* CreateNodeInstance();
@@ -199,26 +201,6 @@ class VTK_MRML_EXPORT vtkMRMLSliceNode : public vtkMRMLNode
   /// Recalculate XYToSlice and XYToRAS in terms or fov, dim, SliceToRAS
   /// - called when any of the inputs change
   void UpdateMatrices();
-
-  /// 
-  /// Name of the layout. Must be unique between all the slice
-  /// nodes because it is used as a singleton tag.
-  /// Must be the same than the slice composite node.
-  /// No name (i.e. "") by default. Typical names are colors:
-  /// "Red", "Green", "Yellow"... to uniquely define the slice node
-  /// \sa vtkMRMLSliceCompositeNode::SetLayoutName
-  void SetLayoutName(const char *layoutName) 
-  {
-    this->SetSingletonTag(layoutName);
-  }
-  char *GetLayoutName() 
-  {
-    return this->GetSingletonTag();
-  }
-
-  /// Label for layout. Usually a 1 character label, e.g. R, 1, 2, etc.
-  vtkSetStringMacro(LayoutLabel);
-  vtkGetStringMacro(LayoutLabel);
 
   /// Color for layout as rgb
   vtkSetVector3Macro(LayoutColor, double);
@@ -416,7 +398,6 @@ protected:
   char *OrientationString;
   char *OrientationReference;
 
-  char * LayoutLabel;
   double LayoutColor[3];
 
   int LayoutGridRows;

commit 783d890f70e67dd5db8a0eb873da33e5441615c8

BUG: Change default value for vtkMRMLAbstractViewNode Visibility property
This commit fixes a regression introduced in r21034 (STYLE: Add abstract
class for all view nodes) where default value of the visibility
property (originally associated with vtkMRMLViewNode) has been changed
from 1 to 0.

This commit set the default value to 1.

Libs/MRML/Core/vtkMRMLAbstractViewNode.cxx
@@ -30,7 +30,7 @@ vtkMRMLAbstractViewNode::vtkMRMLAbstractViewNode()
 {
   this->LayoutLabel = NULL;
   this->Active = 0;
-  this->Visibility = 0;
+  this->Visibility = 1;
 
   double black[3] = {0.,0.,0.};
   memcpy(this->BackgroundColor, black, 3 * sizeof(double));