Slicer/SlicerGitSVNArchive

commit 306069a6c7641eb0ccfaf10665ba1b67b44efed3

BUG: Fixed extension creation in version-controlled repository
It was only possible to create extension in a non-version-controlled directory.
The problem was that extension was created in repositoryRoot/extensionName directory
but when an extension is opened in a version-controlled directory then the extension
is expected to be found in repositoryRoot.

Changed extension creation so that if it is created in a version-controlled directory
then files are added to repositoryRoot (instead of repositoryRoot/extensionName).

git-svn-id: http://svn.slicer.org/Slicer4/trunk@24452 3bd1e089-480b-0410-8dfb-8563597acbee

Utilities/Scripts/SlicerWizard/TemplateManager.py
@@ -115,7 +115,7 @@ class TemplateManager(object):
       fp.write(contents)
 
   #---------------------------------------------------------------------------
-  def copyTemplate(self, destination, category, kind, name):
+  def copyTemplate(self, destination, category, kind, name, createInSubdirectory = True, requireEmptyDirectory = True):
     """Copy (instantiate) a template.
 
     :param destination: Directory in which to create the template copy.
@@ -126,6 +126,10 @@ class TemplateManager(object):
     :type kind: :class:`basestring`
     :param name: Name for the instantiated template.
     :type name: :class:`basestring`
+    :param createInSubdirectory: If True then files are copied to destination/name/, else destination/.
+    :type name: :class:`bool`
+    :param requireEmptyDirectory: If True then files are only copied if the target directory is empty.
+    :type name: :class:`bool`
 
     :return:
       Path to the new instance (``os.path.join(destination, name)``).
@@ -153,8 +157,10 @@ class TemplateManager(object):
 
     kind = kind.lower()
 
+    if createInSubdirectory:
     destination = os.path.join(destination, name)
-    if os.path.exists(destination):
+
+    if requireEmptyDirectory and os.path.exists(destination):
       raise IOError("create %s: refusing to overwrite"
                     " existing directory '%s'" % (category, destination))
 

Modules/Scripted/ExtensionWizard/ExtensionWizard.py
@@ -203,10 +203,34 @@ class ExtensionWizardWidget:
     dlg.setTemplates(self.templateManager.templates("extensions"))
 
     while dlg.exec_() == qt.QDialog.Accepted:
+        
+        # If the selected destination is in a repository then use the root of that repository
+        # as destination
       try:
+        repo = SlicerWizard.Utilities.getRepo(dlg.destination)
+
+        createInSubdirectory = True
+        requireEmptyDirectory = True
+        
+        if repo is None:
+          destination = os.path.join(dlg.destination, dlg.componentName)
+          if os.path.exists(dlg.destination):
+            raise IOError("create extension: refusing to overwrite"
+                          " existing directory '%s'" % dlg.destination)
+
+        else:
+          destination = SlicerWizard.Utilities.localRoot(repo)
+          cmakeFile = os.path.join(destination, "CMakeLists.txt")
+          createInSubdirectory = False # create the files in the destination directory
+          requireEmptyDirectory = False # we only check if no CMakeLists.txt file exists
+          if os.path.exists(cmakeFile):
+            raise IOError("create extension: refusing to overwrite"
+                          " directory containing CMakeLists.txt file at '%s'" % dlg.destination)
+
         path = self.templateManager.copyTemplate(
-                 dlg.destination, "extensions",
-                 dlg.componentType, dlg.componentName)
+                 destination, "extensions",
+                 dlg.componentType, dlg.componentName,
+                 createInSubdirectory, requireEmptyDirectory)
 
       except:
         md = qt.QMessageBox(self.parent.window())

commit 0cc16eaff7b757d22b0301eae6514abb1741570e

BUG: Fix indentation issue in TemplateManager.py
This commit fixes a regression introduced in r24452 (BUG: Fixed extension
creation in version-controlled repository).

Traceback (most recent call last):
  File "<string>", line 1, in <module>
  File "/home/jcfr/Projects/Slicer-Debug/Slicer-build/lib/Slicer-4.4/qt-scripted-modules/ExtensionWizard.py", line 7, in <module>
    import SlicerWizard.ExtensionDescription
  File "/home/jcfr/Projects/Slicer-Debug/Slicer-build/bin/Python/SlicerWizard/__init__.py", line 44, in <module>
    from .ExtensionWizard import ExtensionWizard
  File "/home/jcfr/Projects/Slicer-Debug/Slicer-build/bin/Python/SlicerWizard/ExtensionWizard.py", line 30, in <module>
    from .TemplateManager import TemplateManager
  File "/home/jcfr/Projects/Slicer-Debug/Slicer-build/bin/Python/SlicerWizard/TemplateManager.py", line 161
    destination = os.path.join(destination, name)
              ^
IndentationError: expected an indented block

git-svn-id: http://svn.slicer.org/Slicer4/trunk@24455 3bd1e089-480b-0410-8dfb-8563597acbee

Utilities/Scripts/SlicerWizard/TemplateManager.py
@@ -158,7 +158,7 @@ class TemplateManager(object):
     kind = kind.lower()
 
     if createInSubdirectory:
-    destination = os.path.join(destination, name)
+      destination = os.path.join(destination, name)
 
     if requireEmptyDirectory and os.path.exists(destination):
       raise IOError("create %s: refusing to overwrite"