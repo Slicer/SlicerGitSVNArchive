11f6b4f1eee1284f9dac45c38c028b494da2b305
zhangfanmark <zhangfanmark@gmail.com>
2015-08-05 11:35:49 -0400

    BUG: FiberBundleLabelSelect - VTK as input and output
    MANTIS issue id : 0004025
    
    Fixing the module so that it takes both VTK and VTP when being used as
    CLI

--- Modules/CLI/FiberBundleLabelSelect/FiberBundleLabelSelect.cxx
+++ Modules/CLI/FiberBundleLabelSelect/FiberBundleLabelSelect.cxx
@@ -34,6 +34,10 @@
 #include <vtkXMLPolyDataWriter.h>
 #include <vtkVersion.h>
 
+// added for taking VTK and VTP as input and output
+#include <vtkPolyDataReader.h>
+#include <vtksys/SystemTools.hxx>
+
 // STD includes
 #include <iostream>
 #include <algorithm>
@@ -97,9 +101,26 @@ int main( int argc, char * argv[] )
   imageCastLabel_A->Update();
 
   // Read in fiber bundle input to be selected.
-  vtkNew<vtkXMLPolyDataReader> readerPD;
-  readerPD->SetFileName(InputFibers.c_str());
-  readerPD->Update();
+  // CHANGE: the original code was replaced for taking both VTP and VTK as input
+  std::string extension1 = vtksys::SystemTools::GetFilenameLastExtension(InputFibers.c_str());
+  std::string extension = vtksys::SystemTools::LowerCase(extension1);
+
+  vtkSmartPointer<vtkPolyData> input;
+  if (extension == std::string(".vtk"))
+    {
+    vtkPolyData* output = 0;
+    vtkNew<vtkPolyDataReader> readerPD;
+    readerPD->SetFileName(InputFibers.c_str());
+    readerPD->Update();
+    input = vtkPolyData::SafeDownCast(readerPD->GetOutput());
+    }
+  else if (extension == std::string(".vtp"))
+    {
+      vtkNew<vtkXMLPolyDataReader> readerPD;
+      readerPD->SetFileName(InputFibers.c_str());
+      readerPD->Update();
+      input = vtkPolyData::SafeDownCast(readerPD->GetOutput());
+    }
 
 
   //1. Set up matrices to put fibers into ijk space of volume
@@ -132,8 +153,6 @@ int main( int argc, char * argv[] )
   imageCastLabel_A->GetOutputInformation(0)->Get(
     vtkStreamingDemandDrivenPipeline::WHOLE_EXTENT(), inExt);
 #endif
-
-  vtkPolyData *input = vtkPolyData::SafeDownCast(readerPD->GetOutput());
 
   vtkPoints *inPts =input->GetPoints();
   vtkIdType numPts = inPts->GetNumberOfPoints();
@@ -196,7 +215,7 @@ int main( int argc, char * argv[] )
       if (pt[0] < 0 || pt[1] < 0 || pt[2] < 0 ||
           pt[0] >= labelDims[0] || pt[1] >= labelDims[1] || pt[2] >= labelDims[2])
         {
-        std::cerr << "point #" << j <<" on the line #" << inCellId << " is outside the label\n";
+        //std::cerr << "point #" << j <<" on the line #" << inCellId << " is outside the label\n";
         continue;
         }
 
@@ -325,14 +344,35 @@ int main( int argc, char * argv[] )
     }
 
   //3. Save the output
-  vtkNew<vtkXMLPolyDataWriter> writer;
-  writer->SetFileName(OutputFibers.c_str());
-#if (VTK_MAJOR_VERSION <= 5)
-  writer->SetInput(outFibers.GetPointer());
-#else
-  writer->SetInputData(outFibers.GetPointer());
-#endif
-  writer->Write();
+  // CHANGE: the original code was replaced for taking both VTP and VTK as output
+  std::string extension2 = vtksys::SystemTools::GetFilenameLastExtension(OutputFibers.c_str());
+  std::string extension_output = vtksys::SystemTools::LowerCase(extension2);
+  // The above two lines duplicate the below function, but we can't include vtkMRMLStorageNode.h here.
+  //std::string extension = vtkMRMLStorageNode::GetLowercaseExtensionFromFileName(InputFibers.c_str());
+  if (extension_output == std::string(".vtk"))
+    {
+      vtkNew<vtkPolyDataWriter> writer;
+      writer->SetFileName(OutputFibers.c_str());
+      #if (VTK_MAJOR_VERSION <= 5)
+           writer->SetInput(outFibers.GetPointer());
+      #else
+           writer->SetInputData(outFibers.GetPointer());
+      #endif
+           writer->SetFileTypeToBinary();
+           writer->Write();
+    }
+  else if (extension_output == std::string(".vtp"))
+    {
+           vtkNew<vtkXMLPolyDataWriter> writer;
+           writer->SetFileName(OutputFibers.c_str());
+      #if (VTK_MAJOR_VERSION <= 5)
+           writer->SetInput(outFibers.GetPointer());
+      #else
+           writer->SetInputData(outFibers.GetPointer());
+      #endif
+           writer->SetDataModeToBinary();
+           writer->Write();
+    }
   }
   catch ( ... )
       {
