Slicer/SlicerGitSVNArchive

commit 4d3e2d414a66624a5293ec628d90500d4d35fb65

BUG: fix support for DCMTK DICOM dictionaries
See issue 4013, followup on commit 24473. Dictionaries passed to DCMTK in
DCMDICPATH override embedded dictionaries, and standard dictionary needs to be
specified together with the private one, as shown in example here:

http://support.dcmtk.org/redmine/projects/dcmtk/wiki/Howto_Dictionaries

This commit installs dicom.dic and initializes the environment variable.

git-svn-id: http://svn.slicer.org/Slicer4/trunk@24483 3bd1e089-480b-0410-8dfb-8563597acbee

SuperBuild/External_DCMTK.cmake
@@ -63,10 +63,11 @@ if(NOT DEFINED DCMTK_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
       ${${proj}_DEPENDENCIES}
   )
   set(DCMTK_DIR ${CMAKE_BINARY_DIR}/${proj}-build)
-  set(DCM_DICT_PATH ${CMAKE_BINARY_DIR}/${proj}/dcmdata/data/private.dic)
+  set(DCM_PRIVATE_DICT_PATH ${CMAKE_BINARY_DIR}/${proj}/dcmdata/data/private.dic)
+  set(DCM_STANDARD_DICT_PATH ${CMAKE_BINARY_DIR}/${proj}/dcmdata/data/dicom.dic)
 
   mark_as_superbuild(
-    VARS DCM_DICT_PATH:PATH
+    VARS DCM_STANDARD_DICT_PATH:PATH DCM_PRIVATE_DICT_PATH:PATH
     )
 
   #-----------------------------------------------------------------------------
@@ -84,7 +85,8 @@ if(NOT DEFINED DCMTK_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
     )
 
   # environment variables
-  set(${proj}_ENVVARS_LAUNCHER_BUILD "DCMDICTHOME=${DCM_DICT_PATH}")
+  set(${proj}_ENVVARS_LAUNCHER_BUILD
+    "DCMDICTHOME=${DCM_STANDARD_DICT_PATH}<PATHSEP>${DCM_PRIVATE_DICT_PATH}")
   mark_as_superbuild(
     VARS ${proj}_ENVVARS_LAUNCHER_BUILD
     LABELS "ENVVARS_LAUNCHER_BUILD"
@@ -94,7 +96,9 @@ if(NOT DEFINED DCMTK_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
   # Launcher setting specific to install tree
 
   # environment variables
-  set(${proj}_ENVVARS_LAUNCHER_INSTALLED "DCMDICTHOME=<APPLAUNCHER_DIR>/${Slicer_SHARE_DIR}/private.dic")
+  set(${proj}_ENVVARS_LAUNCHER_INSTALLED
+    "DCMDICTHOME=<APPLAUNCHER_DIR>/${Slicer_SHARE_DIR}/dicom.dic<PATHSEP>\
+<APPLAUNCHER_DIR>/${Slicer_SHARE_DIR}/private.dic")
   mark_as_superbuild(
     VARS ${proj}_ENVVARS_LAUNCHER_INSTALLED
     LABELS "ENVVARS_LAUNCHER_INSTALLED"

Base/QTCore/qSlicerCoreApplication.cxx
@@ -218,7 +218,8 @@ void qSlicerCoreApplicationPrivate::init()
 #if defined(Slicer_BUILD_DICOM_SUPPORT) && defined(Q_OS_MAC)
   if (q->isInstalled())
     {
-    this->setEnvironmentVariable("DCMDICTPATH", this->SlicerHome + "/" Slicer_SHARE_DIR "/private.dic");
+    this->setEnvironmentVariable("DCMDICTPATH", this->SlicerHome + "/" Slicer_SHARE_DIR "/dicom.dic:" +
+       this->SlicerHome + "/" Slicer_SHARE_DIR "/private.dic");
     }
 #endif
 

CMake/SlicerBlockInstallDCMTKResources.cmake
@@ -3,7 +3,10 @@
 # -------------------------------------------------------------------------
 
 find_package(DCMTK REQUIRED)
-if(NOT EXISTS ${DCM_DICT_PATH})
-  message(FATAL_ERROR "error: '${DCM_DICT_PATH}' does not exist !")
+if(NOT EXISTS ${DCM_STANDARD_DICT_PATH})
+  message(FATAL_ERROR "error: '${DCM_STANDARD_DICT_PATH}' does not exist !")
 endif()
-install(FILES ${DCM_DICT_PATH} DESTINATION ${Slicer_INSTALL_SHARE_DIR} COMPONENT Runtime)
+if(NOT EXISTS ${DCM_PRIVATE_DICT_PATH})
+  message(FATAL_ERROR "error: '${DCM_PRIVATE_DICT_PATH}' does not exist !")
+endif()
+install(FILES ${DCM_STANDARD_DICT_PATH} ${DCM_PRIVATE_DICT_PATH} DESTINATION ${Slicer_INSTALL_SHARE_DIR} COMPONENT Runtime)

commit 34444d01cbfa159d7d860e15f3000dcc0e971c29

COMP: Fix build error in External_DCMTK.cmake
Reported-by: Erich Bremer <erich.bremer@stonybrook.edu>
Thanks: Steve Pieper <pieper@bwh.harvard.edu>

This commit fixes a regression introduced in r24483 (BUG: fix support
for DCMTK DICOM dictionaries) where a line continuation character
is used. It turns out that this character caused parsing issue with older
version of CMake (< v3.0.0)

//-------------------------------
CMake Error at SuperBuild/External_DCMTK.cmake:99 (set):
   Syntax error in cmake code at

     /home/erich/MyProjects/trunk/SuperBuild/External_DCMTK.cmake:100

   when parsing string

     DCMDICTHOME=<APPLAUNCHER_DIR>/${Slicer_SHARE_DIR}/dicom.dic<PATHSEP>\

   <APPLAUNCHER_DIR>/${Slicer_SHARE_DIR}/private.dic

   syntax error, unexpected cal_SYMBOL, expecting $end (89)
 Call Stack (most recent call first):
   CMake/ExternalProjectDependency.cmake:593 (include)
   SuperBuild/External_ITKv4.cmake:14 (ExternalProject_Include_Dependencies)
   CMake/ExternalProjectDependency.cmake:593 (include)
   CMake/ExternalProjectDependency.cmake:639 (ExternalProject_Include_Dependencies)
   SuperBuild.cmake:323 (ExternalProject_Include_Dependencies)
   CMakeLists.txt:670 (include)
//-------------------------------

SuperBuild/External_DCMTK.cmake
@@ -97,8 +97,7 @@ if(NOT DEFINED DCMTK_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
 
   # environment variables
   set(${proj}_ENVVARS_LAUNCHER_INSTALLED
-    "DCMDICTHOME=<APPLAUNCHER_DIR>/${Slicer_SHARE_DIR}/dicom.dic<PATHSEP>\
-<APPLAUNCHER_DIR>/${Slicer_SHARE_DIR}/private.dic")
+    "DCMDICTHOME=<APPLAUNCHER_DIR>/${Slicer_SHARE_DIR}/dicom.dic<PATHSEP><APPLAUNCHER_DIR>/${Slicer_SHARE_DIR}/private.dic")
   mark_as_superbuild(
     VARS ${proj}_ENVVARS_LAUNCHER_INSTALLED
     LABELS "ENVVARS_LAUNCHER_INSTALLED"