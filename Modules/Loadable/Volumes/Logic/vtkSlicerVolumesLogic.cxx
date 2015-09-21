Slicer/SlicerGitSVNArchive

commit 9fe70aaf70e7e24081c20e7eff7fba236eed5108

BUG: avoid false positives on geometry check
Add an epsilon value to the volumes logic and use it in the fuzzy compare instead
of the VTK default value. Add a precision calculation from the epsilon so that
any warning messages are printed out with the same precision as was used to
check the values.
Added a sanity check to see if the smallest volume spacing is within 3-10 orders
of magnitude of epsilon and print a warning if not.
Use a copy of the CTK significant decimals utility method to calculate the
precision with which to display differences in compared volumes.
Use a copy of the CTK order of magnitude utility method to compare the
spacing of the volumes with the current epsilon being used to
check them.
To avoid CTK includes which pull in a dependency on Qt, rehome the CTK
core utility methods here in the anonymous namespace until they get ported
to VTK. Reimplemented significantDecimals and orderOfMagnitude using standard
strings and stringstreams rather than Qt strings.
Added a python test to exercise the epsilon, precision and geometry check.

TBD: GUI for setting epsilon or just always use a volume spacing based value.

git-svn-id: http://svn.slicer.org/Slicer4/trunk@23934 3bd1e089-480b-0410-8dfb-8563597acbee

Modules/Loadable/Volumes/Logic/vtkSlicerVolumesLogic.cxx
@@ -53,12 +53,128 @@
 #include <vtkImageReslice.h>
 #include <vtkTransform.h>
 
-
-
+/// CTK includes
+/// to avoid CTK includes which pull in a dependency on Qt, rehome some CTK
+/// core utility methods here in the anonymous namespace until they get ported
+/// to VTK
 
 //----------------------------------------------------------------------------
 namespace
 {
+/// Return a "smart" number of decimals needed to display (in a gui) a floating
+/// number. 16 is the max that can be returned, -1 for NaN numbers. When the
+/// number of decimals is not obvious, it defaults to defaultDecimals if it is
+/// different from -1, 16 otherwise.
+int significantDecimals(double value, int defaultDecimals = -1)
+{
+  if (value == 0.
+      || fabs(value) == std::numeric_limits<double>::infinity())
+    {
+    return 0;
+    }
+  if (value != value) // is NaN
+    {
+    return -1;
+    }
+  std::string number;
+  std::stringstream numberStream;
+  numberStream << std::setprecision(16);
+  numberStream << std::fixed << value;
+  number = numberStream.str();
+  size_t decimalPos = number.find_last_of('.');
+  std::string fractional = number.substr(decimalPos + 1);
+  if (fractional.length() != 16)
+    {
+    return -1;
+    }
+  char previous;
+  int previousRepeat=0;
+  bool only0s = true;
+  bool isUnit = value > -1. && value < 1.;
+  for (int i = 0; i < fractional.length(); ++i)
+    {
+    char digit = fractional.at(i);
+    if (digit != '0')
+      {
+      only0s = false;
+      }
+    // Has the digit been repeated too many times ?
+    if (digit == previous && previousRepeat == 2 &&
+        !only0s)
+      {
+      if (digit == '0' || digit == '9')
+        {
+        return i - previousRepeat;
+        }
+      return i;
+      }
+    // Last digit
+    if (i == fractional.length() - 1)
+      {
+      // If we are here, that means that the right number of significant
+      // decimals for the number has not been figured out yet.
+      if (previousRepeat > 2 && !(only0s && isUnit) )
+        {
+        return i - previousRepeat;
+        }
+      // If defaultDecimals has been provided, just use it.
+      if (defaultDecimals >= 0)
+        {
+        return defaultDecimals;
+        }
+      return fractional.length();
+      }
+    // get ready for next
+    if (previous != digit)
+      {
+      previous = digit;
+      previousRepeat = 1;
+      }
+    else
+      {
+      ++previousRepeat;
+      }
+    }
+  return -1;
+//  return fractional.length();
+};
+
+/// Return the order of magnitude of a number or numeric_limits<int>::min() if
+/// the order of magnitude can't be computed (e.g. 0, inf, Nan, denorm)...
+int orderOfMagnitude(double value)
+{
+  value = fabs(value);
+  if (value == 0.
+      || value == std::numeric_limits<double>::infinity()
+      || value != value // is NaN
+      || value < std::numeric_limits<double>::epsilon() // is tool small to compute
+  )
+    {
+    return std::numeric_limits<int>::min();
+    }
+  double magnitude = 1.00000000000000001;
+  int magnitudeOrder = 0;
+
+  int magnitudeStep = 1;
+  double magnitudeFactor = 10;
+
+  if (value < 1.)
+    {
+    magnitudeOrder = -1;
+    magnitudeStep = -1;
+    magnitudeFactor = 0.1;
+    }
+
+  double epsilon = std::numeric_limits<double>::epsilon();
+  while ( (magnitudeStep > 0 && value >= magnitude) ||
+          (magnitudeStep < 0 && value < magnitude - epsilon))
+    {
+    magnitude *= magnitudeFactor;
+    magnitudeOrder += magnitudeStep;
+    }
+  // we went 1 order too far, so decrement it
+  return magnitudeOrder - magnitudeStep;
+};
 
 //----------------------------------------------------------------------------
 class vtkSlicerErrorSink : public vtkCallbackCommand
@@ -350,6 +466,9 @@ vtkSlicerVolumesLogic::vtkSlicerVolumesLogic()
   this->RegisterArchetypeVolumeNodeSetFactory( ArchetypeVectorVolumeNodeSetFactory );
   this->RegisterArchetypeVolumeNodeSetFactory( LabelMapVolumeNodeSetFactory );
   this->RegisterArchetypeVolumeNodeSetFactory( ScalarVolumeNodeSetFactory );
+
+  this->SetCompareVolumeGeometryEpsilon(0.000001);
+
 }
 
 //----------------------------------------------------------------------------
@@ -907,12 +1026,36 @@ vtkSlicerVolumesLogic::CheckForLabelVolumeValidity(vtkMRMLScalarVolumeNode *volu
 }
 
 //----------------------------------------------------------------------------
+void vtkSlicerVolumesLogic::SetCompareVolumeGeometryEpsilon(double epsilon)
+{
+  vtkDebugMacro("vtkSlicerVolumesLogic setting "
+                << " CompareVolumeGeometryEpsilon to " << epsilon);
+
+  double positiveEpsilon = epsilon;
+  // check for negative values
+  if (positiveEpsilon < 0.0)
+    {
+    positiveEpsilon = fabs(epsilon);
+    }
+
+  if (this->CompareVolumeGeometryEpsilon != positiveEpsilon)
+    {
+    this->CompareVolumeGeometryEpsilon = positiveEpsilon;
+
+    // now set the precision
+    this->CompareVolumeGeometryPrecision = significantDecimals(this->CompareVolumeGeometryEpsilon);
+
+    this->Modified();
+    }
+}
+
+//----------------------------------------------------------------------------
 std::string
 vtkSlicerVolumesLogic::CompareVolumeGeometry(vtkMRMLScalarVolumeNode *volumeNode1,
                                              vtkMRMLScalarVolumeNode *volumeNode2)
 {
   std::stringstream warnings;
-  warnings << "";
+
   if (!volumeNode1 || !volumeNode2)
     {
     if (!volumeNode1)
@@ -943,6 +1086,37 @@ vtkSlicerVolumesLogic::CompareVolumeGeometry(vtkMRMLScalarVolumeNode *volumeNode
       {
       int row, column;
       double volumeValue1, volumeValue2;
+      // set the floating point precision to match the precision of the espilon
+      // used for the fuzzy compare
+      warnings << std::setprecision(this->GetCompareVolumeGeometryPrecision());
+      // sanity check versus the volume spacings
+      double spacing1[3], spacing2[3];
+      volumeNode1->GetSpacing(spacing1);
+      volumeNode2->GetSpacing(spacing2);
+      double minSpacing = spacing1[0];
+      for (int i = 1; i < 3; ++i)
+        {
+        if (spacing1[i] < minSpacing)
+          {
+          minSpacing = spacing1[i];
+          }
+        }
+      for (int i = 0; i < 3; ++i)
+        {
+        if (spacing2[i] < minSpacing)
+          {
+          minSpacing = spacing2[i];
+          }
+        }
+      // in general the defaults assume that an epsilon of 1e-6 works with a min
+      // spacing of 1mm, check that the epsilon is scaled appropriately for the
+      // minimum spacing for these two volumes
+      double logDiff = orderOfMagnitude(minSpacing) - orderOfMagnitude(this->CompareVolumeGeometryEpsilon);
+      vtkDebugMacro("diff in order of mag between min spacing and epsilon = " << logDiff);
+      if (logDiff < 3.0 || logDiff > 10.0)
+        {
+        warnings << "(Minimum spacing for volumes of " << minSpacing << " mismatched with epsilon " << this->CompareVolumeGeometryEpsilon << ",\ngeometry comparison may not be useful.\nTry resetting the Volumes module logic compare volume geometry epsilon variable.)\n";
+        }
       for (row = 0; row < 3; row++)
         {
         volumeValue1 = volumeImage1->GetDimensions()[row];
@@ -978,9 +1152,12 @@ vtkSlicerVolumesLogic::CompareVolumeGeometry(vtkMRMLScalarVolumeNode *volumeNode
           {
           volumeValue1 = volumeIJKToRAS1->GetElement(row,column);
           volumeValue2 = volumeIJKToRAS2->GetElement(row,column);
-          if (!vtkMathUtilities::FuzzyCompare<double>(volumeValue1, volumeValue2))
+          if (!vtkMathUtilities::FuzzyCompare<double>(volumeValue1,
+                                                      volumeValue2,
+                                                      this->CompareVolumeGeometryEpsilon))
             {
-            warnings << "IJKToRAS mismatch at [" << row << ", " << column << "] (" << volumeValue1 << " != " << volumeValue2 << ")\n";
+            warnings << "IJKToRAS mismatch at [" << row << ", " << column << "] ("
+                     << volumeValue1 << " != " << volumeValue2 << ")\n";
             }
           }
         }
@@ -1174,6 +1351,10 @@ void vtkSlicerVolumesLogic::PrintSelf(ostream& os, vtkIndent indent)
 
   os << indent << "ActiveVolumeNode: " <<
     (this->ActiveVolumeNode ? this->ActiveVolumeNode->GetName() : "(none)") << "\n";
+  os << indent << "CompareVolumeGeometryEpsilon: "
+     << this->CompareVolumeGeometryEpsilon << "\n";
+  os << indent << "CompareVolumeGeometryPrecision: "
+     << this->CompareVolumeGeometryPrecision << "\n";
 }
 
 //----------------------------------------------------------------------------

Modules/Loadable/Volumes/Testing/Python/VolumesLogicCompareVolumeGeometry.py
@@ -1,0 +1,118 @@
+import unittest
+from  __main__ import vtk, qt, ctk, slicer
+
+
+class VolumesLogicCompareVolumeGeometryTesting(unittest.TestCase):
+  def setUp(self):
+    pass
+
+  def test_VolumesLogicCompareVolumeGeometry(self):
+    """
+    Load a volume, then call the compare volume geometry test with
+    different values of epsilon and precision.
+    """
+
+    #
+    # first, get some sample data
+    #
+    import SampleData
+    sampleDataLogic = SampleData.SampleDataLogic()
+    head = sampleDataLogic.downloadMRHead()
+
+    #
+    # get the volumes logic and print out default epsilon and precision
+    #
+    volumesLogic = slicer.modules.volumes.logic()
+    print 'Compare volume geometry epsilon: ', volumesLogic.GetCompareVolumeGeometryEpsilon()
+    print 'Compare volume geometry precision: ', volumesLogic.GetCompareVolumeGeometryPrecision()
+
+    #
+    # compare the head against itself, this shouldn't produce any warning
+    # string
+    #
+    warningString = volumesLogic.CompareVolumeGeometry(head, head)
+    if len(warningString) != 0:
+      print 'Error in checking MRHead geometry against itself'
+      print warningString
+      return False
+    else:
+      print 'Success in comparing MRHead vs itself with epsilon',volumesLogic.GetCompareVolumeGeometryEpsilon()
+
+    #
+    # see if you can get it to fail with a tighter epsilon
+    #
+    volumesLogic.SetCompareVolumeGeometryEpsilon(1e-10)
+    precision = volumesLogic.GetCompareVolumeGeometryPrecision()
+    if precision != 10:
+      print 'Error in calculating precision from epsilon of ', volumesLogic.GetCompareVolumeGeometryEpsilon(), ', expected 10, got ', precision
+      return False
+    warningString = volumesLogic.CompareVolumeGeometry(head, head)
+    if len(warningString) != 0:
+      print 'Error in checking MRHead geometry against itself with strict epsilon'
+      print warningString
+      return False
+    else:
+      print 'Success in comparing MRHead vs itself with epsilon',volumesLogic.GetCompareVolumeGeometryEpsilon()
+
+
+
+    #
+    # clone the volume so can test for mismatches in geometry with
+    # that operation
+    #
+    head2 = volumesLogic.CloneVolume(head, 'head2')
+
+    warningString  = volumesLogic.CompareVolumeGeometry(head, head2)
+    if len(warningString) != 0:
+      print 'Error in checking MRHead geometry against itself with epsilon ', volumesLogic.GetCompareVolumeGeometryEpsilon()
+      print warningString
+      return False
+    else:
+      print 'Success in comparing MRHead vs clone with epsilon',volumesLogic.GetCompareVolumeGeometryEpsilon()
+
+    #
+    # now try with a label map volume
+    #
+    headLabel = volumesLogic.CreateAndAddLabelVolume(head, "label vol")
+    warningString = volumesLogic.CompareVolumeGeometry(head, headLabel)
+    if len(warningString) != 0:
+      print 'Error in comparing MRHead geometry against a label map of itself with epsilon',volumesLogic.GetCompareVolumeGeometryEpsilon()
+      print warningString
+      return False
+    else:
+      print 'Success in comparing MRHead vs label map with epsilon',volumesLogic.GetCompareVolumeGeometryEpsilon()
+
+    #
+    # adjust the geometry and make it fail
+    #
+    head2Matrix = vtk.vtkMatrix4x4()
+    head2.GetRASToIJKMatrix(head2Matrix)
+    val = head2Matrix.GetElement(2,0)
+    head2Matrix.SetElement(2,0,val+0.25)
+    head2.SetRASToIJKMatrix(head2Matrix)
+    head2.SetSpacing(0.12345678901234567890, 2.0, 3.4)
+
+    warningString = volumesLogic.CompareVolumeGeometry(head,head2)
+    if len(warningString) == 0:
+      print 'Error in comparing MRHead geometry against an updated clone, with epsilon',volumesLogic.GetCompareVolumeGeometryEpsilon()
+      return False
+    else:
+      print 'Success in making the comparison fail, with with epsilon',volumesLogic.GetCompareVolumeGeometryEpsilon()
+      print warningString
+
+    #
+    # reset the epsilon with an invalid negative number
+    #
+    volumesLogic.SetCompareVolumeGeometryEpsilon(-0.01)
+    epsilon = volumesLogic.GetCompareVolumeGeometryEpsilon()
+    if epsilon != 0.01:
+      print 'Failed to use the absolute value for an epsilon of -0.01: ', epsilon
+      return False
+    precision = volumesLogic.GetCompareVolumeGeometryPrecision()
+    if precision != 2:
+      print 'Failed to set the precision to 2: ',precision
+      return False
+    warningString = volumesLogic.CompareVolumeGeometry(head,head2)
+    print warningString
+
+    return True

Modules/Loadable/Volumes/Logic/vtkSlicerVolumesLogic.h
@@ -242,6 +242,20 @@ public:
   static vtkMRMLScalarVolumeNode* ResampleVolumeToReferenceVolume(vtkMRMLVolumeNode *inputVolumeNode,
                                                            vtkMRMLVolumeNode *referenceVolumeNode);
 
+  /// Getting the epsilon value to use when determining if the
+  /// elements of the IJK to RAS matrices of two volumes match.
+  /// Defaults to 10 to the minus 6.
+  vtkGetMacro(CompareVolumeGeometryEpsilon, double);
+  /// Setting the epsilon value and associated precision to use when determining
+  /// if the elements of the IJK to RAS matrices of two volumes match and how to
+  /// print out the mismatched elements.
+  void SetCompareVolumeGeometryEpsilon(double epsilon);
+
+  /// Get the precision with which to print out volume geometry mismatches,
+  /// value is set when setting the compare volume geometry epsilon.
+  /// \sa SetCompareVolumeGeometryEpsilon
+  vtkGetMacro(CompareVolumeGeometryPrecision, int);
+
 protected:
   vtkSlicerVolumesLogic();
   virtual ~vtkSlicerVolumesLogic();
@@ -273,6 +287,13 @@ protected:
   vtkSmartPointer<vtkMRMLColorLogic> ColorLogic;
 
   NodeSetFactoryRegistry VolumeRegistry;
+
+  /// Allowable difference in comparing volume geometry double values.
+  /// Defaults to 1 to the power of 10 to the minus 6
+  double CompareVolumeGeometryEpsilon;
+  /// Error print out precision, paried with CompareVolumeGeometryEpsilon.
+  /// defaults to 6
+  int CompareVolumeGeometryPrecision;
 };
 
 #endif

Modules/Loadable/Volumes/Testing/Python/CMakeLists.txt
@@ -2,3 +2,5 @@
 #  SLICER_ARGS --disable-cli-modules)
 
 slicer_add_python_unittest(SCRIPT LoadVolumeDisplaybleSceneModelClose.py)
+
+slicer_add_python_unittest(SLICER_ARGS --disable-cli-modules SCRIPT VolumesLogicCompareVolumeGeometry.py)

commit 4456398293e3f391bf0ce802f60cd91a93616fb9

BUG: Fix unitialized variable that causes py_LabelStatistics test failure
In a release build running the py_LabelStatistics test intermittently results in
a hang or memory corruption.

This commit fixes the problem by initializing a member variable in
vtkSlicerVolumesLogic. The member variable, and therefore the regression, was
introduced in r23934 ("BUG: avoid false positives on geometry check"). The
initial value matches what's described in the member variable's comment.

The unit test is updated to check the initial value of the member variable.

Modules/Loadable/Volumes/Logic/vtkSlicerVolumesLogic.cxx
@@ -471,6 +471,7 @@ vtkSlicerVolumesLogic::vtkSlicerVolumesLogic()
   this->RegisterArchetypeVolumeNodeSetFactory( ScalarVolumeNodeSetFactory );
 
   this->CompareVolumeGeometryEpsilon = 0.000001;
+  this->CompareVolumeGeometryPrecision = 6;
 }
 
 //----------------------------------------------------------------------------

Modules/Loadable/Volumes/Testing/Python/VolumesLogicCompareVolumeGeometry.py
@@ -25,6 +25,8 @@ class VolumesLogicCompareVolumeGeometryTesting(unittest.TestCase):
     volumesLogic = slicer.modules.volumes.logic()
     print 'Compare volume geometry epsilon: ', volumesLogic.GetCompareVolumeGeometryEpsilon()
     print 'Compare volume geometry precision: ', volumesLogic.GetCompareVolumeGeometryPrecision()
+    self.assertAlmostEqual(volumesLogic.GetCompareVolumeGeometryEpsilon(), 1e-6)
+    self.assertEqual(volumesLogic.GetCompareVolumeGeometryPrecision(), 6)
 
     #
     # compare the head against itself, this shouldn't produce any warning