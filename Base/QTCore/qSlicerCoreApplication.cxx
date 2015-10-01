Slicer/SlicerGitSVNArchive

commit 3c0c211a290f499de67f05ed42ec6f7ca2e4bd46

ENH: Update DMCTK to use version DCMTK-3.6.1_20150924
The important improvements provided by the new version of DCMTK
is the support for new API that in particular greatly simplify creation of and
interaction with DICOM Segmentation image objects.

More specifically, it introduces the dcmiod, dcmfg and dcmseg classes
helpful in creating multiframe DICOM objects, such as segmentation and
parametric map objects.

Considering that standard and private dictionaries can now be  embedded
in DCMTK library, this commit partially reverts r24473 (ENH: Add support
for DCMTK private dictionary). The test DCMTKPrivateDictTest.py was updated to
not expect DCMDICTPATH environment variable that is no longer needed and is not
initialized.

List of DCMTK changes:

$ git shortlog 3366181..DCMTK-3.6.1_20150924 --no-merges

Andreas Thiel (1):
      Updated man pages for new development snapshot.

Jan Schlamelcher (86):
      Some workarounds for VC6 compiler peculiarity.
      Introduced OFunique_ptr as a fallback if std::unique_ptr is not available.
      Make use of  unique pointer (free memory if C-MOVE/C-FIND is canceled).
      Fixed dll build on windows.
      Correction of inconsistent previous commit.
      Fixed DcmSCPPool test case failing on older systems.
      Enabled shutdown functionality of DcmSCPPool.
      Fixed building with mingw-w64.
      Fixed most of the warnings in dcmjpeg.
      Fixed potential bug in dcmjpeg 12 and 16 bit variants.
      Marked SCPPool-Test (dcmnet_scp_pool) as exhaustive.
      Fixed most of the warnings in dcmdata.
      Fixed a mistake in CHANGES.361.
      Minor fixes in dcmdata.
      Added support for some C++11 types, literals and traits.
      Added documentation for the new macro "DOXYGEN".
      Fixed most of the warnings in ofstd and oflog.
      Added support for some additional C++11 traits.
      Introduced the new explicit boolean type OFExplicitBool.
      Fixed possibly wrong overload resolution in DSRCodedEntryValue.

Base/QTCore/qSlicerCoreApplication.cxx
@@ -215,13 +215,6 @@ void qSlicerCoreApplicationPrivate::init()
   this->setEnvironmentVariable("ITK_AUTOLOAD_PATH", this->ITKFactoriesDir);
   this->setPythonEnvironmentVariables();
   this->setTclEnvironmentVariables();
-#if defined(Slicer_BUILD_DICOM_SUPPORT) && defined(Q_OS_MAC)
-  if (q->isInstalled())
-    {
-    this->setEnvironmentVariable("DCMDICTPATH", this->SlicerHome + "/" Slicer_SHARE_DIR "/dicom.dic:" +
-       this->SlicerHome + "/" Slicer_SHARE_DIR "/private.dic");
-    }
-#endif
 
   // Load default settings if any.
   if (q->defaultSettings())

Applications/SlicerApp/Testing/Python/DCMTKPrivateDictTest.py
@@ -2,12 +2,6 @@ import DICOMLib, sys, slicer, os
 
 dcmfile = sys.argv[1]
 
-print('DCMDICTPATH is'),
-try:
-  print(os.environ['DCMDICTPATH'])
-except KeyError:
-  raise Exception("DCMDICTPATH environment variable is not defined !")
-
 dcmdump=DICOMLib.DICOMCommand('dcmdump',[dcmfile])
 dump=str(dcmdump.start()).splitlines()
 

CMake/SlicerBlockInstallDCMTKResources.cmake
@@ -1,12 +1,0 @@
-# -------------------------------------------------------------------------
-# Find and install DCMTK private dictionary
-# -------------------------------------------------------------------------
-
-find_package(DCMTK REQUIRED)
-if(NOT EXISTS ${DCM_STANDARD_DICT_PATH})
-  message(FATAL_ERROR "error: '${DCM_STANDARD_DICT_PATH}' does not exist !")
-endif()
-if(NOT EXISTS ${DCM_PRIVATE_DICT_PATH})
-  message(FATAL_ERROR "error: '${DCM_PRIVATE_DICT_PATH}' does not exist !")
-endif()
-install(FILES ${DCM_STANDARD_DICT_PATH} ${DCM_PRIVATE_DICT_PATH} DESTINATION ${Slicer_INSTALL_SHARE_DIR} COMPONENT Runtime)

CMake/SlicerCPack.cmake
@@ -15,7 +15,6 @@ endif()
 
 if(Slicer_BUILD_DICOM_SUPPORT AND NOT Slicer_USE_SYSTEM_DCMTK)
   include(${Slicer_CMAKE_DIR}/SlicerBlockInstallDCMTKApps.cmake)
-  include(${Slicer_CMAKE_DIR}/SlicerBlockInstallDCMTKResources.cmake)
 endif()
 
 set(CPACK_INSTALL_CMAKE_PROJECTS)

SuperBuild/External_DCMTK.cmake
@@ -33,8 +33,8 @@ if(NOT DEFINED DCMTK_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
       )
   endif()
 
-  set(${proj}_REPOSITORY ${git_protocol}://github.com/commontk/DCMTK.git)
-  set(${proj}_GIT_TAG "3366181db75ac0e334045c66350e438adf304cb0")
+  set(${proj}_REPOSITORY ${git_protocol}://git.dcmtk.org/dcmtk)
+  set(${proj}_GIT_TAG "DCMTK-3.6.1_20150924")
 
   ExternalProject_Add(${proj}
     ${${proj}_EP_ARGS}
@@ -57,18 +57,14 @@ if(NOT DEFINED DCMTK_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
       -DDCMTK_WITH_XML:BOOL=OFF  # see CTK github issue #25
       -DDCMTK_WITH_ICONV:BOOL=OFF  # see CTK github issue #178
       -DDCMTK_OVERWRITE_WIN32_COMPILER_FLAGS:BOOL=OFF
+      -DDCMTK_ENABLE_BUILTIN_DICTIONARY:BOOL=ON
+      -DDCMTK_ENABLE_PRIVATE_TAGS:BOOL=ON
       ${EXTERNAL_PROJECT_OPTIONAL_ARGS}
     INSTALL_COMMAND ""
     DEPENDS
       ${${proj}_DEPENDENCIES}
   )
   set(DCMTK_DIR ${CMAKE_BINARY_DIR}/${proj}-build)
-  set(DCM_PRIVATE_DICT_PATH ${CMAKE_BINARY_DIR}/${proj}/dcmdata/data/private.dic)
-  set(DCM_STANDARD_DICT_PATH ${CMAKE_BINARY_DIR}/${proj}/dcmdata/data/dicom.dic)
-
-  mark_as_superbuild(
-    VARS DCM_STANDARD_DICT_PATH:PATH DCM_PRIVATE_DICT_PATH:PATH
-    )
 
   #-----------------------------------------------------------------------------
   # Launcher setting specific to build tree
@@ -83,26 +79,6 @@ if(NOT DEFINED DCMTK_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
     VARS ${proj}_LIBRARY_PATHS_LAUNCHER_BUILD
     LABELS "LIBRARY_PATHS_LAUNCHER_BUILD"
     )
-
-  # environment variables
-  set(${proj}_ENVVARS_LAUNCHER_BUILD
-    "DCMDICTPATH=${DCM_STANDARD_DICT_PATH}<PATHSEP>${DCM_PRIVATE_DICT_PATH}")
-  mark_as_superbuild(
-    VARS ${proj}_ENVVARS_LAUNCHER_BUILD
-    LABELS "ENVVARS_LAUNCHER_BUILD"
-    )
-
-  #-----------------------------------------------------------------------------
-  # Launcher setting specific to install tree
-
-  # environment variables
-  set(${proj}_ENVVARS_LAUNCHER_INSTALLED
-    "DCMDICTPATH=<APPLAUNCHER_DIR>/${Slicer_SHARE_DIR}/dicom.dic<PATHSEP><APPLAUNCHER_DIR>/${Slicer_SHARE_DIR}/private.dic")
-  mark_as_superbuild(
-    VARS ${proj}_ENVVARS_LAUNCHER_INSTALLED
-    LABELS "ENVVARS_LAUNCHER_INSTALLED"
-    )
-
 
 else()
   ExternalProject_Add_Empty(${proj} DEPENDS ${${proj}_DEPENDENCIES})

commit 40aca4328ac45eb1015a1851bdc7f480d5e06880

COMP: Update Slicer and CTK to fix DCMTK windows build error.
This commit fixes windows build error and redefined warnings introduced
by the commit r24586 (ENH: Update DMCTK to use version DCMTK-3.6.1_20150924)

Related commits:
 * r24136 (STYLE: Update CTK to fix warning related to wrapping of CTKDICOM module)
 * r23955 (COMP: Workaround to fix HAVE_STAT redefined warning in SlicerDICOMExportDialog)
 * r23952 (COMP: Workaround to fix HAVE_STAT redefined warning in qSlicerCoreApplication)

CTK updates:

$ git shortlog 7199173..6453c27 --no-merges
Jean-Christophe Fillion-Robin (2):
      Workaround refined macro warning when building CTKDICOMWidgetsPythonQt
      Fix CTKDICOMWidgetsPythonQt windows build error undefining HAVE_INT64_T

Reported-by: Andriy Fedorov <fedorov@bwh.harvard.edu>
Thanks: Andras Lasso <lasso@queensu.ca>
Thanks: Jörg Riesmeier
Thanks: Michael Onken

git-svn-id: http://svn.slicer.org/Slicer4/trunk@24588 3bd1e089-480b-0410-8dfb-8563597acbee

Base/QTCore/qSlicerCoreApplication.cxx
@@ -98,8 +98,18 @@
 #include "vtkSlicerVersionConfigure.h" // For Slicer_VERSION_{MINOR, MAJOR}, Slicer_VERSION_FULL
 
 #ifdef Slicer_BUILD_DICOM_SUPPORT
-// XXX Avoid  warning: "HAVE_STAT" redefined
+// XXX Avoid  warning: "HAVE_XXXX" redefined
 #undef HAVE_STAT
+#undef HAVE_FTIME
+#undef HAVE_GETPID
+#undef HAVE_IO_H
+#undef HAVE_STRERROR
+#undef HAVE_SYS_UTIME_H
+#undef HAVE_TEMPNAM
+#undef HAVE_TMPNAM
+#undef HAVE_LONG_LONG
+// XXX Fix windows build error
+#undef HAVE_INT64_T
 #include <ctkDICOMDatabase.h>
 #endif
 

Modules/Scripted/DICOMLib/Widgets/qSlicerDICOMExportDialog.cxx
@@ -53,8 +53,18 @@
 #include "qSlicerApplication.h"
 
 // CTK includes
-// XXX Avoid  warning: "HAVE_STAT" redefined
+// XXX Avoid  warning: "HAVE_XXXX" redefined
 #undef HAVE_STAT
+#undef HAVE_FTIME
+#undef HAVE_GETPID
+#undef HAVE_IO_H
+#undef HAVE_STRERROR
+#undef HAVE_SYS_UTIME_H
+#undef HAVE_TEMPNAM
+#undef HAVE_TMPNAM
+#undef HAVE_LONG_LONG
+// XXX Fix windows build error
+#undef HAVE_INT64_T
 #include "ctkDICOMDatabase.h"
 #include "ctkDICOMIndexer.h"
 

SuperBuild/External_CTK.cmake
@@ -58,7 +58,7 @@ if(NOT DEFINED CTK_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
   ExternalProject_Add(${proj}
     ${${proj}_EP_ARGS}
     GIT_REPOSITORY "${git_protocol}://github.com/commontk/CTK.git"
-    GIT_TAG "71991737acc831dd5533c708d0139624dde2dc96"
+    GIT_TAG "6453c27f5ba25c06a2b914bb71d1d3376b45afec"
     SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
     BINARY_DIR ${proj}-build
     CMAKE_CACHE_ARGS