Slicer/SlicerGitSVNArchive

commit 6a4e5263bdc4b7549e624b9d57f2376709ac9451

BUG: Update BRAINSTools to include fixes for BRAINSFit and BRAINSResample.
This commit fixes a regression introduced
by InsightSoftwareConsortium/ITK@d1e9fe2 (BUG: Ensure Transform IO
factories are  registered once.) and integrated in r24836 (ENH: Update
ITKv4 to v4.9.0 release branch).

Following this ITK commit, the ITKIO transform libraries are build as
shared and dynamic_cast can NOT be used with templated classes that are
instantiated in a translation unit different than the one where they are
defined. It will work only if the classes are explicitly instantiated
and exported.

To workaround the issue, code that was relying on dynamic_cast is updated
to (1) ensure the objects are of the right type doing string comparison
and (2) then use static_cast instead of dynamic_cast.

This patch fixes the following EMSegment test (internally using
BRAINSFit) when compiled using gcc 4.4.3 and executed on Ubuntu 10.04:

  EMSegCL_Task_BRAINS_MRIHumanBrain_scalartype_float_very_small
  EMSegCL_Task_BRAINS_MRIHumanBrain_scalartype_uint_very_small
  EMSegCL_Task_HumanEye_BRAINS_small
  EMSegCL_Task_MRIHumanBrain_BRAINS_small
  EMSegCL_Task_MRIHumanBrainExp_BRAINS_small
  EMSegCL_Task_MRIHumanBrainFullParcellation_BRAINS_small
  EMSegCL_Task_MRIHumanBrainParcellation_BRAINS_small
  EMSegCL_Task_NonHumanPrimate_BRAINS_small

It fixes runtime error like the following:

//-----------------
 Read ITK transform from file: /tmp/EMSegmentCommandLine/EMS282744605229D1c.h5
 HACK: 0  VersorRigid3DTransform
 Caught an ITK exception:

  itk::ExceptionObject (0x18ef450)
  Location: "unknown"
  File: /path/to/Slicer-0-build/BRAINSTools/BRAINSCommonLib/GenericTransformImage.cxx
  Line: 346
  Description: itk::ERROR: Error in type conversion
//-----------------

Complete list of BRAINSTools updates:

$ git shortlog c658c75..e257e62 --no-merges
Alexander Leinoff (18):
      DOC: Updates build instructions for OS X
      COMP: Updates ITK dependency and Removes Python from build system

SuperBuild.cmake
@@ -218,6 +218,7 @@ set(BRAINSTools_options
   BRAINSTools_CLI_INSTALL_LIBRARY_DESTINATION:PATH=${Slicer_INSTALL_CLIMODULES_LIB_DIR}
   BRAINSTools_CLI_INSTALL_ARCHIVE_DESTINATION:PATH=${Slicer_INSTALL_CLIMODULES_LIB_DIR}
   BRAINSTools_CLI_INSTALL_RUNTIME_DESTINATION:PATH=${Slicer_INSTALL_CLIMODULES_BIN_DIR}
+  BRAINSTools_DISABLE_TESTING:BOOL=ON
   USE_BRAINSFit:BOOL=ON
   USE_BRAINSROIAuto:BOOL=ON
   USE_BRAINSResample:BOOL=ON
@@ -251,7 +252,7 @@ set(BRAINSTools_options
   )
 Slicer_Remote_Add(BRAINSTools
   GIT_REPOSITORY "${git_protocol}://github.com/Slicer/BRAINSTools.git"
-  GIT_TAG "c658c752a053ab2006929489d5f0e9297857ba18"  # v4.5.0 + patch of Csaba + patch for ITK ExternalModule
+  GIT_TAG "e257e62c045317a754ea63753daaeed2a8452e85"  # post-v4.6.0
   OPTION_NAME Slicer_BUILD_BRAINSTOOLS
   OPTION_DEPENDS "Slicer_BUILD_CLI_SUPPORT;Slicer_BUILD_CLI"
   LABELS REMOTE_MODULE

commit 163e1c787b9fb5fb069b52fbc8e8cf444c971d10

COMP: Update BRAINSTools to fix MacOSX and Linux packaging.
This commit fixes a regression originally introduced in r24881 (BUG: Update
BRAINSTools to include fixes for BRAINSFit and BRAINSResample.)

Fixes #4135

//---------------------------------------------
commit Slicer/BRAINSTools@1a8e84b
Author: Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
Date:   Sat Jan 23 05:06:52 2016 -0500

    COMP: Force CMAKE_CONFIGURATION_TYPES only when using multi-config generator

    This commit fixes a regression first introduced in BRAINSia/BRAINSTools@831eab6 (ENH: Allow
    selecting C++11 from CMAKE_CXX_STANDARD) and incorrectly fixed
    in BRAINSia/BRAINSTools@8987b5d (BUG: Do not dereference a variable when checking for existance.)

    CMAKE_CONFIGURATION_TYPES variable is now forced to a value only
    if it has been defined by CMake. This ensures that project (e.g Slicer)
    directly adding BRAINSTools source tree can still rely on the
    assumption CMAKE_CONFIGURATION_TYPES is set only when multi-configuration
    generator are used.
//---------------------------------------------

git-svn-id: http://svn.slicer.org/Slicer4/trunk@24889 3bd1e089-480b-0410-8dfb-8563597acbee

SuperBuild.cmake
@@ -252,7 +252,7 @@ set(BRAINSTools_options
   )
 Slicer_Remote_Add(BRAINSTools
   GIT_REPOSITORY "${git_protocol}://github.com/Slicer/BRAINSTools.git"
-  GIT_TAG "e257e62c045317a754ea63753daaeed2a8452e85"  # post-v4.6.0
+  GIT_TAG "1a8e84b5b850a9f08338b23d80c18e459e0b01a1"  # post-v4.6.0
   OPTION_NAME Slicer_BUILD_BRAINSTOOLS
   OPTION_DEPENDS "Slicer_BUILD_CLI_SUPPORT;Slicer_BUILD_CLI"
   LABELS REMOTE_MODULE