Slicer/SlicerGitSVNArchive

commit 12743c6fcebb67ff9b22319cbe52a322a3738134

BUG: Fix transform tests by not using dynamic_cast in vtkITKTransformConverter
This commit fixes a regression introduced
by InsightSoftwareConsortium/ITK@d1e9fe2 (BUG: Ensure Transform IO
factories are  registered once.) and integrated in r24836 (ENH: Update
ITKv4 to v4.9.0 release branch).

Following this ITK commit, the ITKIO transform libraries are build as
shared and dynamic_cast can NOT be used with templated classes that are
instantiated in a translation unit different than the one where they are
defined. It will work only if the classes are explicitly instantiated
and exported.

To workaround the issue, code that was relying on dynamic_cast is updated
to (1) ensure the objects are of the right type doing string comparison
and (2) then use static_cast instead of dynamic_cast.

See InsightSoftwareConsortium/ITK@d1e9fe2
and see http://stackoverflow.com/questions/8024010/why-do-template-class-functions-have-to-be-declared-in-the-same-translation-unit

This commit fixes the following tests:
  vtkSlicerTransformLogicTest1
  vtkMRMLNonlinearTransformNodeTest1

git-svn-id: http://svn.slicer.org/Slicer4/trunk@24875 3bd1e089-480b-0410-8dfb-8563597acbee

Libs/MRML/Core/vtkITKTransformConverter.h
@@ -138,7 +138,6 @@ bool vtkITKTransformConverter::SetVTKLinearTransformFromITK(
 {
   static const unsigned int D = VTKDimension;
   typedef itk::MatrixOffsetTransformBase<T,D,D> LinearTransformType;
-  typedef itk::IdentityTransform<T, D> IdentityTransformType;
   typedef itk::ScaleTransform<T, D> ScaleTransformType;
   typedef itk::TranslationTransform<T, D> TranslateTransformType;
 
@@ -146,11 +145,15 @@ bool vtkITKTransformConverter::SetVTKLinearTransformFromITK(
 
   bool convertedToVtkMatrix=false;
 
+  std::string itkTransformClassName = transformItk_LPS->GetNameOfClass();
+
   // Linear transform of doubles or floats, dimension 3
-  typename LinearTransformType::Pointer dlt
-    = dynamic_cast<LinearTransformType*>( transformItk_LPS.GetPointer() );
-  if (dlt)
+  if (itkTransformClassName == "AffineTransform" ||
+      itkTransformClassName == "Rigid3DTransform" ||
+      itkTransformClassName == "ScaleTransform")
     {
+    typename LinearTransformType::Pointer dlt
+      = static_cast<LinearTransformType*>( transformItk_LPS.GetPointer() );
     convertedToVtkMatrix=true;
     for (unsigned int i=0; i < D; i++)
       {
@@ -163,19 +166,17 @@ bool vtkITKTransformConverter::SetVTKLinearTransformFromITK(
     }
 
   // Identity transform of doubles or floats, dimension 3
-  typename IdentityTransformType::Pointer dit
-    = dynamic_cast<IdentityTransformType*>( transformItk_LPS.GetPointer() );
-  if (dit)
+  if (itkTransformClassName == "IdentityTransform")
     {
     // nothing to do, matrix is already the identity
     convertedToVtkMatrix=true;
     }
 
   // Scale transform of doubles or floats, dimension 3
-  typename ScaleTransformType::Pointer dst
-    = dynamic_cast<ScaleTransformType*>( transformItk_LPS.GetPointer() );
-  if (dst)
+  if (itkTransformClassName == "ScaleTransform")
     {
+    typename ScaleTransformType::Pointer dst
+      = static_cast<ScaleTransformType*>( transformItk_LPS.GetPointer() );
     convertedToVtkMatrix=true;
     for (unsigned int i=0; i < D; i++)
       {
@@ -184,10 +185,10 @@ bool vtkITKTransformConverter::SetVTKLinearTransformFromITK(
     }
 
   // Translate transform of doubles or floats, dimension 3
-  typename TranslateTransformType::Pointer dtt
-    = dynamic_cast<TranslateTransformType*>( transformItk_LPS.GetPointer());
-  if (dtt)
+  if (itkTransformClassName == "TranslationTransform")
     {
+    typename TranslateTransformType::Pointer dtt
+      = static_cast<TranslateTransformType*>( transformItk_LPS.GetPointer());
     convertedToVtkMatrix=true;
     for (unsigned int i=0; i < D; i++)
       {
@@ -440,12 +441,13 @@ template <typename T> bool vtkITKTransformConverter::SetVTKBSplineFromITKv3Gener
   // Set the bulk transform
   if( bulkTransformItk )
     {
+    std::string bulkTransformItkTransformName = bulkTransformItk->GetNameOfClass();
+
     typedef itk::AffineTransform<T,3> BulkTransformType;
-    typedef itk::IdentityTransform<T,3> IdentityBulkTransformType;
-    BulkTransformType* bulkItkAffine = dynamic_cast<BulkTransformType*> (bulkTransformItk.GetPointer());
-    IdentityBulkTransformType* bulkItkIdentity = dynamic_cast<IdentityBulkTransformType*> (bulkTransformItk.GetPointer());
-    if (bulkItkAffine)
+
+    if (bulkTransformItkTransformName == "AffineTransform")
       {
+      BulkTransformType* bulkItkAffine = static_cast<BulkTransformType*> (bulkTransformItk.GetPointer());
       vtkNew<vtkMatrix4x4> bulkMatrix_LPS;
       for (unsigned int i=0; i < VTKDimension; i++)
         {
@@ -466,7 +468,7 @@ template <typename T> bool vtkITKTransformConverter::SetVTKBSplineFromITKv3Gener
       vtkMatrix4x4::Multiply4x4(bulkMatrix_RAS.GetPointer(), rasToLps.GetPointer(), bulkMatrix_RAS.GetPointer());
       bsplineVtk->SetBulkTransformMatrix(bulkMatrix_RAS.GetPointer());
       }
-    else if (bulkItkIdentity)
+    else if (bulkTransformItkTransformName == "IdentityTransform")
       {
       // bulk transform is identity, which is equivalent to no bulk transform
       }
@@ -815,17 +817,25 @@ bool vtkITKTransformConverter::SetVTKOrientedGridTransformFromITK(vtkObject* log
   typedef itk::DisplacementFieldTransform< T, 3 > DisplacementFieldTransformType;
   typedef itk::InverseDisplacementFieldTransform< T, 3 > InverseDisplacementFieldTransformType;
 
-  DisplacementFieldTransformType* displacementFieldTransform = dynamic_cast<DisplacementFieldTransformType*>( transformItk_LPS.GetPointer() );
-  DisplacementFieldTransformType* inverseDisplacementFieldTransform = dynamic_cast<InverseDisplacementFieldTransformType*>( transformItk_LPS.GetPointer() );
+  if (!transformItk_LPS)
+    {
+    vtkErrorWithObjectMacro(loggerObject, "Cannot set VTK oriented grid transform from ITK: the input transform is NULL");
+    return false;
+    }
+
+  std::string transformItkClassName = transformItk_LPS->GetNameOfClass();
+
   bool inverse = false;
   typename DisplacementFieldTransformType::DisplacementFieldType* gridImageItk_Lps = NULL;
-  if (inverseDisplacementFieldTransform!=NULL) // inverse class is derived from forward class, so it has to be checked first
+  if (transformItkClassName == "InverseDisplacementFieldTransform") // inverse class is derived from forward class, so it has to be checked first
     {
+    DisplacementFieldTransformType* inverseDisplacementFieldTransform = static_cast<InverseDisplacementFieldTransformType*>( transformItk_LPS.GetPointer() );
     inverse = true;
     gridImageItk_Lps = inverseDisplacementFieldTransform->GetDisplacementField();
     }
-  else if (displacementFieldTransform!=NULL)
+  else if (transformItkClassName == "DisplacementFieldTransform")
     {
+    DisplacementFieldTransformType* displacementFieldTransform = static_cast<DisplacementFieldTransformType*>( transformItk_LPS.GetPointer() );
     inverse = false;
     gridImageItk_Lps = displacementFieldTransform->GetDisplacementField();
     }
@@ -1054,19 +1064,27 @@ bool vtkITKTransformConverter::SetVTKThinPlateSplineTransformFromITK(vtkObject* 
     return false;
     }
 
-  ThinPlateSplineTransformType* tpsTransform = dynamic_cast<ThinPlateSplineTransformType*>( transformItk_LPS.GetPointer() );
-  ThinPlateSplineTransformType* inverseTpsTransform = dynamic_cast<InverseThinPlateSplineTransformType*>( transformItk_LPS.GetPointer() );
+  if (!transformItk_LPS)
+    {
+    vtkErrorWithObjectMacro(loggerObject, "Cannot set VTK thin-plate spline transform from ITK: the input transform is NULL");
+    return false;
+    }
+
+  std::string transformItkClassName = transformItk_LPS->GetNameOfClass();
+
   bool inverse = false;
   typename ThinPlateSplineTransformType::PointSetType::Pointer sourceLandmarksItk_Lps;
   typename ThinPlateSplineTransformType::PointSetType::Pointer targetLandmarksItk_Lps;
-  if (inverseTpsTransform!=NULL) // inverse class is derived from forward class, so it has to be checked first
+  if (transformItkClassName == "InverseThinPlateSplineKernelTransform") // inverse class is derived from forward class, so it has to be checked first
     {
+    ThinPlateSplineTransformType* inverseTpsTransform = static_cast<InverseThinPlateSplineTransformType*>( transformItk_LPS.GetPointer() );
     inverse = true;
     sourceLandmarksItk_Lps = inverseTpsTransform->GetSourceLandmarks();
     targetLandmarksItk_Lps = inverseTpsTransform->GetTargetLandmarks();
     }
-  else if (tpsTransform!=NULL)
+  else if (transformItkClassName == "ThinPlateSplineKernelTransform")
     {
+    ThinPlateSplineTransformType* tpsTransform = static_cast<ThinPlateSplineTransformType*>( transformItk_LPS.GetPointer() );
     inverse = false;
     sourceLandmarksItk_Lps = tpsTransform->GetSourceLandmarks();
     targetLandmarksItk_Lps = tpsTransform->GetTargetLandmarks();
@@ -1351,12 +1369,13 @@ itk::Object::Pointer vtkITKTransformConverter::CreateITKTransformFromVTK(vtkObje
         return 0;
         }
 
-      CompositeTransformType::TransformType::Pointer singleTransformItkTypeChecked = dynamic_cast< CompositeTransformType::TransformType* >( singleTransformItk.GetPointer() );
-      if (singleTransformItkTypeChecked.IsNull())
+      if (singleTransformItk.IsNull()
+          || std::string(singleTransformItk->GetNameOfClass()).find("Transform") == std::string::npos)
         {
         vtkErrorWithObjectMacro(loggerObject, "vtkITKTransformConverter::CreateITKTransformFromVTK failed: invalid element found while trying to create a composite transform");
         return 0;
         }
+      CompositeTransformType::TransformType::Pointer singleTransformItkTypeChecked = static_cast< CompositeTransformType::TransformType* >( singleTransformItk.GetPointer() );
       compositeTransformItk->AddTransform(singleTransformItkTypeChecked.GetPointer());
       }
     return primaryTransformItk;

commit 7e41e8af8aac7945c3a3aeb5d8a3128d3b55e77a

BUG: Fix registration addressing regression in vtkITKTransformConverter.
Fixes #4139

This commit allows BRAINSFit registration to complete by addressing
a regression introduced in r24875 (BUG: Fix transform tests by not
using dynamic_cast in vtkITKTransformConverter)

It basically ensure the comparison of transform name includes
all derived type associated with itk::MatrixOffsetTransformBase

The python code allowing to reproduce the issue is reported below
and has been integrated into a self-test named "BRAINSFitRigidRegistrationCrashIssue4139"

8<---8<---8<---8<---8<---8<---8<---8<---8<---8<---8<---8<---8<---
import SampleData
from SampleData import SampleDataLogic
sampleDatalogic = SampleDataLogic()

fixed = sampleDatalogic.downloadMRBrainTumor1()
moving = sampleDatalogic.downloadMRBrainTumor2()

outputTransform = slicer.vtkMRMLLinearTransformNode()
slicer.mrmlScene.AddNode(outputTransform)

outputVolume = slicer.vtkMRMLScalarVolumeNode()
slicer.mrmlScene.AddNode(outputVolume)

parameters = {
'fixedVolume' : fixed,
'movingVolume' : moving,
'linearTransform' : outputTransform,
'outputVolume' : outputVolume,
'useRigid' : True
}
cmdLineNode = slicer.cli.runSync(slicer.modules.brainsfit, parameters=parameters)
8<---8<---8<---8<---8<---8<---8<---8<---8<---8<---8<---8<---8<---

Reported-by: Christian Herz <christian.herz@mevis.fraunhofer.de>
Suggested-by: Hans Johnson <hans-johnson@uiowa.edu>

git-svn-id: http://svn.slicer.org/Slicer4/trunk@24902 3bd1e089-480b-0410-8dfb-8563597acbee

Libs/MRML/Core/vtkITKTransformConverter.h
@@ -148,9 +148,19 @@ bool vtkITKTransformConverter::SetVTKLinearTransformFromITK(
   std::string itkTransformClassName = transformItk_LPS->GetNameOfClass();
 
   // Linear transform of doubles or floats, dimension 3
-  if (itkTransformClassName == "AffineTransform" ||
+  if (itkTransformClassName.find( "AffineTransform" ) != std::string::npos ||
+      itkTransformClassName == "MatrixOffsetTransformBase" ||
       itkTransformClassName == "Rigid3DTransform" ||
-      itkTransformClassName == "ScaleTransform")
+      itkTransformClassName == "Euler3DTransform" ||
+      itkTransformClassName == "CenteredEuler3DTransform" ||
+      itkTransformClassName == "QuaternionRigidTransform" ||
+      itkTransformClassName == "VersorTransform" ||
+      itkTransformClassName == "VersorRigid3DTransform" ||
+      itkTransformClassName == "ScaleSkewVersor3DTransform" ||
+      itkTransformClassName == "ScaleVersor3DTransform" ||
+      itkTransformClassName == "Similarity3DTransform" ||
+      itkTransformClassName == "ScaleTransform" ||
+      itkTransformClassName == "ScaleLogarithmicTransform")
     {
     typename LinearTransformType::Pointer dlt
       = static_cast<LinearTransformType*>( transformItk_LPS.GetPointer() );

Applications/SlicerApp/Testing/Python/BRAINSFitRigidRegistrationCrashIssue4139.py
@@ -1,0 +1,136 @@
+import os
+import unittest
+import vtk, qt, ctk, slicer
+from slicer.ScriptedLoadableModule import *
+import logging
+
+#
+# BRAINSFitRigidRegistrationCrashIssue4139
+#
+
+class BRAINSFitRigidRegistrationCrashIssue4139(ScriptedLoadableModule):
+  """Uses ScriptedLoadableModule base class, available at:
+  https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
+  """
+
+  def __init__(self, parent):
+    ScriptedLoadableModule.__init__(self, parent)
+    self.parent.title = "BRAINSFit Rigid Registration vtkITKTransformConverter crash (Issue 4139)"
+    self.parent.categories = ["Testing.TestCases"]
+    self.parent.dependencies = []
+    self.parent.contributors = ["Jean-Christophe Fillion-Robin (Kitware)"] # replace with "Firstname Lastname (Organization)"
+    self.parent.helpText = """This test has been added to check that
+    Slicer does not crash in vtkITKTransformConverter after completing BRAINSFit rigid registration.
+
+    Problem has been documented in issue #4139. Commit r24901 fixes the problem
+    by updating vtkITKTransformConverter class.
+    """
+    self.parent.acknowledgementText = """
+    This file was originally developed by Jean-Christophe Fillion-Robin, Kitware Inc.
+    and was partially funded by NIH grant 1U24CA194354-01.
+    """ # replace with organization, grant and thanks.
+
+#
+# BRAINSFitRigidRegistrationCrashIssue4139Widget
+#
+
+class BRAINSFitRigidRegistrationCrashIssue4139Widget(ScriptedLoadableModuleWidget):
+  """Uses ScriptedLoadableModuleWidget base class, available at:
+  https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
+  """
+
+  def setup(self):
+    ScriptedLoadableModuleWidget.setup(self)
+
+#
+# BRAINSFitRigidRegistrationCrashIssue4139Logic
+#
+
+class BRAINSFitRigidRegistrationCrashIssue4139Logic(ScriptedLoadableModuleLogic):
+  """This class should implement all the actual
+  computation done by your module.  The interface
+  should be such that other python code can import
+  this class and make use of the functionality without
+  requiring an instance of the Widget.
+  Uses ScriptedLoadableModuleLogic base class, available at:
+  https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
+  """
+
+  def hasImageData(self,volumeNode):
+    """This is an example logic method that
+    returns true if the passed in volume
+    node has valid image data
+    """
+    if not volumeNode:
+      logging.debug('hasImageData failed: no volume node')
+      return False
+    if volumeNode.GetImageData() is None:
+      logging.debug('hasImageData failed: no image data in volume node')
+      return False
+    return True
+
+class BRAINSFitRigidRegistrationCrashIssue4139Test(ScriptedLoadableModuleTest):
+  """
+  This is the test case for your scripted module.
+  Uses ScriptedLoadableModuleTest base class, available at:
+  https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
+  """
+
+  def setUp(self):
+    """ Do whatever is needed to reset the state - typically a scene clear will be enough.
+    """
+    slicer.mrmlScene.Clear(0)
+
+  def runTest(self):
+    """Run as few or as many tests as needed here.
+    """
+    self.setUp()
+    self.test_BRAINSFitRigidRegistrationCrashIssue4139()
+
+  def test_BRAINSFitRigidRegistrationCrashIssue4139(self):
+    """ Ideally you should have several levels of tests.  At the lowest level
+    tests should exercise the functionality of the logic with different inputs
+    (both valid and invalid).  At higher levels your tests should emulate the
+    way the user would interact with your code and confirm that it still works
+    the way you intended.
+    One of the most important features of the tests is that it should alert other
+    developers when their changes will have an impact on the behavior of your
+    module.  For example, if a developer removes a feature that you depend on,
+    your test should break so they know that the feature is needed.
+    """
+
+    self.delayDisplay("Starting the test")
+
+    logic = BRAINSFitRigidRegistrationCrashIssue4139Logic()
+
+    import SampleData
+    from SampleData import SampleDataLogic
+    sampleDatalogic = SampleDataLogic()
+
+    fixed = sampleDatalogic.downloadMRBrainTumor1()
+    self.assertIsNotNone(logic.hasImageData(fixed))
+
+    moving = sampleDatalogic.downloadMRBrainTumor2()
+    self.assertIsNotNone(logic.hasImageData(moving))
+
+    self.delayDisplay('Finished with download and loading')
+
+    outputTransform = slicer.vtkMRMLLinearTransformNode()
+    slicer.mrmlScene.AddNode(outputTransform)
+
+    outputVolume = slicer.vtkMRMLScalarVolumeNode()
+    slicer.mrmlScene.AddNode(outputVolume)
+
+    parameters = {
+      'fixedVolume' : fixed,
+      'movingVolume' : moving,
+      'linearTransform' : outputTransform,
+      'outputVolume' : outputVolume,
+      'useRigid' : True
+    }
+    cmdLineNode = slicer.cli.runSync(slicer.modules.brainsfit, parameters=parameters)
+    self.assertIsNotNone(cmdLineNode)
+
+    # If test reach this point without crashing it is a success
+
+    self.delayDisplay('Test passed!')

Applications/SlicerApp/Testing/Python/CMakeLists.txt
@@ -287,6 +287,14 @@ if(Slicer_USE_QtTesting AND Slicer_USE_PYTHONQT)
       TwoCLIsInARowTest.py
       TwoCLIsInParallelTest.py
       )
+
+    if(Slicer_BUILD_BRAINSTOOLS)
+      slicer_add_python_unittest(SCRIPT BRAINSFitRigidRegistrationCrashIssue4139.py)
+
+      list(APPEND KIT_PYTHON_SCRIPTS
+        BRAINSFitRigidRegistrationCrashIssue4139.py
+        )
+    endif()
   endif()
 
   set(KIT_PYTHON_RESOURCES