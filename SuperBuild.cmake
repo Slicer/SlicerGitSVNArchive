Slicer/SlicerGitSVNArchive

commit a6f341234d4e2885227e70618904eda526458f9b

BUG: Update BRAINSTools to include BRAINSFit fix for centerOfROI initializer
Tested-by: Hans Johnson <hans-johnson@uiowa.edu>

$ git shortlog e257e62...29d203c --no-merges
Alexander Leinoff (5):
      COMP: BCD should only request VTK when building GUI
      COMP: CompareImageFilter should only use VTK when building GUI
      BUG: Replaces all baselines for BCDTests
      BUG: Fixes typo in previous commit
      BUG: Fixes version check in top level CMAKELists.txt

Ali Ghayoor (13):
      ENH: Upgrade vnl_powell to itkPowellOptimizerv4 in BCD
      ENH: Write the debug image based on requested debug level
      ENH: BCD can process input images with different dynamic range
      ENH: Change tolerance and step size for Powell optimizer
      BUG: Fixed a bug in BABC when a low debugLevel requested
      ENH: New parameters for multimodal segmentation in BABC
      ENH: Updated purePlugsThreshold value for AutoWorkup
      BUG: fixed a bug in BABC denoise filtering
      ENH: Initialize a matrix variable
      ENH: remove an intermediate variable to avoid extra memory usage
      BUG: defined an orderedmap class for BABC
      STYLE: Cleanup unnecessary if statement
      ENH: Prevent holes in created brain mask by ROIAutoFilter

Andrey Fedorov (2):
      BUG: fix centerOfROI initializer
      ENH: adjust restriction on the required registration phases

Hans J. Johnson (40):
      ENH: Update ITK, ANTS, and teem
      STYLE: Remove MinSizeRel option
      COMP: Resolve clang static analysis warnings
      BUG:  Compilation error fixed due to typo.
      ENH: Update external tools support.
      COMP: Improve coverage by using removeIntensityOutliers flag.
      COMP: Force C++11 compliance testing
      COMP: Added compatibility testing with apple and CXX11
      BUG: Nipype now reqires invert_initial_moving_transform to be set
      ENH: Selection of denoising filter improved.
      ENH: Changed options to reflect current processing

SuperBuild.cmake
@@ -251,8 +251,8 @@ set(BRAINSTools_options
   USE_DWIConvert:BOOL=${Slicer_BUILD_DICOM_SUPPORT} ## Need to figure out library linking
   )
 Slicer_Remote_Add(BRAINSTools
-  GIT_REPOSITORY "${git_protocol}://github.com/Slicer/BRAINSTools.git"
-  GIT_TAG "1a8e84b5b850a9f08338b23d80c18e459e0b01a1"  # post-v4.6.0
+  GIT_REPOSITORY "${git_protocol}://github.com/BRAINSia/BRAINSTools.git"
+  GIT_TAG "29d203c63045f7b2c9bad64b9acebee9e3e7b463"  # post-v4.6.0
   OPTION_NAME Slicer_BUILD_BRAINSTOOLS
   OPTION_DEPENDS "Slicer_BUILD_CLI_SUPPORT;Slicer_BUILD_CLI"
   LABELS REMOTE_MODULE

commit 2bde4f0db1ac7413763d17fbb07c4a82d90d5df2

BUG: Update BRAINSTools to fix mask checks
This should resolve the regression introduced in r24963 that caused failing
EMSegment test. The problem was identified and solution proposed by @jcfr

$ git shortlog 29d203c...16526a5 --no-merges
Andrey Fedorov (1):
      BUG: fix mask checks for centerOfROI initializer

SuperBuild.cmake
@@ -252,7 +252,7 @@ set(BRAINSTools_options
   )
 Slicer_Remote_Add(BRAINSTools
   GIT_REPOSITORY "${git_protocol}://github.com/BRAINSia/BRAINSTools.git"
-  GIT_TAG "29d203c63045f7b2c9bad64b9acebee9e3e7b463"  # post-v4.6.0
+  GIT_TAG "16526a5dbab238dfd9a82298953b277a30b4ef4d"  # post-v4.6.0
   OPTION_NAME Slicer_BUILD_BRAINSTOOLS
   OPTION_DEPENDS "Slicer_BUILD_CLI_SUPPORT;Slicer_BUILD_CLI"
   LABELS REMOTE_MODULE