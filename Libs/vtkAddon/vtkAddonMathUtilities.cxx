85bb7be22b2969c53399f14df127cd39be94e022
Davide <punzodavide@hotmail.it>
2016-05-19 14:20:52 -0400

    ENH: added vtkAddonMathUtilities class with Matrix4x4AreEqual method

---
+++ Libs/vtkAddon/vtkAddonMathUtilities.cxx
@@ -0,0 +1,54 @@
+/*=auto==============================================================================
+
+  Program: 3D Slicer
+
+  See COPYRIGHT.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+===============================================================================auto=*/
+
+#include <vtkAddonMathUtilities.h>
+#include <vtkMath.h>
+#include <vtkNew.h>
+#include <vtkObjectFactory.h>
+#include <math.h>
+
+vtkStandardNewMacro(vtkAddonMathUtilities);
+
+//----------------------------------------------------------------------------
+vtkAddonMathUtilities::vtkAddonMathUtilities()
+{
+}
+
+//----------------------------------------------------------------------------
+vtkAddonMathUtilities::~vtkAddonMathUtilities()
+{
+}
+
+//----------------------------------------------------------------------------
+void vtkAddonMathUtilities::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os,indent);  
+}
+
+bool vtkAddonMathUtilities::Matrix4x4AreEqual(vtkMatrix4x4 *m1, vtkMatrix4x4 *m2, double tolerance)
+{
+  int i,j;
+  for (i = 0; i < 4; i++)
+    {
+    for (j = 0; j < 4; j++)
+      {
+      if ( m1->GetElement(i, j) - m2->GetElement(i, j) > tolerance )
+        {
+        return 0;
+        }
+      }
+    }
+    return 1;
+}
