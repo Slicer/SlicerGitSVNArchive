Slicer/SlicerGitSVNArchive

commit 9cb5668fde1abc8f0430a91ca37fc29277ceeb4e

STYLE: Simplify loading of loadable module python modules
This commit removes the special case and ensures all loadable module
python dependencies (Logic, MRML, MRMLDisplayableManager and Widget) are
loaded using the method "qSlicerLoadableModule::importModulePythonExtensions".

Since prior to this change, moduledm, modulelogic, modulemrml and modulewidget
where imported into slicer from within slicer/__init__.py, this commit
updates "slicer.util.importModuleObjects" to explicitly add the classes
to the slicer module.

git-svn-id: http://svn.slicer.org/Slicer4/trunk@25115 3bd1e089-480b-0410-8dfb-8563597acbee

Base/QTGUI/qSlicerLoadableModule.cxx
@@ -59,7 +59,9 @@ qSlicerLoadableModule::~qSlicerLoadableModule()
 
 //-----------------------------------------------------------------------------
 bool qSlicerLoadableModule::importModulePythonExtensions(
-    qSlicerCorePythonManager * pythonManager, const QString& intDir, const QString& modulePath)
+    qSlicerCorePythonManager * pythonManager,
+    const QString& intDir,const QString& modulePath,
+    bool isEmbedded)
 {
 #ifdef Slicer_USE_PYTHONQT
   if(!pythonManager)
@@ -72,14 +74,18 @@ bool qSlicerLoadableModule::importModulePythonExtensions(
     modulePathWithoutIntDir.cdUp();
     }
   QString pythonPath = modulePathWithoutIntDir.filePath("Python");
-  QStringList paths; paths << QFileInfo(modulePath).dir().absolutePath() << pythonPath;
-  pythonManager->appendPythonPaths(paths);
+  if (!isEmbedded)
+    {
+    QStringList paths; paths << QFileInfo(modulePath).dir().absolutePath() << pythonPath;
+    pythonManager->appendPythonPaths(paths);
+    }
   // Update current application directory, so that *PythonD modules can be loaded
   ctkScopedCurrentDir scopedCurrentDir(modulePathWithoutIntDir.absolutePath());
   pythonManager->executeString(QString(
         "from slicer.util import importVTKClassesFromDirectory;"
         "importVTKClassesFromDirectory('%1', 'slicer.modulelogic', filematch='vtkSlicer*ModuleLogic.py');"
         "importVTKClassesFromDirectory('%1', 'slicer.modulemrml', filematch='vtkSlicer*ModuleMRML.py');"
+        "importVTKClassesFromDirectory('%1', 'slicer.modulemrml', filematch='vtkSlicer*ModuleMRMLDisplayableManager.py');"
         ).arg(pythonPath));
   pythonManager->executeString(QString(
         "from slicer.util import importQtClassesFromDirectory;"
@@ -152,11 +158,13 @@ void qSlicerLoadableModule::setup()
 
 #ifdef Slicer_USE_PYTHONQT
   qSlicerCoreApplication * app = qSlicerCoreApplication::application();
-  if (app && !app->isEmbeddedModule(this->path()))
+  if (app)
     {
     // By convention, if the module is not embedded,
     // "<MODULEPATH>/Python" will be appended to PYTHONPATH
-    if (!Self::importModulePythonExtensions(app->corePythonManager(), app->intDir(), this->path()))
+    if (!Self::importModulePythonExtensions(
+          app->corePythonManager(), app->intDir(), this->path(),
+          app->isEmbeddedModule(this->path())))
       {
       qWarning() << "qSlicerLoadableModule::setup - Failed to import module" << this->name() << "python extensions";
       }

Base/Python/slicer/moduledm/__init__.py
@@ -1,15 +1,1 @@
-""" This module loads the Slicer Module Displayable Manager vtk classes into its namespace."""
-
-from __main__ import _qSlicerCoreApplicationInstance as app
-from slicer.util import importVTKClassesFromDirectory
-from os import path
-
-# HACK Ideally constant from vtkSlicerConfigure and vtkSlicerVersionConfigure should
-#      be wrapped.
-slicer_qt_loadable_modules_lib_subdir =  path.join("lib", "Slicer-%d.%d", "qt-loadable-modules") % (app.majorVersion, app.minorVersion)
-directory = path.join(app.slicerHome, slicer_qt_loadable_modules_lib_subdir, "Python")
-if not app.commandOptions().disableBuiltInLoadableModules:
-    importVTKClassesFromDirectory(directory, __name__, filematch = "vtkSlicer*ModuleMRMLDisplayableManager.py")
-
-# Removing things the user shouldn't have to see.
-del app, importVTKClassesFromDirectory, directory, slicer_qt_loadable_modules_lib_subdir, path
+""" This module lists the Slicer Module Displayable Manager vtk classes into its namespace."""

Base/Python/slicer/modulelogic/__init__.py
@@ -1,22 +1,1 @@
-""" This module loads the Slicer Module Logic vtk classes into its namespace."""
-
-# Import the CLI logic
-# HACK Ideally constant from vtkSlicerConfigure should be wrapped,
-#      that way the following try/except could be avoided.
-try:
-  from qSlicerBaseQTCLIPython import vtkSlicerCLIModuleLogic
-except: pass
-
-from __main__ import _qSlicerCoreApplicationInstance as app
-from slicer.util import importVTKClassesFromDirectory
-from os import path
-
-# HACK Ideally constant from vtkSlicerConfigure and vtkSlicerVersionConfigure should
-#      be wrapped.
-slicer_qt_loadable_modules_lib_subdir =  path.join("lib", "Slicer-%d.%d", "qt-loadable-modules") % (app.majorVersion, app.minorVersion)
-directory = path.join(app.slicerHome, slicer_qt_loadable_modules_lib_subdir, "Python")
-if not app.commandOptions().disableBuiltInLoadableModules:
-    importVTKClassesFromDirectory(directory, __name__, filematch = "vtkSlicer*ModuleLogic.py")
-
-# Removing things the user shouldn't have to see.
-del app, importVTKClassesFromDirectory, directory, slicer_qt_loadable_modules_lib_subdir, path
+""" This module lists the Slicer Module Logic vtk classes into its namespace."""

Base/Python/slicer/modulemrml/__init__.py
@@ -1,15 +1,1 @@
-""" This module loads the Slicer Module MRML vtk classes into its namespace."""
-
-from __main__ import _qSlicerCoreApplicationInstance as app
-from slicer.util import importVTKClassesFromDirectory
-from os import path
-
-# HACK Ideally constant from vtkSlicerConfigure and vtkSlicerVersionConfigure should
-#      be wrapped.
-slicer_qt_loadable_modules_lib_subdir =  path.join("lib", "Slicer-%d.%d", "qt-loadable-modules") % (app.majorVersion, app.minorVersion)
-directory = path.join(app.slicerHome, slicer_qt_loadable_modules_lib_subdir, "Python")
-if not app.commandOptions().disableBuiltInLoadableModules:
-    importVTKClassesFromDirectory(directory, __name__, filematch = "vtkSlicer*ModuleMRML.py")
-
-# Removing things the user shouldn't have to see.
-del app, importVTKClassesFromDirectory, directory, slicer_qt_loadable_modules_lib_subdir, path
+""" This module lists the Slicer Module MRML vtk classes into its namespace."""

Base/Python/slicer/modulewidget/__init__.py
@@ -1,15 +1,1 @@
-""" This module loads the Slicer Module Qt classes into its namespace."""
-
-from __main__ import _qSlicerCoreApplicationInstance as app
-from slicer.util import importQtClassesFromDirectory
-from os import path
-
-# HACK Ideally constant from vtkSlicerConfigure and vtkSlicerVersionConfigure should
-#      be wrapped.
-slicer_qt_loadable_modules_lib_subdir =  path.join("lib", "Slicer-%d.%d", "qt-loadable-modules") % (app.majorVersion, app.minorVersion)
-directory = path.join(app.slicerHome, slicer_qt_loadable_modules_lib_subdir, app.intDir)
-if not app.commandOptions().disableBuiltInLoadableModules:
-    importQtClassesFromDirectory(directory, __name__, filematch = "qSlicer*PythonQt.*")
-
-# Removing things the user shouldn't have to see.
-del app, importQtClassesFromDirectory, directory, slicer_qt_loadable_modules_lib_subdir, path
+""" This module lists the Slicer Module Qt classes into its namespace."""

Base/Python/slicer/util.py
@@ -79,6 +79,7 @@ def importModuleObjects(from_module_name, dest_module_name, type_name):
   # Obtain a reference to the module identifed by 'dest_module_name'
   import sys
   dest_module = sys.modules[dest_module_name]
+  slicer_module = sys.modules['slicer']
 
   # Obtain a reference to the module identified by 'from_module_name'
   import imp
@@ -94,6 +95,7 @@ def importModuleObjects(from_module_name, dest_module_name, type_name):
     # Add the object to dest_module_globals_dict if any
     if type(item).__name__ == type_name:
       setattr(dest_module, item_name, item)
+      setattr(slicer_module, item_name, item)
 
 #
 # UI

Base/QTGUI/qSlicerLoadableModule.h
@@ -52,6 +52,7 @@ public:
   /// <ul>
   ///   <li>vtkSlicer*ModuleLogic.py</li>
   ///   <li>vtkSlicer*ModuleMRML.py</li>
+  ///   <li>vtkSlicer*ModuleMRMLDisplayableManager.py</li>
   ///   <li>qSlicer*PythonQt.* python</li>
   /// </ul>
   /// These files are searched within the \a modulePath minus the \a IntDir
@@ -59,7 +60,8 @@ public:
   /// \sa qSlicerCoreApplication::intDir(), qSlicerCoreApplication::corePythonManager()
   static bool importModulePythonExtensions(qSlicerCorePythonManager * pythonManager,
                                            const QString& intDir,
-                                           const QString& modulePath);
+                                           const QString& modulePath,
+                                           bool isEmbedded=false);
 
   /// Set \a module identified by \a moduleName has an attribute of "slicer.modules" module dictionary.
   /// qSlicerCoreApplication::corePythonManager()

commit 69d00eab37959a7886a7855c06ef3c6eaad9c10b

BUG: Fix loading of PythonQt module from visual studio build tree
This commit fixes a regression introduced in r25115 (STYLE: Simplify
loading of loadable module python modules)

Module like "qSlicer*PythonQt.*" are built in the intermediate directory.

Thanks: Andras Lasso <lasso@queensu.ca>
Thanks: Csaba Pinter <csaba.pinter@queensu.ca>

git-svn-id: http://svn.slicer.org/Slicer4/trunk@25122 3bd1e089-480b-0410-8dfb-8563597acbee

Base/QTGUI/qSlicerLoadableModule.cxx
@@ -90,7 +90,7 @@ bool qSlicerLoadableModule::importModulePythonExtensions(
   pythonManager->executeString(QString(
         "from slicer.util import importQtClassesFromDirectory;"
         "importQtClassesFromDirectory('%1', 'slicer', filematch='qSlicer*PythonQt.*');"
-        ).arg(modulePathWithoutIntDir.absolutePath()));
+        ).arg(modulePathWithoutIntDir.absoluteFilePath(intDir)));
   return !pythonManager->pythonErrorOccured();
 #else
   Q_UNUSED(pythonManager);