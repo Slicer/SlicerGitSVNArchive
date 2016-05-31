602a8235a3f9c858af72f4d00072c5ea290896ef
Thomas Greer <hastings.greer@kitware.com>
2016-05-31 18:30:37 -0400

    PERF: unit test for slicer.util.getNodes no longer loads a file.
    new tests also cover more potential issues

--- Base/Python/slicer/tests/test_slicer_util_getNodes.py
+++ Base/Python/slicer/tests/test_slicer_util_getNodes.py
@@ -3,8 +3,18 @@ import slicer
 import slicer.util
 
 class SlicerUnitTestTest(unittest.TestCase):
+    def setUp(self):
+        slicer.mrmlScene.AddNode(slicer.vtkMRMLScalarVolumeNode()).SetName("Volume1")
+        slicer.mrmlScene.AddNode(slicer.vtkMRMLScalarVolumeNode()).SetName("Volume2")
+
     def test_getNodesNoArg(self):
         self.assertEqual(slicer.util.getNodes(), slicer.util.getNodes("*"))
+        self.assertTrue("Volume1" in slicer.util.getNodes("*"))
+        self.assertTrue("Volume2" in slicer.util.getNodes("*"))
+        self.assertTrue("Volume1" in slicer.util.getNodes())
+        self.assertTrue("Volume2" in slicer.util.getNodes())
 
     def test_getNodes_MRHead(self):
-        self.assertEqual(slicer.util.getNodes("MR-head").keys(), ["MR-head"])
+        self.assertEqual(slicer.util.getNodes("Volume1").keys(), ["Volume1"])
+        self.assertEqual(slicer.util.getNodes("Volume2").keys(), ["Volume2"])
+        self.assertEqual(slicer.util.getNodes("Volume*").keys(), ["Volume1", "Volume2"])
