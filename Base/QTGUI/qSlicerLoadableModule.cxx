Slicer/SlicerGitSVNArchive

commit 47ff01472431e639254a1d1154299bff4c20f4fe

STYLE: Simplify import of Logic/MRML/DisplayableManager python modules
Instead of creating intermediate python scripts, this commit updates
loadable modules wrapping and import mechanism to directly use the
shared library.

The management of PythonQt and VTK*Python module is now identical.

git-svn-id: http://svn.slicer.org/Slicer4/trunk@25128 3bd1e089-480b-0410-8dfb-8563597acbee

Base/QTGUI/qSlicerLoadableModule.cxx
@@ -63,38 +63,28 @@ bool qSlicerLoadableModule::importModulePythonExtensions(
     const QString& intDir,const QString& modulePath,
     bool isEmbedded)
 {
+  Q_UNUSED(intDir);
+  Q_UNUSED(isEmbedded);
 #ifdef Slicer_USE_PYTHONQT
   if(!pythonManager)
     {
     return false;
     }
-  QDir modulePathWithoutIntDir = QFileInfo(modulePath).dir();
-  if (intDir ==  modulePathWithoutIntDir.dirName())
-    {
-    modulePathWithoutIntDir.cdUp();
-    }
-  QString pythonPath = modulePathWithoutIntDir.filePath("Python");
-  if (!isEmbedded)
-    {
-    QStringList paths; paths << QFileInfo(modulePath).dir().absolutePath() << pythonPath;
-    pythonManager->appendPythonPaths(paths);
-    }
   // Update current application directory, so that *PythonD modules can be loaded
-  ctkScopedCurrentDir scopedCurrentDir(modulePathWithoutIntDir.absolutePath());
+  ctkScopedCurrentDir scopedCurrentDir(QFileInfo(modulePath).absolutePath());
   pythonManager->executeString(QString(
         "from slicer.util import importVTKClassesFromDirectory;"
-        "importVTKClassesFromDirectory('%1', 'slicer', filematch='vtkSlicer*ModuleLogic.py');"
-        "importVTKClassesFromDirectory('%1', 'slicer', filematch='vtkSlicer*ModuleMRML.py');"
-        "importVTKClassesFromDirectory('%1', 'slicer', filematch='vtkSlicer*ModuleMRMLDisplayableManager.py');"
-        ).arg(pythonPath));
+        "importVTKClassesFromDirectory('%1', 'slicer', filematch='vtkSlicer*ModuleLogicPython.*');"
+        "importVTKClassesFromDirectory('%1', 'slicer', filematch='vtkSlicer*ModuleMRMLPython.*');"
+        "importVTKClassesFromDirectory('%1', 'slicer', filematch='vtkSlicer*ModuleMRMLDisplayableManagerPython.*');"
+        ).arg(scopedCurrentDir.currentPath()));
   pythonManager->executeString(QString(
         "from slicer.util import importQtClassesFromDirectory;"
         "importQtClassesFromDirectory('%1', 'slicer', filematch='qSlicer*PythonQt.*');"
-        ).arg(modulePathWithoutIntDir.absoluteFilePath(intDir)));
+        ).arg(scopedCurrentDir.currentPath()));
   return !pythonManager->pythonErrorOccured();
 #else
   Q_UNUSED(pythonManager);
-  Q_UNUSED(intDir);
   Q_UNUSED(modulePath);
   return false;
 #endif

CMake/SlicerMacroPythonWrapModuleVTKLibrary.cmake
@@ -81,28 +81,4 @@ macro(SlicerMacroPythonWrapModuleVTKLibrary)
     KIT_PYTHON_LIBRARIES ${PYTHONWRAPMODULEVTKLIBRARY_Wrapped_LIBRARIES}
     )
 
-  # Generate "Python/<lib_name>.py" file
-  file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/Python/${PYTHONWRAPMODULEVTKLIBRARY_RELATIVE_PYTHON_DIR}/${lib_name}.py "
-\"\"\" This module loads all the classes from the ${lib_name} library into its
-namespace.\"\"\"
-
-from ${lib_name}Python import *
-")
-
-  file(GLOB PYFILES
-    RELATIVE "${CMAKE_CURRENT_BINARY_DIR}/Python"
-    "${CMAKE_CURRENT_BINARY_DIR}/Python/${PYTHONWRAPMODULEVTKLIBRARY_RELATIVE_PYTHON_DIR}/*.py")
-  if(PYFILES)
-    include(ctkMacroCompilePythonScript)
-    ctkMacroCompilePythonScript(
-      TARGET_NAME ${lib_name}
-      SCRIPTS "${PYFILES}"
-      RESOURCES ""
-      SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/Python
-      DESTINATION_DIR ${CMAKE_BINARY_DIR}/${Slicer_QTLOADABLEMODULES_PYTHON_LIB_DIR}
-      INSTALL_DIR ${Slicer_INSTALL_QTLOADABLEMODULES_PYTHON_LIB_DIR}
-      NO_INSTALL_SUBDIR
-      )
-  endif()
-
 endmacro()

commit 47dec1ff2f8ad79a65af98230ce2b03608a7059c

BUG: Ensure extension python wrapped modules can be loaded
This commit fixes a regression introduced in r25128 (STYLE: Simplify
import of Logic/MRML/DisplayableManager python modules)

Reported-by: Andras Lasso <lasso@queensu.ca>

Base/QTGUI/qSlicerLoadableModule.cxx
@@ -64,7 +64,6 @@ bool qSlicerLoadableModule::importModulePythonExtensions(
     bool isEmbedded)
 {
   Q_UNUSED(intDir);
-  Q_UNUSED(isEmbedded);
 #ifdef Slicer_USE_PYTHONQT
   if(!pythonManager)
     {
@@ -72,6 +71,13 @@ bool qSlicerLoadableModule::importModulePythonExtensions(
     }
   // Update current application directory, so that *PythonD modules can be loaded
   ctkScopedCurrentDir scopedCurrentDir(QFileInfo(modulePath).absolutePath());
+
+  if (!isEmbedded)
+    {
+    QStringList paths; paths << scopedCurrentDir.currentPath();
+    pythonManager->appendPythonPaths(paths);
+    }
+
   pythonManager->executeString(QString(
         "from slicer.util import importVTKClassesFromDirectory;"
         "importVTKClassesFromDirectory('%1', 'slicer', filematch='vtkSlicer*ModuleLogicPython.*');"
@@ -84,8 +90,9 @@ bool qSlicerLoadableModule::importModulePythonExtensions(
         ).arg(scopedCurrentDir.currentPath()));
   return !pythonManager->pythonErrorOccured();
 #else
-  Q_UNUSED(pythonManager);
+  Q_UNUSED(isEmbedded);
   Q_UNUSED(modulePath);
+  Q_UNUSED(pythonManager);
   return false;
 #endif
 }