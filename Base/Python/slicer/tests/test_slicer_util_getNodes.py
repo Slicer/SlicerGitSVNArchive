Slicer/SlicerGitSVNArchive

commit c32a44f71250e31b905d7bc6cb7b185b19b538e7

BUG: slicer.util.getNodes: Return MRML nodes by default. Fixes #4201
From: Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>

git-svn-id: http://svn.slicer.org/Slicer4/trunk@25140 3bd1e089-480b-0410-8dfb-8563597acbee

Base/Python/slicer/util.py
@@ -471,7 +471,9 @@ def getNodes(pattern = "", scene=None):
       node = scene.GetNthNode(idx)
       name = node.GetName()
       id = node.GetID()
-      if fnmatch.fnmatchcase(name, pattern) or fnmatch.fnmatchcase(id, pattern):
+      if (pattern == "" or
+          fnmatch.fnmatchcase(name, pattern) or
+          fnmatch.fnmatchcase(id, pattern)):
         nodes[node.GetName()] = node
     return nodes
 

Applications/SlicerApp/Testing/Python/CMakeLists.txt
@@ -234,6 +234,12 @@ slicer_add_python_unittest(
   TESTNAME_PREFIX nomainwindow_
   )
 
+slicer_add_python_unittest(
+  SCRIPT ${Slicer_SOURCE_DIR}/Base/Python/slicer/tests/test_slicer_util_getNodes.py
+  SLICER_ARGS --no-main-window --disable-cli-modules --disable-scripted-loadable-modules
+  TESTNAME_PREFIX nomainwindow_
+  )
+
 ## Test reading MGH file format types.
 slicer_add_python_unittest(
   SCRIPT ${Slicer_SOURCE_DIR}/Base/Python/slicer/tests/test_slicer_mgh.py

Base/Python/slicer/tests/test_slicer_util_getNodes.py
@@ -1,0 +1,20 @@
+import unittest
+import slicer
+import slicer.util
+
+class SlicerUtilTest(unittest.TestCase):
+    def setUp(self):
+        slicer.mrmlScene.AddNode(slicer.vtkMRMLScalarVolumeNode()).SetName("Volume1")
+        slicer.mrmlScene.AddNode(slicer.vtkMRMLScalarVolumeNode()).SetName("Volume2")
+
+    def test_getNodes(self):
+        self.assertEqual(slicer.util.getNodes(), slicer.util.getNodes("*"))
+
+        self.assertTrue("Volume1" in slicer.util.getNodes("*"))
+        self.assertTrue("Volume2" in slicer.util.getNodes("*"))
+        self.assertTrue("Volume1" in slicer.util.getNodes())
+        self.assertTrue("Volume2" in slicer.util.getNodes())
+
+        self.assertEqual(slicer.util.getNodes("Volume1").keys(), ["Volume1"])
+        self.assertEqual(slicer.util.getNodes("Volume2").keys(), ["Volume2"])
+        self.assertEqual(slicer.util.getNodes("Volume*").keys(), ["Volume1", "Volume2"])

commit 8305abcaab4283efae8177bef626782fbe722cd9

BUG: Fix LandmarkRegistration test updating slicer.util.getNode. See #4201
This commit fixes a regression introduced in r25140 (BUG: slicer.util.getNodes:
Return MRML nodes by default. Fixes #4201)

The method "getNode()" should *NOT* return a node if an empty pattern
is provided.

git-svn-id: http://svn.slicer.org/Slicer4/trunk@25150 3bd1e089-480b-0410-8dfb-8563597acbee

Base/Python/slicer/util.py
@@ -458,9 +458,10 @@ def resetSliceViews():
 # MRML
 #
 
-def getNodes(pattern = "", scene=None):
-    """Return a dictionary of nodes where the name or id matches the 'pattern'.
-    Providing an empty 'pattern' string will return all nodes.
+def getNodes(pattern="*", scene=None):
+    """Return a dictionary of nodes where the name or id matches the ``pattern``.
+    By default, ``pattern`` is a wildcard and it returns all nodes associated
+    with ``slicer.mrmlScene``.
     """
     import slicer, fnmatch
     nodes = {}
@@ -471,20 +472,19 @@ def getNodes(pattern = "", scene=None):
       node = scene.GetNthNode(idx)
       name = node.GetName()
       id = node.GetID()
-      if (pattern == "" or
-          fnmatch.fnmatchcase(name, pattern) or
+      if (fnmatch.fnmatchcase(name, pattern) or
           fnmatch.fnmatchcase(id, pattern)):
         nodes[node.GetName()] = node
     return nodes
 
-def getNode(pattern = "", index = 0, scene=None):
-    """Return the indexth node where name or id matches 'pattern'.
-    Providing an empty 'pattern' string will return all nodes.
+def getNode(pattern="*", index=0, scene=None):
+    """Return the indexth node where name or id matches ``pattern``.
+    By default, ``pattern`` is a wildcard and it returns the first node
+    associated with ``slicer.mrmlScene``.
     """
     nodes = getNodes(pattern, scene)
     try:
-      if nodes.keys():
-        return nodes.values()[index]
+      return nodes.values()[index]
     except IndexError:
       return None
 

Applications/SlicerApp/Testing/Python/CMakeLists.txt
@@ -238,7 +238,7 @@ slicer_add_python_unittest(
 
 slicer_add_python_unittest(
   SCRIPT ${Slicer_SOURCE_DIR}/Base/Python/slicer/tests/test_slicer_util_getNodes.py
-  SLICER_ARGS --no-main-window --disable-cli-modules --disable-scripted-loadable-modules
+  SLICER_ARGS --no-main-window --disable-modules
   TESTNAME_PREFIX nomainwindow_
   )
 

Base/Python/slicer/tests/test_slicer_util_getNodes.py
@@ -4,11 +4,29 @@ import slicer.util
 
 class SlicerUtilTest(unittest.TestCase):
     def setUp(self):
-        slicer.mrmlScene.AddNode(slicer.vtkMRMLScalarVolumeNode()).SetName("Volume1")
-        slicer.mrmlScene.AddNode(slicer.vtkMRMLScalarVolumeNode()).SetName("Volume2")
+        self.nodes = self._configure_scene(slicer.mrmlScene)
+
+    @staticmethod
+    def _configure_scene(scene):
+        nodes = [slicer.vtkMRMLScalarVolumeNode() for idx in range(2)]
+        scene.AddNode(nodes[0]).SetName("Volume1")
+        scene.AddNode(nodes[1]).SetName("Volume2")
+        return nodes
+
+    def test_getNode(self):
+        self.assertEqual(slicer.util.getNode(""), None)
+
+        # For the following tests, use a dedicated scene where
+        # all nodes are known.
+        scene = slicer.vtkMRMLScene()
+        nodes = self._configure_scene(scene)
+        self.assertEqual(slicer.util.getNode("*", scene=scene), nodes[0])
+        self.assertEqual(slicer.util.getNode("*", index=1, scene=scene), nodes[1])
+        self.assertEqual(slicer.util.getNode(scene=scene), nodes[0])
 
     def test_getNodes(self):
         self.assertEqual(slicer.util.getNodes(), slicer.util.getNodes("*"))
+        self.assertEqual(slicer.util.getNodes(""), {})
 
         self.assertTrue("Volume1" in slicer.util.getNodes("*"))
         self.assertTrue("Volume2" in slicer.util.getNodes("*"))