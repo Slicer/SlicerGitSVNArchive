Slicer/SlicerGitSVNArchive

commit 749448e51197d549aff544b364a8ccdbcd4c7d13

STYLE: Generate description file using template file
Co-authored-by: Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>

From: Jean-Baptiste VIMORT <jb.vimort@gmail.com>

git-svn-id: http://svn.slicer.org/Slicer4/trunk@25014 3bd1e089-480b-0410-8dfb-8563597acbee

Extensions/CMake/SlicerFunctionGenerateExtensionDescription.cmake
@@ -107,53 +107,9 @@ function(slicerFunctionGenerateExtensionDescription)
     set(scm_url ${CMAKE_CURRENT_SOURCE_DIR})
   endif()
 
-
-  file(WRITE ${filename}
-"#
-# First token of each non-comment line is the keyword and the rest of the line
-# (including spaces) is the value.
-# - the value can be blank
-#
-
-# This is source code manager (i.e. svn)
-scm ${scm_type}
-${scm_path_token} ${scm_url}
-scmrevision ${MY_EXTENSION_WC_REVISION}
-
-# list dependencies
-# - These should be names of other modules that have .s4ext files
-# - The dependencies will be built first
-depends     ${MY_EXTENSION_DEPENDS}
-
-# Inner build directory (default is \".\")
-build_subdirectory ${MY_EXTENSION_BUILD_SUBDIRECTORY}
-
-# homepage
-homepage    ${MY_EXTENSION_HOMEPAGE}
-
-# Firstname1 Lastname1 ([SubOrg1, ]Org1), Firstname2 Lastname2 ([SubOrg2, ]Org2)
-# For example: Jane Roe (Superware), John Doe (Lab1, Nowhere), Joe Bloggs (Noware)
-contributors ${MY_EXTENSION_CONTRIBUTORS}
-
-# Match category in the xml description of the module (where it shows up in Modules menu)
-category    ${MY_EXTENSION_CATEGORY}
-
-# url to icon (png, size 128x128 pixels)
-iconurl     ${MY_EXTENSION_ICONURL}
-
-# Give people an idea what to expect from this code
-#  - Is it just a test or something you stand behind?
-status      ${MY_EXTENSION_STATUS}
-
-# One line stating what the module does
-description ${MY_EXTENSION_DESCRIPTION}
-
-# Space separated list of urls
-screenshoturls ${MY_EXTENSION_SCREENSHOTURLS}
-
-# 0 or 1: Define if the extension should be enabled after its installation.
-enabled ${MY_EXTENSION_ENABLED}
-")
+ configure_file(
+ ${CMAKE_CURRENT_LIST_DIR}/../Utilities/Templates/Extensions/extension_description.s4ext.in
+ ${filename})
 
 message(STATUS "Extension description has been written to: ${filename}")
 

Utilities/Templates/Extensions/extension_description.s4ext.in
@@ -1,0 +1,44 @@
+#
+# First token of each non-comment line is the keyword and the rest of the line
+# (including spaces) is the value.
+# - the value can be blank
+#
+
+# This is source code manager (i.e. svn)
+scm ${scm_type}
+${scm_path_token} ${scm_url}
+scmrevision ${MY_EXTENSION_WC_REVISION}
+
+# list dependencies
+# - These should be names of other modules that have .s4ext files
+# - The dependencies will be built first
+depends ${MY_EXTENSION_DEPENDS}
+
+# Inner build directory (default is ".")
+build_subdirectory ${MY_EXTENSION_BUILD_SUBDIRECTORY}
+
+# homepage
+homepage ${MY_EXTENSION_HOMEPAGE}
+
+# Firstname1 Lastname1 ([SubOrg1, ]Org1), Firstname2 Lastname2 ([SubOrg2, ]Org2)
+# For example: Jane Roe (Superware), John Doe (Lab1, Nowhere), Joe Bloggs (Noware)
+contributors ${MY_EXTENSION_CONTRIBUTORS}
+
+# Match category in the xml description of the module (where it shows up in Modules menu)
+category ${MY_EXTENSION_CATEGORY}
+
+# url to icon (png, size 128x128 pixels)
+iconurl ${MY_EXTENSION_ICONURL}
+
+# Give people an idea what to expect from this code
+#  - Is it just a test or something you stand behind?
+status ${MY_EXTENSION_STATUS}
+
+# One line stating what the module does
+description ${MY_EXTENSION_DESCRIPTION}
+
+# Space separated list of urls
+screenshoturls ${MY_EXTENSION_SCREENSHOTURLS}
+
+# 0 or 1: Define if the extension should be enabled after its installation.
+enabled ${MY_EXTENSION_ENABLED}

commit eb6ffb8806bcffd8960e6f5c6fc1f61695097c4c

BUG: GenerateExtensionDescription: Fix module to ensure template is found
This commit fixes a regression introduced in r25014 (STYLE: Generate
description file using template file)

Extensions/CMake/SlicerFunctionGenerateExtensionDescription.cmake
@@ -18,6 +18,10 @@
 #
 ################################################################################
 
+if(NOT DEFINED Slicer_EXTENSIONS_CMAKE_DIR)
+  set(Slicer_EXTENSIONS_CMAKE_DIR ${CMAKE_CURRENT_LIST_DIR})
+endif()
+
 function(slicerFunctionGenerateExtensionDescription)
   set(options)
   set(oneValueArgs
@@ -105,7 +109,7 @@ function(slicerFunctionGenerateExtensionDescription)
   endif()
 
   configure_file(
-    ${CMAKE_CURRENT_LIST_DIR}/../Utilities/Templates/Extensions/extension_description.s4ext.in
+    ${Slicer_EXTENSIONS_CMAKE_DIR}/../../Utilities/Templates/Extensions/extension_description.s4ext.in
     ${filename}
     )
 