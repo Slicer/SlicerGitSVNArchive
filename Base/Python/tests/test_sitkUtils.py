45ba75ca7b210cbb0dfc3229c917e82991cd3f53
Mayeul Chassagnard <mayeul.chassagnard@kitware.com>
2016-07-14 17:27:10 -0400

    STYLE: Changes due to the previous commit remarks

--- Base/Python/tests/test_sitkUtils.py
+++ Base/Python/tests/test_sitkUtils.py
@@ -1,15 +1,9 @@
 import slicer
-#import SimpleITK as sitk
 import sitkUtils as su
 
 import unittest
 
 class SitkUtilsTests(unittest.TestCase):
-
-    def runTest(self):
-        self.setUp()
-        self.test_SimpleITK_SlicerPushPull()
-        self.tearDown()
 
     def setUp(self):
         """ Download the MRHead node
@@ -17,33 +11,32 @@ class SitkUtilsTests(unittest.TestCase):
 
         from SampleData import SampleDataLogic
         SampleDataLogic().downloadMRHead()
-        myNode1 = slicer.util.getNode('MRHead')
-
-        print("Node Name is %s" % myNode1.GetName())
-        self.assertIsNotNone(myNode1.GetName(),"ERROR: Node Name is None !")
-
-        self.myNode1 = myNode1
+        volumeNode1 = slicer.util.getNode('MRHead')
+        self.assertEqual(volumeNode1.GetName(), "MRHead")
+        self.volumeNode1 = volumeNode1
 
     def test_SimpleITK_SlicerPushPull(self):
         """ Try to pull a slicer MRML image, return the SimpleITK,
         push it to Slicer, and then compare if the image are the same
         """
 
-        sitkimage = su.PullFromSlicer(self.myNode1.GetName())
+        sitkimage = su.PullFromSlicer(self.volumeNode1.GetName())
         su.PushToSlicer(sitkimage, 'MRHead', compositeView=0, overwrite=False)
-        myNode2 = slicer.util.getNode('MRHead')
+        volumeNode2 = slicer.util.getNode('MRHead')
 
-        self.assertEqual(self.myNode1, myNode2,
-                         'Error Push Pull: Node are not the same')
-        self.assertEqual(self.myNode1.GetID(), myNode2.GetID(),
-                         'Error Push Pull: id are not the same')
-        self.assertEqual(self.myNode1.GetMTime(), myNode2.GetMTime(),
+        self.assertEqual(self.volumeNode1, volumeNode2,
+                         'Error Push Pull: volumeNode are not the same')
+        self.assertEqual(self.volumeNode1.GetID(), volumeNode2.GetID(),
+                         'Error Push Pull: ID are not the same')
+        self.assertEqual(self.volumeNode1.GetMTime(), volumeNode2.GetMTime(),
                          'Error Push Pull: Modify Time are not the same')
 
         """ To perform the test:
-        Try with a modified sitkimage and verify the GetMTime are diffrents
-        Try with all the compositeView and overwrite (all possiblities)
+        Try to modified the image with sitk tools (just few sanity checks)
+        Verify the GetMTime are different after a modification of sitkimage
+        Try with all the compositeView and overwrite (any possibility)
         """
 
     def tearDown(self):
-        del self.myNode1
+        del self.volumeNode1
+        slicer.mrmlScene.Clear(0)
