8631701091d73a41bbd1cbf3fd503eedf70d29c2
Mayeul Chassagnard <mayeul.chassagnard@kitware.com>
2016-07-26 14:27:31 -0400

    ENH: Add unit test for sitkUtils.py
    Test on image pull from slicer to sitk where we modify pixels, origin, direction
    Verify the Modification Time of the Node from an image modified in sitk
    
    Every possibilities of pushing the data to slicer are working alone but when we do a loop the test is failing because of the hack used in EnsureRegistration() in the file sitkUtils.py

--- Base/Python/tests/test_sitkUtils.py
+++ Base/Python/tests/test_sitkUtils.py
@@ -32,9 +32,62 @@ class SitkUtilsTests(unittest.TestCase):
                          'Error Push Pull: Modify Time are not the same')
 
         """ To perform the test:
-        Try to modified the image with sitk tools (just few sanity checks)
-        Verify the GetMTime are different after a modification of sitkimage
+        DONE : Try to modified the image with sitk tools (just few sanity checks)
+        DONE : Verify the GetMTime are different after a modification of sitkimage
         Try with all the compositeView and overwrite (any possibility)
+        """
+
+        """ Few modification of the image : Direction, Origin """
+        sitkimage.SetDirection((-1.0, 1.0, 0.0, 0.0, -1.0, 1.0, 1.0, 0.0, 1.0))
+        sitkimage.SetOrigin((100.0, 100.0, 100.0))
+
+        """ Few pixel changed """
+        size = sitkimage.GetSize()
+        for x in xrange(0,size[0],(int)(size[0]/10)):
+            for y in xrange(0,size[1],(int)(size[1]/10)):
+                for z in xrange(0,size[2],(int)(size[2]/10)):
+                    sitkimage.SetPixel(x,y,z,0L)
+
+        su.PushToSlicer(sitkimage, 'ImageChanged', compositeView=0, overwrite=False)
+        volumeNode2 = slicer.util.getNode('ImageChanged')
+        self.assertEqual(volumeNode2.GetName(), "ImageChanged",
+                         'Error Push Pull: Node Name are not the same')
+        self.assertNotEqual(self.volumeNode1.GetMTime(), volumeNode2.GetMTime(),
+                            'Error Push Pull: Modify Time are the same')
+
+        """ Test the consistency between sitkimage and volumeNode2
+        """
+        tmp = volumeNode2.GetOrigin()
+        valToCompare = (-tmp[0], -tmp[1], tmp[2])
+        self.assertEqual(valToCompare,sitkimage.GetOrigin(),
+                         'Error Push Pull: Different Origin')
+
+        """ ===========
+        Viewing options
+        ---------------
+        bit 0: Set as background image
+        bit 1: Set as foreground image
+        bit 2: Set as label image
+        """
+        """ TODO : one by one those Push are working but there is an error during the loop
+        caused by the hack in EnsureRegistration() in sitkUtils.py : _DUMMY_DOES_NOT_EXISTS__
+
+        for compositeView in xrange(3):
+            for overwrite in [False, True]:
+                su.PushToSlicer(sitkimage, 'volumeNode'+str(compositeView)+str(overwrite),
+                                compositeView, overwrite)
+                volumeNode = slicer.util.getNode('volumeNode'+str(compositeView)+str(overwrite))
+
+                print("compositeView : %s" %compositeView )
+                print("overwrite : %s " %overwrite )
+
+                #Check if it's a label
+                if compositeView == 2:
+                    self.assertEqual(volumeNode.GetClassName(),'vtkMRMLLabelMapVolumeNode',
+                                     'Error Push Pull: Not a label Class Name')
+                else:
+                    self.assertEqual(volumeNode.GetClassName(), 'vtkMRMLScalarVolumeNode',
+                                     'Error Push Pull: Not a back/foreground Class Name')
         """
 
     def tearDown(self):
