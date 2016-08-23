Slicer/SlicerGitSVNArchive

commit 195c0d5500975f78cf407fe53cf4a6a09e8fe01c

BUG: Extension packaging: For SuperBuild extension, ensure fixup happens last
For extension of type "SuperBuild", the external projects to install are
specified by appending build directories to the variable
"CPACK_INSTALL_CMAKE_PROJECTS", then this variable is processed at
packaging time by "CPack".

This means that simply ensuring the fixup script is executed last during
the install of the current project was not sufficient.

Instead, this commit creates a minimal CMake project named
"<ExtensionName>-Fixup" that is including the fixup script. Then, this
project is appending last to the variable "CPACK_INSTALL_CMAKE_PROJECTS".

See SBU-BMI/SlicerOpenCV#30

git-svn-id: http://svn.slicer.org/Slicer4/trunk@25181 3bd1e089-480b-0410-8dfb-8563597acbee

CMake/SlicerExtensionCPack.cmake
@@ -158,12 +158,53 @@ if(APPLE)
     ${Slicer_EXTENSION_CPACK_BUNDLE_FIXUP}
     "${slicer_extension_cpack_bundle_fixup_directory}/SlicerExtensionCPackBundleFixup.cmake"
     @ONLY)
-  # HACK - For a given directory, "install(SCRIPT ...)" rule will be evaluated first,
-  #        let's make sure the following install rule is evaluated within its own directory.
-  #        Otherwise, the associated script will be executed before any other relevant install rules.
-  file(WRITE ${slicer_extension_cpack_bundle_fixup_directory}/CMakeLists.txt
-    "install(SCRIPT \"${slicer_extension_cpack_bundle_fixup_directory}/SlicerExtensionCPackBundleFixup.cmake\")")
-  add_subdirectory(${slicer_extension_cpack_bundle_fixup_directory} ${slicer_extension_cpack_bundle_fixup_directory}-binary)
+
+  set(msg "Checking if extension type is SuperBuild")
+  message(STATUS "${msg}")
+  if(DEFINED ${EXTENSION_NAME}_SUPERBUILD)
+    message(STATUS "${msg} - true")
+    set(_is_superbuild_extension 1)
+  else()
+    message(STATUS "${msg} - false")
+    set(_is_superbuild_extension 0)
+  endif()
+
+  if(NOT _is_superbuild_extension)
+
+    message(STATUS "Extension fixup mode: adding <cpack_bundle_fixup_directory>")
+    # HACK - For a given directory, "install(SCRIPT ...)" rule will be evaluated first,
+    #        let's make sure the following install rule is evaluated within its own directory.
+    #        Otherwise, the associated script will be executed before any other relevant install rules.
+    add_subdirectory(${slicer_extension_cpack_bundle_fixup_directory} ${slicer_extension_cpack_bundle_fixup_directory}-binary)
+
+  else()
+
+    message(STATUS "Extension fixup mode: updating CPACK_INSTALL_CMAKE_PROJECTS with <cpack_bundle_fixup_directory>")
+    # Configure project and append the build directory to the
+    # list of project to install. This will ensure the fixup happen last
+    # for SuperBuild extensions.
+
+    file(WRITE ${slicer_extension_cpack_bundle_fixup_directory}/CMakeLists.txt
+    "cmake_minimum_required(VERSION 3.0)
+install(SCRIPT \"${slicer_extension_cpack_bundle_fixup_directory}/SlicerExtensionCPackBundleFixup.cmake\")")
+    set(source_dir "${slicer_extension_cpack_bundle_fixup_directory}")
+    set(build_dir "${slicer_extension_cpack_bundle_fixup_directory}-binary")
+    execute_process(
+      COMMAND ${CMAKE_COMMAND} -E make_directory ${build_dir}
+      RESULT_VARIABLE result
+      )
+    if(NOT result EQUAL 0)
+      message(FATAL_ERROR "${EXTENSION_NAME}-Fixup: Failed to create build directory:${build_dir}")
+    endif()
+    execute_process(
+      COMMAND ${CMAKE_COMMAND} ${source_dir}
+      WORKING_DIRECTORY ${build_dir}
+      RESULT_VARIABLE result)
+    if(NOT result EQUAL 0)
+      message(FATAL_ERROR "${EXTENSION_NAME}-Fixup: Failed to configure project [source_dir:${source_dir}, build_dir:${build_dir}")
+    endif()
+    set(CPACK_INSTALL_CMAKE_PROJECTS "${CPACK_INSTALL_CMAKE_PROJECTS};${build_dir};${EXTENSION_NAME}-Fixup;ALL;/")
+  endif()
 endif()
 
 #-----------------------------------------------------------------------------

commit b02e72e0402bf1a9c2ed4e5c41c156f0feb8349c

COMP: SlicerExtensionCPack: Use proper variable evaluation
This commit should fix UKFTractography extension packaging, it is
currently failing because "UKFTractography_SUPERBUILD" is an option
and checking if it is defined always return TRUE.

It also fixes a regression introduced in r25181 (BUG: Extension
packaging: For SuperBuild extension, ensure fixup happen last)

Reviewed-by: Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>

From: Isaiah Norton <inorton@bwh.harvard.edu>

git-svn-id: http://svn.slicer.org/Slicer4/trunk@25303 3bd1e089-480b-0410-8dfb-8563597acbee

CMake/SlicerExtensionCPack.cmake
@@ -161,7 +161,7 @@ if(APPLE)
 
   set(msg "Checking if extension type is SuperBuild")
   message(STATUS "${msg}")
-  if(DEFINED ${EXTENSION_NAME}_SUPERBUILD)
+  if(${EXTENSION_NAME}_SUPERBUILD)
     message(STATUS "${msg} - true")
     set(_is_superbuild_extension 1)
   else()