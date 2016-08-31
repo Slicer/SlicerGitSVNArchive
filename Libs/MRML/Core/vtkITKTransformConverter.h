Slicer/SlicerGitSVNArchive

commit 1464b3f8427e2831caad1df633a67877248a5f76

BUG: Fix vtkMRMLNonlinearTransformNodeTest1
This commit fixes the reading of ITKv3 transforms by addressing
a regression introduced in r24875 (BUG: Fix transform tests by not
using dynamic_cast in vtkITKTransformConverter)

It basically ensures that executable created using compiler (e.g g++4.4.3)
[not supporting the use of dynamic_cast with templated classes instantiated
in a translation unit different than the one where they are defined] still
work as expected.

See #4139

git-svn-id: http://svn.slicer.org/Slicer4/trunk@25318 3bd1e089-480b-0410-8dfb-8563597acbee

Libs/MRML/Core/vtkITKTransformConverter.h
@@ -341,8 +341,17 @@ bool vtkITKTransformConverter::SetVTKBSplineParametersFromITKGeneric(
     return false;
     }
 
-  typename BSplineTransformType::Pointer bsplineItk = dynamic_cast< BSplineTransformType* >( warpTransformItk.GetPointer() );
-  if (!bsplineItk.GetPointer())
+  typename BSplineTransformType::Pointer bsplineItk;
+  std::string warpTransformItkName = warpTransformItk->GetNameOfClass();
+  if (warpTransformItkName == "InverseBSplineTransform" ||
+      warpTransformItkName == "BSplineTransform" ||
+      warpTransformItkName == "BSplineDeformableTransform" ||
+      warpTransformItkName == "InverseBSplineDeformableTransform"
+      )
+    {
+    bsplineItk = static_cast< BSplineTransformType* >( warpTransformItk.GetPointer() );
+    }
+  else
     {
     return false;
     }

commit 39c7cf1f854acace03df46a8c40d00504cbd2983

BUG: Fix EMSegment tests
This commit fixes a regression introduced in r25318 (BUG: Fix
vtkMRMLNonlinearTransformNodeTest1), to properly implement the behavior
of dynamic_cast, this commit specialized a template function returning
the name of the expected transform class name.

Libs/MRML/Core/vtkITKTransformConverter.h
@@ -310,6 +310,41 @@ bool vtkITKTransformConverter::IsIdentityMatrix(vtkMatrix4x4 *matrix)
   return true;
 }
 
+namespace
+{
+
+//----------------------------------------------------------------------------
+template<typename BSplineTransformType>
+std::string
+BSplineTransformClassName()
+{
+  return std::string("Unknown");
+}
+
+#define BSplineTransformType_Specialization(TRANSFORMTYPE, DATATYPE) \
+  template<> \
+  std::string \
+  BSplineTransformClassName<itk::TRANSFORMTYPE<DATATYPE, VTKDimension, VTKDimension> >() \
+  { \
+    return std::string(#TRANSFORMTYPE); \
+  }
+
+BSplineTransformType_Specialization(BSplineTransform, double)
+BSplineTransformType_Specialization(BSplineTransform, float)
+
+BSplineTransformType_Specialization(InverseBSplineTransform, double)
+BSplineTransformType_Specialization(InverseBSplineTransform, float)
+
+BSplineTransformType_Specialization(BSplineDeformableTransform, double)
+BSplineTransformType_Specialization(BSplineDeformableTransform, float)
+
+BSplineTransformType_Specialization(InverseBSplineDeformableTransform, double)
+BSplineTransformType_Specialization(InverseBSplineDeformableTransform, float)
+
+#undef BSplineTransformType_Specialization
+
+}
+
 //----------------------------------------------------------------------------
 template <typename BSplineTransformType>
 bool vtkITKTransformConverter::SetVTKBSplineParametersFromITKGeneric(
@@ -341,13 +376,20 @@ bool vtkITKTransformConverter::SetVTKBSplineParametersFromITKGeneric(
     return false;
     }
 
+  if (!warpTransformItk.GetPointer())
+    {
+    return false;
+    }
+
   typename BSplineTransformType::Pointer bsplineItk;
+  std::string expectedWarpTransformItkName = BSplineTransformClassName<BSplineTransformType>();
+  if (expectedWarpTransformItkName == "Unknown")
+    {
+    vtkErrorWithObjectMacro(loggerObject, "Failed to retrieve name of expected transform");
+    return false;
+    }
   std::string warpTransformItkName = warpTransformItk->GetNameOfClass();
-  if (warpTransformItkName == "InverseBSplineTransform" ||
-      warpTransformItkName == "BSplineTransform" ||
-      warpTransformItkName == "BSplineDeformableTransform" ||
-      warpTransformItkName == "InverseBSplineDeformableTransform"
-      )
+  if(warpTransformItkName == expectedWarpTransformItkName)
     {
     bsplineItk = static_cast< BSplineTransformType* >( warpTransformItk.GetPointer() );
     }