Slicer/SlicerGitSVNArchive

commit a6d3609430dc99c1a264ab891c3fffc0666e20c5

ENH: Standardize how JsonCpp config is managed
For consistency the JsonCpp variables passed to the projects were changed
to JsonCpp_INCLUDE_DIR and JsonCpp_LIBRARY.

JsonCpp_DIR should be used for project providing a Config module.

$ git shortlog 983d2112e..8692b09b6 --no-merges
Johan Andruejol (2):
      ENH: Uniformize the use of JsonCpp with VTK
      BUG: Fix GenerateCLP_FORWARD_PATHS_[BUILD/INSTALL] variable

Co-Authored: Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>

git-svn-id: http://svn.slicer.org/Slicer4/trunk@25346 3bd1e089-480b-0410-8dfb-8563597acbee

SuperBuild/External_JsonCpp.cmake
@@ -59,6 +59,18 @@ if(NOT DEFINED ${proj}_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
     )
   set(${proj}_DIR ${CMAKE_BINARY_DIR}/${proj}-build)
   set(${proj}_SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj})
+  set(${proj}_INCLUDE_DIR ${${proj}_SOURCE_DIR}/include)
+  if(WIN32)
+    set(lib_prefix "")
+    set(lib_ext "lib")
+  elseif(APPLE)
+    set(lib_prefix "lib")
+    set(lib_ext "dylib")
+  else()
+    set(lib_prefix "lib")
+    set(lib_ext "so")
+  endif()
+  set(${proj}_LIBRARY ${${proj}_DIR}/src/lib_json/${CMAKE_CFG_INTDIR}/${lib_prefix}jsoncpp.${lib_ext})
 
   #-----------------------------------------------------------------------------
   # Launcher setting specific to build tree
@@ -74,6 +86,10 @@ else()
 endif()
 
 mark_as_superbuild(
-  VARS JsonCpp_DIR:PATH
+  VARS ${proj}_INCLUDE_DIR:PATH
+  LABELS "FIND_PACKAGE"
+  )
+mark_as_superbuild(
+  VARS ${proj}_LIBRARY:PATH
   LABELS "FIND_PACKAGE"
   )

SuperBuild/External_ParameterSerializer.cmake
@@ -36,7 +36,8 @@ if(NOT DEFINED ${proj}_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
       -DCMAKE_C_FLAGS:STRING=${ep_common_c_flags} # Unused
       -DBUILD_TESTING:BOOL=OFF
       #-DBUILD_SHARED:BOOL=ON
-      -DJsonCpp_DIR:PATH=${JsonCpp_DIR}
+      -DJsonCpp_INCLUDE_DIR:PATH=${JsonCpp_INCLUDE_DIR}
+      -DJsonCpp_LIBRARY:PATH=${JsonCpp_LIBRARY}
       -DITK_DIR:PATH=${ITK_DIR}
       ${EXTERNAL_PROJECT_OPTIONAL_ARGS}
     INSTALL_COMMAND ""

SuperBuild/External_SlicerExecutionModel.cmake
@@ -56,14 +56,15 @@ if(NOT DEFINED SlicerExecutionModel_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM
     ${Slicer_INSTALL_CLIMODULES_LIB_DIR})
 
   if(Slicer_BUILD_PARAMETERSERIALIZER_SUPPORT)
-    _set(JsonCpp_DIR PATH ${JsonCpp_DIR})
+    _set(JsonCpp_INCLUDE_DIR PATH ${JsonCpp_INCLUDE_DIR})
+    _set(JsonCpp_LIBRARY PATH ${JsonCpp_LIBRARY})
     _set(ParameterSerializer_DIR PATH ${ParameterSerializer_DIR})
   endif()
 
   ExternalProject_Add(${proj}
     ${${proj}_EP_ARGS}
     GIT_REPOSITORY "${git_protocol}://github.com/Slicer/SlicerExecutionModel.git"
-    GIT_TAG "9a74e80ac70fb21ac6cf82a3dad8e23f0959214d"
+    GIT_TAG "983d2112ec431af20cafa49ca6cf7bd1a777869a"
     SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
     BINARY_DIR ${proj}-build
     CMAKE_CACHE_ARGS

commit 14d5fddc118f74cc8da78ab84fabe3aaafc55e5d

BUG: Fix windows packaging with Slicer_BUILD_PARAMETERSERIALIZER_SUPPORT
The packaging was broken in r25346 as the packaging relies on the
JsonCpp_DIR variable.
No label is given to this superbuild variable as projects that want to
depend explicitly on JsonCpp should rely on the JsonCpp_INCLUDE_DIR and
JsonCpp_LIBRARY variables.

SuperBuild/External_JsonCpp.cmake
@@ -93,3 +93,12 @@ mark_as_superbuild(
   VARS ${proj}_LIBRARY:PATH
   LABELS "FIND_PACKAGE"
   )
+# JsonCpp is a project built using CMake. JsconCpp_DIR ensures the install
+# rules will be used to create the Slicer package. However because JsonCpp
+# doesn't provide a JsonCppConfig.cmake, it is not yet associated with the
+# "FIND_PACKAGE" label.
+# Instead the variables JsonCpp_INCLUDE_DIR and JsonCpp_LIBRARY are
+# expected to be set to ensure FindJsonCpp.cmake finds everything.
+mark_as_superbuild(
+  VARS ${proj}_DIR:PATH
+  )