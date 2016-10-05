Slicer/SlicerGitSVNArchive

commit 4f515b5c8c15d0a9adac1594a2729bbd69a1f681

ENH: prefer executable CLIs by default
This resolves a known issue with the application crashing while using modules
as shared libraries under certain circumstances.

Motivated by this thread:

http://slicer-devel.65872.n3.nabble.com/Slicer-crashes-when-running-registration-without-having-settings-set-to-quot-Prefer-Executable-CLIs--td4037245.html

git-svn-id: http://svn.slicer.org/Slicer4/trunk@25404 3bd1e089-480b-0410-8dfb-8563597acbee

Base/QTGUI/qSlicerSettingsModulesPanel.cxx
@@ -119,7 +119,7 @@ void qSlicerSettingsModulesPanelPrivate::init()
   this->FavoritesMoreButton->setChecked(false);
 
   // Default values
-  this->PreferExecutableCLICheckBox->setChecked(false);
+  this->PreferExecutableCLICheckBox->setChecked(true);
   this->TemporaryDirectoryButton->setDirectory(coreApp->defaultTemporaryPath());
   this->DisableModulesListView->setFactoryManager( factoryManager );
   this->FavoritesModulesListView->setFactoryManager( factoryManager );

commit df702d7786740aa0a9d448cbd26b02bf24fc32bf

BUG: Introduce macro to set "Prefer CLI executable" settings default value
This commit fixes a regression introduced in r25404 (ENH: prefer executable
CLIs by default) and ensures the settings default value is consistently set.

Base/QTApp/qSlicerApplicationHelper.cxx
@@ -120,7 +120,7 @@ void qSlicerApplicationHelper::setupModuleFactoryManager(qSlicerModuleFactoryMan
 
     // Option to prefer executable CLIs to limit memory consumption.
     bool preferExecutableCLIs =
-      app->userSettings()->value("Modules/PreferExecutableCLI", false).toBool();
+      app->userSettings()->value("Modules/PreferExecutableCLI", Slicer_CLI_PREFER_EXECUTABLE_DEFAULT).toBool();
 
     qSlicerCLILoadableModuleFactory* cliLoadableFactory = new qSlicerCLILoadableModuleFactory();
     cliLoadableFactory->setTempDirectory(tempDirectory);

Base/QTGUI/qSlicerApplication.cxx
@@ -921,7 +921,7 @@ void qSlicerApplication::logApplicationInformation() const
          developerModeEnabled ? "yes" : "no");
 
   // Prefer executable CLI
-  bool preferExecutableCli = settings.value("Modules/PreferExecutableCLI", false).toBool();
+  bool preferExecutableCli = settings.value("Modules/PreferExecutableCLI", Slicer_CLI_PREFER_EXECUTABLE_DEFAULT).toBool();
   qDebug("%s: %s",
          qPrintable(titles.at(6).leftJustified(titleWidth, '.')),
          preferExecutableCli ? "yes" : "no");

Base/QTGUI/qSlicerSettingsModulesPanel.cxx
@@ -119,7 +119,7 @@ void qSlicerSettingsModulesPanelPrivate::init()
   this->FavoritesMoreButton->setChecked(false);
 
   // Default values
-  this->PreferExecutableCLICheckBox->setChecked(true);
+  this->PreferExecutableCLICheckBox->setChecked(Slicer_CLI_PREFER_EXECUTABLE_DEFAULT);
   this->TemporaryDirectoryButton->setDirectory(coreApp->defaultTemporaryPath());
   this->DisableModulesListView->setFactoryManager( factoryManager );
   this->FavoritesModulesListView->setFactoryManager( factoryManager );

CMake/vtkSlicerConfigure.h.in
@@ -73,6 +73,8 @@
 #define Slicer_CLIMODULES_LIB_DIR "@Slicer_CLIMODULES_LIB_DIR@"
 #define Slicer_CLIMODULES_SHARE_DIR "@Slicer_CLIMODULES_SHARE_DIR@"
 
+#define Slicer_CLI_PREFER_EXECUTABLE_DEFAULT true
+
 #cmakedefine Slicer_USE_PYTHONQT
 #ifdef Slicer_USE_PYTHONQT
 #  define Slicer_PYTHON_VERSION_DOT "@Slicer_PYTHON_VERSION_DOT@"