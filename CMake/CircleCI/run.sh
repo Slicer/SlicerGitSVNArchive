5c05ced4c5a81f36575363e1e35e3d2baf00df34
Mayeul Chassagnard <mayeul.chassagnard@kitware.com>
2016-10-26 09:30:35 -0400

    ENH: Continuous Integration for Slicer
    Run tests (which are built in the slicer/slicer-build:BUILD_TESTING_ON
    docker image) with X_Window_System supported inside the slicer/slicer-test:opengl
    docker image based on thewtex/opengl:centos-2016.10.20

---
+++ CMake/CircleCI/run.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+script_dir="`cd $(dirname $0); pwd`"
+
+# Run the images which contains the build of slicer (to link with the testing image with mounted volume)
+docker run -d --name slicer-build-with-test slicer/slicer-build:BUILD_TESTING_ON bash
+
+# Run the opengl docker image which will run the tests located on the volume shared by slicer/slicer-build
+$script_dir/run_opengl.sh -i slicer/slicer-test:opengl -p 6081 \
+    -r --env="CIRCLE_SHA1=$1" -r --env="CIRCLE_BRANCH=$2" -r --env="SITE_BUILD_TYPE=$3" \
+    -r --volumes-from -r slicer-build-with-test
+
+docker rm slicer-build-with-test
---
+++ CMake/CircleCI/test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# This is a script to build the modules and run the test suite in the base
+# Docker container.
+
+die() {
+  echo "Error: $@" 1>&2
+  exit 1;
+}
+
+mkdir /usr/src/Slicer-build
+cd /usr/src/Slicer-build || die "Could not cd into the build directory"
+
+mkdir /usr/src/Slicer-build/Slicer-build
+cd /usr/src/Slicer-build/Slicer-build || die "Could not cd into the build directory"
+
+ctest \
+  -S /usr/src/Slicer/CMake/CircleCI/CircleCI_Slicer_Docker.cmake \
+  -VV || die "ctest failed"
