88733f04493b8fabf251cc75e2cc4a8799cbc69c
Mayeul Chassagnard <mayeul.chassagnard@kitware.com>
2016-12-13 14:27:21 -0500

    WIP: TEST: CircleCI Time Kill command update
    Once the Pull Request is done, CircleCI will trigger and launch
    kill_time_test.sh which is a simple while true displaying a dot every minutes
    and the number of hours elapsed every hour.
    
    EDIT: launch multiple times kill_time_test.sh whose the time is less
    than 2 hours to avoid a run time out from a simple command.

--- CMake/CircleCI/kill_time_test.sh
+++ CMake/CircleCI/kill_time_test.sh
@@ -3,13 +3,17 @@
 min=0
 hour=0
 
-while true
+while (($min < 100))
 do
   echo -n .
   sleep 1m
   min=$(($min + 1))
   if ! (($min % 60)); then
     hour=$(($hour + 1))
-    echo "Hours elapsed: $hour"
+  fi
+  # Time Displayed every 10min
+  if ! (($min % 10)); then
+    min_modulo=$(($min % 60))
+    echo "Time elapsed: $hour:$min_modulo"
   fi
 done
