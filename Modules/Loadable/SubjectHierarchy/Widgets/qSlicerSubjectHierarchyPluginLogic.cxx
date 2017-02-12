20a34f0afe283d4cecf7a04793b58d8c860318ee
Csaba Pinter <csaba.pinter@queensu.ca>
2017-02-12 18:06:59 -0500

    ENH: Added python scripted subject hierarchy plugins
    For SegmentEditor (that replaces the core SegmentPlugin) and SegmentStatistics. Both are mostly sample plugins, to demonstrate how to write scripted plugins, and as a proof of concept for multiple scripted plugins built.
    The reason why SegmentEditorSubjectHierarchyPlugins subfolder was chosen for source and install path: 1. If the plugin is added with the module file, it is attempted to be recognized as a module and will throw error 2. If a common SubjectHierarchyPlugins folder is created in each module having a scripted plugin, the __init__ files are not merged, but the last one overwrites the previous ones, and only one plugin is exposed.

--- Modules/Loadable/SubjectHierarchy/Widgets/qSlicerSubjectHierarchyPluginLogic.cxx
+++ Modules/Loadable/SubjectHierarchy/Widgets/qSlicerSubjectHierarchyPluginLogic.cxx
@@ -33,7 +33,6 @@
 #include "qSlicerSubjectHierarchyChartsPlugin.h"
 #include "qSlicerSubjectHierarchyParseLocalDataPlugin.h"
 #include "qSlicerSubjectHierarchyRegisterPlugin.h"
-#include "qSlicerSubjectHierarchySegmentPlugin.h"
 #include "qSlicerSubjectHierarchyFolderPlugin.h"
 
 // SlicerQt includes
@@ -73,6 +72,8 @@ qSlicerSubjectHierarchyPluginLogicPrivate::qSlicerSubjectHierarchyPluginLogicPri
   : q_ptr(&object)
   , AutoDeleteSubjectHierarchyChildren(false)
 {
+  // Register vtkIdType for use in python for subject hierarchy item IDs
+  qRegisterMetaType<vtkIdType>("vtkIdType");
 }
 
 //-----------------------------------------------------------------------------
@@ -131,8 +132,6 @@ void qSlicerSubjectHierarchyPluginLogic::registerCorePlugins()
     new qSlicerSubjectHierarchyChartsPlugin());
   qSlicerSubjectHierarchyPluginHandler::instance()->registerPlugin(
     new qSlicerSubjectHierarchyRegisterPlugin());
-  qSlicerSubjectHierarchyPluginHandler::instance()->registerPlugin(
-    new qSlicerSubjectHierarchySegmentPlugin());
 }
 
 //-----------------------------------------------------------------------------
---
+++ Modules/Scripted/SegmentEditor/SegmentEditorSubjectHierarchyPlugins/__init__.py
@@ -0,0 +1,1 @@
+from SegmentEditorSubjectHierarchyPlugin import *
