Slicer/SlicerGitSVNArchive

commit 533f2c7fc88a83df1900a7838db803d2f0e77e0b

ENH: Remove sliceBounds assert from qMRMLSliceControllerWidgetPrivate
Removed Q_ASSERT(sliceBounds[4] <= sliceBounds[5]), as invalid slice bounds is not an error (it just means there is no content in the slice view).

git-svn-id: http://svn.slicer.org/Slicer4/trunk@25741 3bd1e089-480b-0410-8dfb-8563597acbee

Libs/MRML/Widgets/qMRMLSliceControllerWidget.cxx
@@ -1266,8 +1266,14 @@ void qMRMLSliceControllerWidgetPrivate::onSliceLogicModifiedEvent()
   // Calculate the number of slices in the current range
   double sliceBounds[6] = {0, -1, 0, -1, 0, -1};
   this->SliceLogic->GetLowestVolumeSliceBounds(sliceBounds);
-  Q_ASSERT(sliceBounds[4] <= sliceBounds[5]);
-  q->setSliceOffsetRange(sliceBounds[4], sliceBounds[5]);
+  if (sliceBounds[4] >= sliceBounds[5])
+    {
+    q->setSliceOffsetRange(sliceBounds[4], sliceBounds[5]);
+    }
+  else
+    {
+    q->setSliceOffsetRange(this->SliceLogic->GetSliceOffset(), this->SliceLogic->GetSliceOffset());
+    }
 
   // Update slider position
   this->SliceOffsetSlider->setValue(this->SliceLogic->GetSliceOffset());

commit 3c13155016e5a1de9dcb3fdb4635ef292156fdd8

BUG: Fix setting of slice viewer offset
This commit fixes a regression introduced in r25741 (ENH: Remove
sliceBounds assert from qMRMLSliceControllerWidgetPrivate) where
the logic originally captured by the Q_ASSERT was incorrectly
adapted.

Reported-by: Neal Siekierski <neal.siekierski@kitware.com>

git-svn-id: http://svn.slicer.org/Slicer4/trunk@25744 3bd1e089-480b-0410-8dfb-8563597acbee

Libs/MRML/Widgets/qMRMLSliceControllerWidget.cxx
@@ -1266,7 +1266,7 @@ void qMRMLSliceControllerWidgetPrivate::onSliceLogicModifiedEvent()
   // Calculate the number of slices in the current range
   double sliceBounds[6] = {0, -1, 0, -1, 0, -1};
   this->SliceLogic->GetLowestVolumeSliceBounds(sliceBounds);
-  if (sliceBounds[4] >= sliceBounds[5])
+  if (sliceBounds[4] <= sliceBounds[5])
     {
     q->setSliceOffsetRange(sliceBounds[4], sliceBounds[5]);
     }