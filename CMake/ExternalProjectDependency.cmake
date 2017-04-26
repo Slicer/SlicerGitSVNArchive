Slicer/SlicerGitSVNArchive

commit 7cafe8c8bc9be7128ffd3179a734a98850539af9

ENH: Update ExternalProjectDependency based on commontk/Artichoke@5ee3d50
This commit improves the support for Ninja generator by ensuring the
option USES_TERMINAL_* options are set to give Ninja access to the
terminal when the CMake version supports it.

See https://cmake.org/cmake/help/v3.4/module/ExternalProject.html

Also Update CTK to integrate a similar update:

$ git shortlog 19fc678..b8587e5 --no-merges
Jean-Christophe Fillion-Robin (2):
      Update ExternalProjectDependency based on commontk/Artichoke@5ee3d50
      ENH: ExternalProjecDependency: Re-apply commontk/CTK@6cc426b

git-svn-id: http://svn.slicer.org/Slicer4/trunk@25929 3bd1e089-480b-0410-8dfb-8563597acbee

CMake/ExternalProjectDependency.cmake
@@ -343,7 +343,14 @@ function(_sb_get_external_project_arguments proj varname)
     list(APPEND _ep_arguments CMAKE_GENERATOR_PLATFORM ${_sb_CMAKE_GENERATOR_PLATFORM})
   endif()
   list(APPEND _ep_arguments CMAKE_GENERATOR_TOOLSET ${_sb_CMAKE_GENERATOR_TOOLSET})
-
+  if(CMAKE_VERSION VERSION_GREATER "3.3")
+    # USES_TERMINAL_* options were introduced in CMake 3.4
+    foreach(step IN ITEMS DOWNLOAD UPDATE CONFIGURE BUILD TEST INSTALL)
+      list(APPEND _ep_arguments
+        USES_TERMINAL_${step}
+        )
+    endforeach()
+  endif()
   set(${varname} ${_ep_arguments} PARENT_SCOPE)
 endfunction()
 

SuperBuild/External_CTK.cmake
@@ -63,7 +63,7 @@ if(NOT DEFINED CTK_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
 
   ExternalProject_SetIfNotDefined(
     ${CMAKE_PROJECT_NAME}_${proj}_GIT_TAG
-    "19fc678c71251a6a825a0fc73ad499529fb8ad03"
+    "b8587e55ee7d6843a6ab2688432ade35fc7581b6"
     QUIET
     )
 

commit 4952fa6387b7feeade86560be6fa3f1adc9a737c

ENH: Update ExternalProjectDependency based on commontk/Artichoke@0fff214
This commit fixes a configure error introduced in r25929 (ENH: Update
ExternalProjectDependency based on commontk/Artichoke@5ee3d50) by ensuring
USES_TERMINAL_* option are not used with CMake 3.3.2.

Also Update CTK to integrate a similar update:

$ git shortlog commontk/CTK@185e6e3..commontk/CTK@a63253f --no-merges
Jean-Christophe Fillion-Robin (1):
      Update ExternalProjectDependency based on commontk/Artichoke@0fff214

Reported-by: Bill Lorensen <bill.lorensen@gmail.com>

git-svn-id: http://svn.slicer.org/Slicer4/trunk@25978 3bd1e089-480b-0410-8dfb-8563597acbee

CMake/ExternalProjectDependency.cmake
@@ -343,7 +343,7 @@ function(_sb_get_external_project_arguments proj varname)
     list(APPEND _ep_arguments CMAKE_GENERATOR_PLATFORM ${_sb_CMAKE_GENERATOR_PLATFORM})
   endif()
   list(APPEND _ep_arguments CMAKE_GENERATOR_TOOLSET ${_sb_CMAKE_GENERATOR_TOOLSET})
-  if(CMAKE_VERSION VERSION_GREATER "3.3")
+  if(CMAKE_VERSION VERSION_EQUAL "3.4" OR CMAKE_VERSION VERSION_GREATER "3.4")
     # USES_TERMINAL_* options were introduced in CMake 3.4
     foreach(step IN ITEMS DOWNLOAD UPDATE CONFIGURE BUILD TEST INSTALL)
       list(APPEND _ep_arguments

SuperBuild/External_CTK.cmake
@@ -63,7 +63,7 @@ if(NOT DEFINED CTK_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
 
   ExternalProject_SetIfNotDefined(
     ${CMAKE_PROJECT_NAME}_${proj}_GIT_TAG
-    "185e6e3ffa1d4b8fa25f32d4c60136d90575bbfe"
+    "a63253f5b0b01016e6780df9489cf6e170cd0fd3"
     QUIET
     )
 