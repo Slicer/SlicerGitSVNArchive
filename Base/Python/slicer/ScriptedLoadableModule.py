0383704c60c7ce1b4e33fc8f8800083197244ba7
Mohamed Moselhy <mohamed.moselhy@uwo.ca>
2017-07-28 13:15:09 -0400

    Update ScriptedLoadableModule.py

--- Base/Python/slicer/ScriptedLoadableModule.py
+++ Base/Python/slicer/ScriptedLoadableModule.py
@@ -142,8 +142,8 @@ class ScriptedLoadableModuleWidget:
     ModuleWizard will substitute correct default moduleName.
     Generic reload method for any scripted module.
     """
-    for i in range(10): print('\n')
-    print('------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------')
+    print('\n' * 10)
+    print('-' * 330)
     slicer.util.reloadScriptedModule(self.moduleName)
 
   def onReloadAndTest(self):
