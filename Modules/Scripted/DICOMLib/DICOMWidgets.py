Slicer/SlicerGitSVNArchive

commit 38d9add492727e24701bdd42e5d744ccfd4d9470

COMP: Update DICOM module to work with Qt5 API
- use setSingleShot method (singleShot is a read-only method)

- use more specifice signal signature with enum

- update dicom browser for Qt5 no keyword C++ constructor from python
  and avoids this runtime error:

```
  File "/home/pieper/slicer4/latest/Slicer-qt5-superbuild/Slicer-build/lib/Slicer-4.7/qt-scripted-modules/DICOMLib/DICOMWidgets.py", line 964, in __init__
    super(DICOMPluginSelector, self).__init__(parent, width=width, height=height)
ValueError: Calling C++ functions with Python keywords is not supported! Function: QWidget(QWidget parent, Qt::WindowFlags f) Keywords: {'width': 50, 'height': 100}
```

From: Steve Pieper <pieper@isomics.com>

git-svn-id: http://svn.slicer.org/Slicer4/trunk@26253 3bd1e089-480b-0410-8dfb-8563597acbee

Modules/Scripted/DICOMLib/DICOMWidgets.py
@@ -1013,7 +1013,7 @@ class DICOMPluginSelector(qt.QWidget):
   """
 
   def __init__(self, parent, width=50, height=100):
-    super(DICOMPluginSelector, self).__init__(parent, width=width, height=height)
+    super(DICOMPluginSelector, self).__init__(parent)
     self.setMinimumHeight(height)
     self.setMinimumWidth(width)
     self.setLayout(qt.QVBoxLayout())
@@ -1076,10 +1076,10 @@ class DICOMLoadableTable(qt.QTableWidget):
     self.setColumnCount(3)
     self.setHorizontalHeaderLabels(['DICOM Data', 'Reader', 'Warnings'])
     self.setSelectionBehavior(qt.QTableView.SelectRows)
-    self.horizontalHeader().setResizeMode(qt.QHeaderView.Stretch)
-    self.horizontalHeader().setResizeMode(0, qt.QHeaderView.ResizeToContents)
-    self.horizontalHeader().setResizeMode(1, qt.QHeaderView.Stretch)
-    self.horizontalHeader().setResizeMode(2, qt.QHeaderView.Stretch)
+    self.horizontalHeader().setSectionResizeMode(qt.QHeaderView.Stretch)
+    self.horizontalHeader().setSectionResizeMode(0, qt.QHeaderView.ResizeToContents)
+    self.horizontalHeader().setSectionResizeMode(1, qt.QHeaderView.Stretch)
+    self.horizontalHeader().setSectionResizeMode(2, qt.QHeaderView.Stretch)
 
   def addLoadableRow(self, loadable, row, reader):
     self.insertRow(row)
@@ -1172,9 +1172,9 @@ class DICOMHeaderWidget(qt.QTableWidget):
   def configure(self):
     self.setColumnCount(2)
     self.setHorizontalHeaderLabels(['Tag', 'Value'])
-    self.horizontalHeader().setResizeMode(qt.QHeaderView.Stretch)
-    self.horizontalHeader().setResizeMode(0, qt.QHeaderView.Stretch)
-    self.horizontalHeader().setResizeMode(1, qt.QHeaderView.Stretch)
+    self.horizontalHeader().setSectionResizeMode(qt.QHeaderView.Stretch)
+    self.horizontalHeader().setSectionResizeMode(0, qt.QHeaderView.Stretch)
+    self.horizontalHeader().setSectionResizeMode(1, qt.QHeaderView.Stretch)
 
   def setHeader(self, dcmFile=None):
     #TODO: this method never gets called. Should be called when clicking on items from the SeriesTable

Modules/Scripted/DICOM/DICOM.py
@@ -228,7 +228,7 @@ class DICOMWidget:
     # - if the update is requested before the timeout, the call to timer.start() resets it
     # - the actual update only happens when the the full time elapses since the last request
     self.updateRecentActivityTimer = qt.QTimer()
-    self.updateRecentActivityTimer.singleShot = True
+    self.updateRecentActivityTimer.setSingleShot(True)
     self.updateRecentActivityTimer.interval = 500
     self.updateRecentActivityTimer.connect('timeout()', self.onUpateRecentActivityRequestTimeout)
 
@@ -288,7 +288,7 @@ class DICOMWidget:
     self.toggleListener.checkable = True
     if hasattr(slicer, 'dicomListener'):
       self.toggleListener.text = "Stop Listener"
-      slicer.dicomListener.process.connect('stateChanged(int)',self.onListenerStateChanged)
+      slicer.dicomListener.process.connect('stateChanged(QProcess::ProcessState)',self.onListenerStateChanged)
     else:
       self.toggleListener.text = "Start Listener"
     self.localFrame.layout().addWidget(self.toggleListener)

commit 1412ba3b71619eb2cad76d1635d130ce4270a31f

BUG: Update DICOMWidget to fix regression breaking DICOM tests using Qt4
This commit fixes a regression introduced in r26253 (COMP: Update DICOM
module to work with Qt5 API) and ensures the DICOMWidget can work with
both Qt4 and Qt5.

Tests fixed by this commit are the following:

* py_DICOMReaders
* py_nomainwindow_qSlicerDICOMModuleGenericTest
* py_JRC2013Vis
* py_SubjectHierarchyGenericSelfTest
* py_RSNAVisTutorial

git-svn-id: http://svn.slicer.org/Slicer4/trunk@26272 3bd1e089-480b-0410-8dfb-8563597acbee

Modules/Scripted/DICOMLib/DICOMWidgets.py
@@ -2,12 +2,16 @@ import os, copy
 import qt
 import vtk
 import logging
+
+from packaging import version
+
 from ctk import ctkDICOMObjectListWidget, ctkDICOMDatabase, ctkDICOMIndexer, ctkDICOMBrowser, ctkPopupWidget, ctkExpandButton
 import slicer
 from slicer.util import VTKObservationMixin
 
 from slicer.util import settingsValue, toBool
 import DICOMLib
+
 
 #########################################################
 #
@@ -1054,6 +1058,13 @@ class DICOMPluginSelector(qt.QWidget):
     return selectedPlugins
 
 
+def _setSectionResizeMode(header, *args, **kwargs):
+  if version.parse(qt.Qt.qVersion()) < version.parse("5.0.0"):
+    header.setResizeMode(*args, **kwargs)
+  else:
+    header.setSectionResizeMode(*args, **kwargs)
+
+
 class DICOMLoadableTable(qt.QTableWidget):
   """Implement the Qt code for a table of
   selectable slicer data to be made from
@@ -1076,10 +1087,10 @@ class DICOMLoadableTable(qt.QTableWidget):
     self.setColumnCount(3)
     self.setHorizontalHeaderLabels(['DICOM Data', 'Reader', 'Warnings'])
     self.setSelectionBehavior(qt.QTableView.SelectRows)
-    self.horizontalHeader().setSectionResizeMode(qt.QHeaderView.Stretch)
-    self.horizontalHeader().setSectionResizeMode(0, qt.QHeaderView.ResizeToContents)
-    self.horizontalHeader().setSectionResizeMode(1, qt.QHeaderView.Stretch)
-    self.horizontalHeader().setSectionResizeMode(2, qt.QHeaderView.Stretch)
+    _setSectionResizeMode(self.horizontalHeader(), qt.QHeaderView.Stretch)
+    _setSectionResizeMode(self.horizontalHeader(), 0, qt.QHeaderView.ResizeToContents)
+    _setSectionResizeMode(self.horizontalHeader(), 1, qt.QHeaderView.Stretch)
+    _setSectionResizeMode(self.horizontalHeader(), 2, qt.QHeaderView.Stretch)
 
   def addLoadableRow(self, loadable, row, reader):
     self.insertRow(row)
@@ -1172,9 +1183,9 @@ class DICOMHeaderWidget(qt.QTableWidget):
   def configure(self):
     self.setColumnCount(2)
     self.setHorizontalHeaderLabels(['Tag', 'Value'])
-    self.horizontalHeader().setSectionResizeMode(qt.QHeaderView.Stretch)
-    self.horizontalHeader().setSectionResizeMode(0, qt.QHeaderView.Stretch)
-    self.horizontalHeader().setSectionResizeMode(1, qt.QHeaderView.Stretch)
+    _setSectionResizeMode(self.horizontalHeader(), qt.QHeaderView.Stretch)
+    _setSectionResizeMode(self.horizontalHeader(), 0, qt.QHeaderView.Stretch)
+    _setSectionResizeMode(self.horizontalHeader(), 1, qt.QHeaderView.Stretch)
 
   def setHeader(self, dcmFile=None):
     #TODO: this method never gets called. Should be called when clicking on items from the SeriesTable