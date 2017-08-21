Slicer/SlicerGitSVNArchive

commit ec56773b54bc062e07c1d8a20109d811f05dc820

ENH: Update CTKAppLauncherLib adding support for Qt5
$ git shortlog 5b592f1..685dd47 --no-merges
Jean-Christophe Fillion-Robin (1):
      Add support for Qt5

Max Smolens (2):
      COMP: Fix Qt4 required components
      COMP: Use proper Qt4 component capitalization

git-svn-id: http://svn.slicer.org/Slicer4/trunk@26217 3bd1e089-480b-0410-8dfb-8563597acbee

SuperBuild/External_CTKAppLauncherLib.cmake
@@ -26,7 +26,7 @@ if(NOT DEFINED CTKAppLauncherLib_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${
 
   ExternalProject_SetIfNotDefined(
     ${CMAKE_PROJECT_NAME}_${proj}_GIT_TAG
-    "5b592f1baaa7dd41d68e1cd56a2a82e298196ebe"
+    "685dd47523617bb52fbafa774c0f1d6b2244640e"
     QUIET
     )
 

commit 468e2d0c6ca230084b12b2cab9c3479da32e45f3

COMP: Update CTKAppLauncherLib to fix extension build error on macOS
This commit fixes error like this one:

```
make[8]: *** No rule to make target `/Users/kitware/Dashboards/Support/qt-everywhere-opensource-build-4.8.7/lib/libQtUiTools.a/QtUiTools',
needed by `lib/Slicer-4.7/qt-loadable-modules/libqSlicerDicomRtImportExportSubjectHierarchyPlugins.dylib'.
Stop
```

and ensures that Qt libraries are handled similarly when building both
Slicer and Slicer extensions.

It addresses a regression introduced in r26217 (ENH: Update CTKAppLauncherLib
adding support for Qt5) where CTKAppLauncherLibConfig was explicitly
setting QT_USE_IMPORTED_TARGETS to 1 when using Qt4.

Root cause:

Since on MacOSX not all modules are available as "framework" (e.g libQtUiTools.a),
the assumption made in "FindQt4.cmake" is not always valid and systematically
setting the imported libraries location to framework location is incorrect.

See https://gitlab.kitware.com/cmake/cmake/issues/17195


Changes:

$ git shortlog 685dd47..5aa21a3 --no-merges
Jean-Christophe Fillion-Robin (1):
      CTKAppLauncherLibConfig: Do not force use of Qt4 imported targets

git-svn-id: http://svn.slicer.org/Slicer4/trunk@26296 3bd1e089-480b-0410-8dfb-8563597acbee

SuperBuild/External_CTKAppLauncherLib.cmake
@@ -26,7 +26,7 @@ if(NOT DEFINED CTKAppLauncherLib_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${
 
   ExternalProject_SetIfNotDefined(
     ${CMAKE_PROJECT_NAME}_${proj}_GIT_TAG
-    "685dd47523617bb52fbafa774c0f1d6b2244640e"
+    "5aa21a383abdc65a031114b3198531f449159935"
     QUIET
     )
 