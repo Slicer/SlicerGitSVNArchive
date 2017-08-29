Slicer/SlicerGitSVNArchive

commit 31ce4da19af5f6e55cd8ff1f1b4c786b6fbfa697

COMP: Fix VTK python wrapping of extension built against Slicer with VTK8
This commit ensures the complete set of include directories is provided
to the vtkWrapHierarchy executable.

This problem was discovered while adding support for Qt5 and VTK8 to
the CarreraSlice extension.

It fixes the following error where vtkTypeMacro was improperly considered
as a function:

```
In file included from /home/jcfr/Projects/Slicer-2-Qt5-VTK8-build/VTKv8/Common/Core/vtkVariant.h:37:0,
                 from /tmp/CarreraSlice-build/src/fastGrowCut/Logic/vtkFastGrowCutPython.cxx:10:
/tmp/CarreraSlice-build/src/fastGrowCut/Logic/vtkFastGrowCutPython.cxx: In function ‘PyObject* PyvtkFastGrowCut_vtkTypeMacro(PyObject*, PyObject*)’:
/home/jcfr/Projects/Slicer-2-Qt5-VTK8-build/VTKv8/Common/Core/vtkSetGet.h:630:3: error: expected unqualified-id before ‘protected’
   protected: \
   ^
/home/jcfr/Projects/Slicer-2-Qt5-VTK8-build/VTKv8/Common/Core/vtkSetGet.h:664:3: note: in expansion of macro ‘vtkAbstractTypeMacroWithNewInstanceType’
   vtkAbstractTypeMacroWithNewInstanceType(thisClass, superclass, thisClass, #thisClass) \
   ^
/home/jcfr/Projects/Slicer-2-Qt5-VTK8-build/VTKv8/Common/Core/vtkSetGet.h:670:3: note: in expansion of macro ‘vtkAbstractTypeMacro’
   vtkAbstractTypeMacro(thisClass, superclass) \
   ^
/tmp/CarreraSlice-build/src/fastGrowCut/Logic/vtkFastGrowCutPython.cxx:45:11: note: in expansion of macro ‘vtkTypeMacro’
       op->vtkTypeMacro(*temp0, *temp1);
           ^
```

git-svn-id: http://svn.slicer.org/Slicer4/trunk@26313 3bd1e089-480b-0410-8dfb-8563597acbee

CMake/vtkMacroKitPythonWrap.cmake
@@ -80,7 +80,7 @@ macro(vtkMacroKitPythonWrap)
       # Create list of wrapping dependencies for generating the hierarchy file.
       # Also create list of include directories of dependencies for wrapping.
       set(_kit_wrap_depends)
-      set(_kit_wrap_include_dirs)
+      set(_kit_wrap_include_dirs ${VTK_INCLUDE_DIRS})
 
       # Add kit include dirs
       list(APPEND _kit_wrap_include_dirs ${CMAKE_CURRENT_SOURCE_DIR} ${CMAKE_CURRENT_BINARY_DIR})

Libs/vtkAddon/CMakeLists.txt
@@ -93,10 +93,6 @@ set(lib_name ${PROJECT_NAME})
 set(srcs ${vtkAddon_SRCS})
 add_library(${lib_name} ${srcs})
 
-# Teem vtkNRRDReader overwrites VTK vtkNrrdReader
-set(VTK_WRAP_INCLUDE_DIRS ${CMAKE_CURRENT_SOURCE_DIR} ${VTK_INCLUDE_DIRS}
-    CACHE INTERNAL "VTK wrap include dirs" FORCE)
-
 set(libs
   ${VTK_LIBRARIES}
   )

Libs/vtkTeem/CMakeLists.txt
@@ -86,10 +86,6 @@ set(lib_name ${PROJECT_NAME})
 set(srcs ${vtkTeem_SRCS})
 add_library(${lib_name} ${srcs})
 
-# Teem vtkNRRDReader overwrites VTK vtkNrrdReader
-set(VTK_WRAP_INCLUDE_DIRS ${CMAKE_CURRENT_SOURCE_DIR} ${VTK_INCLUDE_DIRS}
-    CACHE INTERNAL "VTK wrap include dirs" FORCE)
-
 set(libs
   ${Teem_LIBRARIES}
   ${VTK_LIBRARIES}

commit 4bf9a5eaaa752da6dfea5cd3cadd8acb1e32a1dd

COMP: Fix regression in VTK python wrapping built using VTK7
This commit fixes a regression introduced in previous commit r26313 (COMP: Fix
VTK python wrapping of extension built against Slicer with VTK8)

It ensures that VTK_WRAP_INCLUDE_DIRS is also set when building
with vTK <= 8.

It fixes the following error reported when building EMSegment python
wrappers:

```
/path/to/Slicer-build/Slicer-build/Modules/Remote/EMSegment/Qt/Logic/vtkImageEMLocalGenericClassPython.cxx: In function ‘PyObject* PyvtkImageEMLocalGenericClass_SetProbDataPtr(PyObject*, PyObject*)’:
/path/to/Slicer-build/Slicer-build/Modules/Remote/EMSegment/Qt/Logic/vtkImageEMLocalGenericClassPython.cxx:262:11: error: ‘class vtkImageEMLocalGenericClass’ has no member named ‘SetProbDataPtr’
       op->SetProbDataPtr(temp0);
           ^
/path/to/Slicer-build/Slicer-build/Modules/Remote/EMSegment/Qt/Logic/vtkImageEMLocalGenericClassPython.cxx:266:40: error: ‘class vtkImageEMLocalGenericClass’ has no member named ‘SetProbDataPtr’
       op->vtkImageEMLocalGenericClass::SetProbDataPtr(temp0);
                                        ^
```

git-svn-id: http://svn.slicer.org/Slicer4/trunk@26314 3bd1e089-480b-0410-8dfb-8563597acbee

CMake/vtkMacroKitPythonWrap.cmake
@@ -73,46 +73,45 @@ macro(vtkMacroKitPythonWrap)
     set(TMP_WRAP_FILES ${MY_KIT_SRCS} ${MY_KIT_WRAP_HEADERS})
     set(_wrap_hierarchy_stamp_file)
 
+    # Create list of wrapping dependencies for generating the hierarchy file.
+    set(_kit_wrap_depends)
+    set(_kit_wrap_include_dirs ${VTK_INCLUDE_DIRS})
+
+    # Add kit include dirs
+    list(APPEND _kit_wrap_include_dirs ${CMAKE_CURRENT_SOURCE_DIR} ${CMAKE_CURRENT_BINARY_DIR})
+    list(APPEND _kit_wrap_include_dirs ${Slicer_Base_INCLUDE_DIRS})
+    set(_kit_include_dirs ${${MY_KIT_NAME}_INCLUDE_DIRS})
+    if(_kit_include_dirs)
+      list(APPEND _kit_wrap_include_dirs ${_kit_include_dirs})
+    endif()
+
+    # Add VTK dependencies
+    foreach(_dep ${VTK_LIBRARIES})
+      list(APPEND _kit_wrap_depends ${_dep})
+    endforeach()
+
+    # Recursively add dependencies and get their include directories
+    foreach(_dep ${MY_KIT_PYTHON_LIBRARIES})
+      set(_${MY_KIT_NAME}_wrap_depends)
+      set(_${MY_KIT_NAME}_wrap_include_dirs)
+      _get_dependencies_recurse("${MY_KIT_NAME}" "${_dep}")
+      list(APPEND _kit_wrap_depends ${_${MY_KIT_NAME}_wrap_depends})
+      list(APPEND _kit_wrap_include_dirs ${_${MY_KIT_NAME}_wrap_include_dirs})
+    endforeach()
+
+    if(_kit_wrap_depends)
+      list(REMOVE_DUPLICATES _kit_wrap_depends)
+    endif()
+    if(_kit_wrap_include_dirs)
+      list(REMOVE_DUPLICATES _kit_wrap_include_dirs)
+    endif()
+
+    # Update list of include directories for wrapper tool command lines
+    list(APPEND VTK_WRAP_INCLUDE_DIRS ${_kit_wrap_include_dirs})
+
     # Generate hierarchy files for VTK8 and later
     if(NOT ${Slicer_VTK_VERSION_MAJOR} VERSION_LESS 8)
       include(${VTK_CMAKE_DIR}/vtkWrapHierarchy.cmake)
-
-      # Create list of wrapping dependencies for generating the hierarchy file.
-      # Also create list of include directories of dependencies for wrapping.
-      set(_kit_wrap_depends)
-      set(_kit_wrap_include_dirs ${VTK_INCLUDE_DIRS})
-
-      # Add kit include dirs
-      list(APPEND _kit_wrap_include_dirs ${CMAKE_CURRENT_SOURCE_DIR} ${CMAKE_CURRENT_BINARY_DIR})
-      list(APPEND _kit_wrap_include_dirs ${Slicer_Base_INCLUDE_DIRS})
-      set(_kit_include_dirs ${${MY_KIT_NAME}_INCLUDE_DIRS})
-      if(_kit_include_dirs)
-        list(APPEND _kit_wrap_include_dirs ${_kit_include_dirs})
-      endif()
-
-      # Add VTK dependencies
-      foreach(_dep ${VTK_LIBRARIES})
-        list(APPEND _kit_wrap_depends ${_dep})
-      endforeach()
-
-      # Recursively add dependencies and get their include directories
-      foreach(_dep ${MY_KIT_PYTHON_LIBRARIES})
-        set(_${MY_KIT_NAME}_wrap_depends)
-        set(_${MY_KIT_NAME}_wrap_include_dirs)
-        _get_dependencies_recurse("${MY_KIT_NAME}" "${_dep}")
-        list(APPEND _kit_wrap_depends ${_${MY_KIT_NAME}_wrap_depends})
-        list(APPEND _kit_wrap_include_dirs ${_${MY_KIT_NAME}_wrap_include_dirs})
-      endforeach()
-
-      if(_kit_wrap_depends)
-        list(REMOVE_DUPLICATES _kit_wrap_depends)
-      endif()
-      if(_kit_wrap_include_dirs)
-        list(REMOVE_DUPLICATES _kit_wrap_include_dirs)
-      endif()
-
-      # Update list of include directories for wrapper tool command lines
-      list(APPEND VTK_WRAP_INCLUDE_DIRS ${_kit_wrap_include_dirs})
 
       # Set variables for this and future runs of vtk_wrap_hierarchy:
       #  - <module_name>_WRAP_DEPENDS
@@ -216,4 +215,3 @@ macro(vtkMacroKitPythonWrap)
   endif()
 
 endmacro()
-