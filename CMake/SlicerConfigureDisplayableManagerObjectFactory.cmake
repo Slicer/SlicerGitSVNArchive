Slicer/SlicerGitSVNArchive

commit e38d2729b2d4b860d9e5a8e1369b0e9f0132eff6

COMP: Support VTK 8.1 updating displayable manager to use vtkObjectFactory
This commit introduces the CMake function "SlicerConfigureDisplayableManagerObjectFactory"
used to configure VTK object factory allowing to instantiate displayable
manager given the class name.

It updates SlicerMacroBuildModuleLogic and qSlicerExport.h.in so that
the generated object factories are properly initialized.

It removes the use of vtkInstantiator that is deprecated following
kitware/VTK@8328eda merged on 2017-09-26.

See https://gitlab.kitware.com/vtk/vtk/merge_requests/3324

Co-authored-by: Jean-Baptiste Vimort <jb.vimort@kitware.com>

git-svn-id: http://svn.slicer.org/Slicer4/trunk@26458 3bd1e089-480b-0410-8dfb-8563597acbee

CMake/SlicerConfigureDisplayableManagerObjectFactory.cmake
@@ -1,0 +1,122 @@
+
+#
+# Configure Slicer displayable manager VTK object factory
+#
+# Given a list of displayable manager source files, this function will
+# generate sources responsible for registering the classes with the
+# vtkObjectFactory.
+#
+# This function set the variable "${TARGET_NAME}_AUTOINIT" to 1 in the parent
+# scope. This tells SlicerMacroBuildModuleLogic to set Slicer_EXPORT_HEADER_CUSTOM_CONTENT
+# and define the associated ${TARGET_NAME}_AUTOINIT compilation flag.
+#
+function(SlicerConfigureDisplayableManagerObjectFactory)
+  set(options
+    NO_EXPORT
+    )
+  set(oneValueArgs
+    TARGET_NAME
+    EXPORT_MACRO
+    EXPORT_HEADER
+    OUTPUT_SRCS_VAR
+    )
+  set(multiValueArgs
+    SRCS
+    )
+  CMAKE_PARSE_ARGUMENTS(DM_OBJECT_FACTORY
+    "${options}"
+    "${oneValueArgs}"
+    "${multiValueArgs}"
+    ${ARGN}
+    )
+
+  # Sanity checks
+  if(DM_OBJECT_FACTORY_UNPARSED_ARGUMENTS)
+    message(FATAL_ERROR "Unknown keywords given to SlicerConfigureDisplayableManagerObjectFactory(): \"${DM_OBJECT_FACTORY_UNPARSED_ARGUMENTS}\"")
+  endif()
+  set(expected_nonempty_vars
+    TARGET_NAME
+    OUTPUT_SRCS_VAR
+    SRCS
+    )
+  if(NOT DM_OBJECT_FACTORY_NO_EXPORT)
+    list(APPEND expected_nonempty_vars
+      EXPORT_MACRO
+      EXPORT_HEADER
+      )
+  endif()
+  foreach(var ${expected_nonempty_vars})
+    if("${DM_OBJECT_FACTORY_${var}}" STREQUAL "")
+      message(FATAL_ERROR "error: ${var} CMake variable is empty !")
+    endif()
+  endforeach()
+
+  # Initialize local variables
+  set(_vtk_override_includes)
+  set(_vtk_override_creates )
+
+  # For each class
+  foreach(FILE ${DM_OBJECT_FACTORY_SRCS})
+
+    # what is the filename without the extension
+    get_filename_component(TMP_FILENAME ${FILE} NAME_WE)
+
+    set(_class ${TMP_FILENAME})
+    set(_override ${_class})
+
+    set(_vtk_override_includes
+      "${_vtk_override_includes}#include \"${_override}.h\"\n")
+
+    set(_vtk_override_creates "${_vtk_override_creates}
+      VTK_CREATE_CREATE_FUNCTION(${_override})")
+
+    set(_vtk_override_do "${_vtk_override_do}
+    this->RegisterOverride(\"${_class}\",
+                           \"${_override}\",
+                           \"Override for ${vtk-module} module\", 1,
+                           vtkObjectFactoryCreate${_override});")
+
+  endforeach()
+
+  set(vtk-module ${DM_OBJECT_FACTORY_TARGET_NAME})
+
+  if(DM_OBJECT_FACTORY_NO_EXPORT)
+    string(TOUPPER ${DM_OBJECT_FACTORY_TARGET_NAME} VTK-MODULE)
+    set(_export_header "#define ${VTK-MODULE}_EXPORT")
+  else()
+    string(REPLACE "_EXPORT" "" VTK-MODULE "${DM_OBJECT_FACTORY_EXPORT_MACRO}")
+    set(_export_header "#include \"${DM_OBJECT_FACTORY_EXPORT_HEADER}\"")
+  endif()
+  set(CMAKE_CONFIGURABLE_FILE_CONTENT "
+#ifndef __${vtk-module}Module_h
+#define __${vtk-module}Module_h
+${_export_header}
+#endif
+")
+  configure_file(
+    ${CMAKE_ROOT}/Modules/CMakeConfigurableFile.in
+    ${CMAKE_CURRENT_BINARY_DIR}/${vtk-module}Module.h
+    )
+  configure_file(
+    ${VTK_CMAKE_DIR}/vtkObjectFactory.h.in
+    ${CMAKE_CURRENT_BINARY_DIR}/${vtk-module}ObjectFactory.h
+    )
+  configure_file(
+    ${VTK_CMAKE_DIR}/vtkObjectFactory.cxx.in
+    ${CMAKE_CURRENT_BINARY_DIR}/${vtk-module}ObjectFactory.cxx
+    )
+
+  set_source_files_properties(
+    ${CMAKE_CURRENT_BINARY_DIR}/${vtk-module}ObjectFactory.cxx
+    PROPERTIES GENERATED 1 WRAP_EXCLUDE 1 ABSTRACT 0
+    )
+
+  # add the source file to the source list
+  set(${DM_OBJECT_FACTORY_OUTPUT_SRCS_VAR}
+    ${${DM_OBJECT_FACTORY_OUTPUT_SRCS_VAR}}
+    ${CMAKE_CURRENT_BINARY_DIR}/${vtk-module}ObjectFactory.cxx
+    PARENT_SCOPE
+    )
+
+  set(${DM_OBJECT_FACTORY_TARGET_NAME}_AUTOINIT 1 PARENT_SCOPE)
+endfunction()

CMake/SlicerMacroBuildModuleLogic.cmake
@@ -86,6 +86,20 @@ macro(SlicerMacroBuildModuleLogic)
     set(MODULELOGIC_NO_INSTALL_OPTION "NO_INSTALL")
   endif()
 
+  set(Slicer_EXPORT_HEADER_CUSTOM_CONTENT)
+  if(${MODULELOGIC_NAME}_AUTOINIT)
+    set(Slicer_EXPORT_HEADER_CUSTOM_CONTENT "
+      // ####### Expanded from \@Slicer_EXPORT_HEADER_CUSTOM_CONTENT\@ #######
+      #if defined(${MODULELOGIC_NAME}_AUTOINIT)
+      # include \"${MODULELOGIC_NAME}ObjectFactory.h\"
+      # include \"vtkAutoInit.h\"
+      VTK_AUTOINIT(${MODULELOGIC_NAME})
+      #endif
+      // ##################################################
+      "
+      )
+  endif()
+
   SlicerMacroBuildModuleVTKLibrary(
     NAME ${MODULELOGIC_NAME}
     EXPORT_DIRECTIVE ${MODULELOGIC_EXPORT_DIRECTIVE}
@@ -95,6 +109,13 @@ macro(SlicerMacroBuildModuleLogic)
     TARGET_LIBRARIES ${MODULELOGIC_TARGET_LIBRARIES}
     ${MODULELOGIC_NO_INSTALL_OPTION}
     )
+
+  if(${MODULELOGIC_NAME}_AUTOINIT)
+    set_property(TARGET ${MODULELOGIC_NAME}
+      APPEND PROPERTY COMPILE_DEFINITIONS
+      "${PROJECT_NAME}_AUTOINIT=1(${MODULELOGIC_NAME})"
+      )
+  endif()
 
   set_property(GLOBAL APPEND PROPERTY SLICER_MODULE_LOGIC_TARGETS ${MODULELOGIC_NAME})
 

Libs/MRML/DisplayableManager/vtkMRMLDisplayableManagerExport.h
@@ -26,4 +26,10 @@
 #define VTK_MRML_DISPLAYABLEMANAGER_EXPORT
 #endif
 
+#if defined(MRMLDisplayableManager_AUTOINIT)
+#include "MRMLDisplayableManagerObjectFactory.h"
+#include "vtkAutoInit.h"
+VTK_AUTOINIT(MRMLDisplayableManager)
+#endif
+
 #endif

CMake/qSlicerExport.h.in
@@ -28,5 +28,5 @@
 #else
  #define @MY_LIBRARY_EXPORT_DIRECTIVE@
 #endif
-
+@Slicer_EXPORT_HEADER_CUSTOM_CONTENT@
 #endif

Libs/MRML/DisplayableManager/CMakeLists.txt
@@ -124,15 +124,20 @@ if(MRMLDisplayableManager_USE_PYTHON)
     )
 endif()
 
-# Create the instantiator for DisplayableManager classes.
-include(${VTK_CMAKE_DIR}/vtkMakeInstantiator.cmake)
-VTK_MAKE_INSTANTIATOR3("${PROJECT_NAME}Instantiator"
-  DisplayableManagerInstantiator_SRCS
-  "${DisplayableManager_SRCS}"
-  "VTK_MRML_DISPLAYABLEMANAGER_EXPORT"
-  ${PROJECT_BINARY_DIR}
-  "vtk${PROJECT_NAME}Export.h")
+# --------------------------------------------------------------------------
+set(lib_name ${PROJECT_NAME})
 
+# --------------------------------------------------------------------------
+# Create the instantiator for DisplayableManager classes.
+# --------------------------------------------------------------------------
+include(SlicerConfigureDisplayableManagerObjectFactory)
+SlicerConfigureDisplayableManagerObjectFactory(
+  TARGET_NAME ${lib_name}
+  SRCS "${DisplayableManager_SRCS}"
+  EXPORT_MACRO "VTK_MRML_DISPLAYABLEMANAGER_EXPORT"
+  EXPORT_HEADER "vtk${PROJECT_NAME}Export.h"
+  OUTPUT_SRCS_VAR DisplayableManagerInstantiator_SRCS
+  )
 set(srcs
   ${KIT_SRCS}
   ${DisplayableManagerInstantiator_SRCS}
@@ -141,7 +146,6 @@ set(srcs
 # --------------------------------------------------------------------------
 # Build library
 # --------------------------------------------------------------------------
-set(lib_name ${PROJECT_NAME})
 add_library(${lib_name} ${srcs})
 
 set(libs
@@ -162,6 +166,13 @@ if(MRMLDisplayableManager_USE_PYTHON)
 endif()
 
 target_link_libraries(${lib_name} ${libs})
+
+if(${lib_name}_AUTOINIT)
+  set_property(TARGET ${lib_name}
+    APPEND PROPERTY COMPILE_DEFINITIONS
+    "${lib_name}_AUTOINIT=1(${lib_name})"
+    )
+endif()
 
 # Apply user-defined properties to the library target.
 if(Slicer_LIBRARY_PROPERTIES)

Libs/MRML/DisplayableManager/Testing/Cxx/CMakeLists.txt
@@ -26,12 +26,12 @@ set(DisplayableManagerTest_SRCS
   )
 
 # Create the instantiator for DisplayableManager classes.
-VTK_MAKE_INSTANTIATOR3("${PROJECT_NAME}TestInstantiator"
-  DisplayableManagerInstantiatorTest_SRCS
-  "${DisplayableManagerTest_SRCS}"
-  ""
-  ${PROJECT_BINARY_DIR}
-  "")
+SlicerConfigureDisplayableManagerObjectFactory(
+  TARGET_NAME ${KIT}CxxTests
+  SRCS "${DisplayableManagerTest_SRCS}"
+  NO_EXPORT
+  OUTPUT_SRCS_VAR DisplayableManagerInstantiatorTest_SRCS
+  )
 
 add_executable(${KIT}CxxTests
   ${Tests}

Libs/MRML/DisplayableManager/Testing/Cxx/vtkMRMLThreeDViewDisplayableManagerFactoryTest1.cxx
@@ -35,8 +35,6 @@
 #include <vtkRenderer.h>
 #include <vtkSmartPointer.h>
 
-// STD includes
-
 //----------------------------------------------------------------------------
 int vtkMRMLThreeDViewDisplayableManagerFactoryTest1(int vtkNotUsed(argc), char* vtkNotUsed(argv)[])
 {

Libs/MRML/DisplayableManager/vtkMRMLDisplayableManagerGroup.cxx
@@ -33,7 +33,6 @@
 
 // VTK includes
 #include <vtkCallbackCommand.h>
-#include <vtkInstantiator.h>
 #include <vtkObjectFactory.h>
 #include <vtkRenderer.h>
 #include <vtkRenderWindow.h>
@@ -134,7 +133,7 @@ bool vtkMRMLDisplayableManagerGroup
 {
   // Check if displayableManagerName is a valid displayable manager
   vtkSmartPointer<vtkObject> objectSmartPointer;
-  objectSmartPointer.TakeReference(vtkInstantiator::CreateInstance(displayableManagerName));
+  objectSmartPointer.TakeReference(vtkObjectFactory::CreateInstance(displayableManagerName));
   if (objectSmartPointer.GetPointer() &&
       objectSmartPointer->IsA("vtkMRMLAbstractDisplayableManager"))
     {
@@ -171,7 +170,7 @@ vtkMRMLAbstractDisplayableManager* vtkMRMLDisplayableManagerGroup
 #endif
     // Object will be unregistered when the SmartPointer will go out-of-scope
     displayableManager = vtkMRMLAbstractDisplayableManager::SafeDownCast(
-      vtkInstantiator::CreateInstance(displayableManagerName));
+      vtkObjectFactory::CreateInstance(displayableManagerName));
 #ifdef MRMLDisplayableManager_USE_PYTHON
     }
 #endif

commit 19b54de734a496ad6f76d5e7f1d670677eb7b141

COMP: DisplayableManager: Fix windows link error
This commit fixes "redefinition; different linkage" error reported on
windows. The regression was introduced in r26458 (COMP: Support VTK 8.1
updating displayable manager to use vtkObjectFactory)

It implements the following changes:

* It removes redundant include of ObjectFactory implementation
  from export header. Since it is already part of the translation unit,
  there is no need to add it again here.

* It disables the export of object factory AutoInit_Construct and
  AutoInit_Destruct symbols. Since the factory is initialized in the
  same translation unit using "VTK_AUTOINIT", these symbols do not need
  to be exported.

* It also fixes a typo in vtkMRMLDisplayableManagerExport.h changing
  VTKMRMLDisplayableManager_STATIC into MRMLDisplayableManager_STATIC.


Content of the error:

```
C:\S4R\Slicer-build\Libs\MRML\DisplayableManager\MRMLDisplayableManagerObjectFactory.cxx(106): error C2375: 'MRMLDisplayableManager_AutoInit_Construct' : redefinition; different linkage [C:\S4R\Slicer-build\Libs\MRML\DisplayableManager\MRMLDisplayableManager.vcxproj] [C:\S4R\Slicer.vcxproj]

            C:\S4\Libs\MRML\DisplayableManager\vtkMRMLDisplayableManagerExport.h(32) : see declaration of 'MRMLDisplayableManager_AutoInit_Construct'

C:\S4R\Slicer-build\Libs\MRML\DisplayableManager\MRMLDisplayableManagerObjectFactory.cxx(121): error C2375: 'MRMLDisplayableManager_AutoInit_Destruct' : redefinition; different linkage [C:\S4R\Slicer-build\Libs\MRML\DisplayableManager\MRMLDisplayableManager.vcxproj] [C:\S4R\Slicer.vcxproj]

            C:\S4\Libs\MRML\DisplayableManager\vtkMRMLDisplayableManagerExport.h(32) : see declaration of 'MRMLDisplayableManager_AutoInit_Destruct'
```

Reported-by: Andras Lasso <lasso@queensu.ca>

CMake/SlicerConfigureDisplayableManagerObjectFactory.cmake
@@ -98,11 +98,11 @@ ${_export_header}
     ${CMAKE_CURRENT_BINARY_DIR}/${vtk-module}Module.h
     )
   configure_file(
-    ${VTK_CMAKE_DIR}/vtkObjectFactory.h.in
+    ${Slicer_CMAKE_DIR}/vtkSlicerObjectFactory.h.in
     ${CMAKE_CURRENT_BINARY_DIR}/${vtk-module}ObjectFactory.h
     )
   configure_file(
-    ${VTK_CMAKE_DIR}/vtkObjectFactory.cxx.in
+    ${Slicer_CMAKE_DIR}/vtkSlicerObjectFactory.cxx.in
     ${CMAKE_CURRENT_BINARY_DIR}/${vtk-module}ObjectFactory.cxx
     )
 

CMake/SlicerMacroBuildModuleLogic.cmake
@@ -91,7 +91,6 @@ macro(SlicerMacroBuildModuleLogic)
     set(Slicer_EXPORT_HEADER_CUSTOM_CONTENT "
       // ####### Expanded from \@Slicer_EXPORT_HEADER_CUSTOM_CONTENT\@ #######
       #if defined(${MODULELOGIC_NAME}_AUTOINIT)
-      # include \"${MODULELOGIC_NAME}ObjectFactory.h\"
       # include \"vtkAutoInit.h\"
       VTK_AUTOINIT(${MODULELOGIC_NAME})
       #endif

CMake/vtkSlicerObjectFactory.cxx.in
@@ -1,0 +1,67 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    @vtk-module@ObjectFactory.cxx
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+
+#include "@vtk-module@ObjectFactory.h"
+#include "vtkVersion.h"
+
+// Include all of the classes we want to create overrides for.
+@_vtk_override_includes@
+
+vtkStandardNewMacro(@vtk-module@ObjectFactory)
+
+// Now create the functions to create overrides with.
+@_vtk_override_creates@
+
+@vtk-module@ObjectFactory::@vtk-module@ObjectFactory()
+{
+@_vtk_override_do@
+}
+
+const char * @vtk-module@ObjectFactory::GetVTKSourceVersion()
+{
+  return VTK_SOURCE_VERSION;
+}
+
+void @vtk-module@ObjectFactory::PrintSelf(ostream &os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os, indent);
+}
+
+// Registration of object factories.
+static unsigned int @vtk-module@Count;
+
+/* @VTK-MODULE@_EXPORT */ void @vtk-module@_AutoInit_Construct()
+{
+  if(++@vtk-module@Count == 1)
+    {
+    @vtk-object-factory-extra-init@
+    @vtk-module@ObjectFactory* factory = @vtk-module@ObjectFactory::New();
+    if (factory)
+      {
+      // vtkObjectFactory keeps a reference to the "factory",
+      vtkObjectFactory::RegisterFactory(factory);
+      factory->Delete();
+      }
+    }
+}
+
+/* @VTK-MODULE@_EXPORT */ void @vtk-module@_AutoInit_Destruct()
+{
+  if(--@vtk-module@Count == 0)
+    {
+    // Do not call vtkObjectFactory::UnRegisterFactory because
+    // vtkObjectFactory.cxx statically unregisters all factories.
+    }
+}

CMake/vtkSlicerObjectFactory.h.in
@@ -1,0 +1,42 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    @vtk-module@ObjectFactory.h
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+
+#ifndef __@vtk-module@ObjectFactory_h
+#define __@vtk-module@ObjectFactory_h
+
+#include "@vtk-module@Module.h" // For export macro
+#include "vtkObjectFactory.h"
+
+class @VTK-MODULE@_EXPORT @vtk-module@ObjectFactory : public vtkObjectFactory
+{
+public:
+  static @vtk-module@ObjectFactory * New();
+  vtkTypeMacro(@vtk-module@ObjectFactory, vtkObjectFactory)
+
+  const char * GetDescription() override { return "@vtk-module@ factory overrides."; }
+
+  const char * GetVTKSourceVersion() override;
+
+  void PrintSelf(ostream &os, vtkIndent indent) override;
+
+protected:
+  @vtk-module@ObjectFactory();
+
+private:
+  @vtk-module@ObjectFactory(const @vtk-module@ObjectFactory&) = delete;
+  void operator=(const @vtk-module@ObjectFactory&) = delete;
+};
+
+#endif // __@vtk-module@ObjectFactory_h

Libs/MRML/DisplayableManager/vtkMRMLDisplayableManagerExport.h
@@ -16,7 +16,7 @@
 
 #include <vtkMRMLDisplayableManagerConfigure.h>
 
-#if defined(WIN32) && !defined(VTKMRMLDisplayableManager_STATIC)
+#if defined(WIN32) && !defined(MRMLDisplayableManager_STATIC)
 #if defined(MRMLDisplayableManager_EXPORTS)
 #define VTK_MRML_DISPLAYABLEMANAGER_EXPORT __declspec( dllexport )
 #else
@@ -27,7 +27,6 @@
 #endif
 
 #if defined(MRMLDisplayableManager_AUTOINIT)
-#include "MRMLDisplayableManagerObjectFactory.h"
 #include "vtkAutoInit.h"
 VTK_AUTOINIT(MRMLDisplayableManager)
 #endif