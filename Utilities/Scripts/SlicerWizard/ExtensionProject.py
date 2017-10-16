Slicer/SlicerGitSVNArchive

commit ce39d3035116a479832b609125084c7d8d9cdb63

ENH: ExtensionWizard: Support layout with main CMakeLists in subdirectory
This allows to contribute description file update for the CarreraSlice
extension.

In that particular case, the top-level CMakeLists.txt doesn't contain
all variables of the form EXTENSION_*, instead they are found in
src/CMakeLists.txt.

git-svn-id: http://svn.slicer.org/Slicer4/trunk@26461 3bd1e089-480b-0410-8dfb-8563597acbee

Utilities/Scripts/SlicerWizard/ExtensionProject.py
@@ -51,8 +51,35 @@ class ExtensionProject(object):
     if not os.path.exists(cmakeFile):
       raise IOError("CMakeLists.txt not found")
 
-    self._scriptPath = cmakeFile
+    self._scriptContents, self._encoding = self._parse(cmakeFile, encoding=encoding)
 
+    try:
+      self.getValue("EXTENSION_NAME")
+    except KeyError:
+      for cmakeFile in self._collect_cmakefiles(path):
+        self._scriptContents, self._encoding = self._parse(cmakeFile, encoding=encoding)
+        try:
+          self.getValue("EXTENSION_NAME")
+          break
+        except KeyError:
+          continue
+
+  @staticmethod
+  def _collect_cmakefiles(path):
+    """Return list of CMakeLists.txt found in `path` at depth=1"""
+    cmakeFiles = []
+    dirnames = []
+    for _, dirnames, _ in os.walk(path):
+      break
+    for dirname in dirnames:
+      cmakeFile = os.path.join(path, dirname, "CMakeLists.txt")
+      if os.path.exists(cmakeFile):
+        cmakeFiles.append(cmakeFile)
+    return cmakeFiles
+
+  #---------------------------------------------------------------------------
+  @staticmethod
+  def _parse(cmakeFile, encoding=None):
     with open(cmakeFile) as fp:
       contents = fp.read()
 
@@ -69,14 +96,14 @@ class ExtensionProject(object):
         # If unable to determine encoding, skip unicode conversion... users
         # must not feed any unicode into the script or things will likely break
         # later (e.g. when trying to save the project)
-        self._scriptContents = CMakeParser.CMakeScript(contents)
+        contents = CMakeParser.CMakeScript(contents)
 
       else:
         # Otherwise, decode the contents into unicode
         contents = contents.decode(encoding)
-        self._scriptContents = CMakeParser.CMakeScript(contents)
+        contents = CMakeParser.CMakeScript(contents)
 
-      self._encoding = encoding
+    return contents, encoding
 
   #---------------------------------------------------------------------------
   def __enter__(self):

commit 0b2e5032cbd48c3f48b1a5f4247d26e0a9aceb7e

BUG: ExtensionWizard: Fix describe command
This commit fixes a regression introduced in r26461 (ENH: ExtensionWizard:
Support layout with main CMakeLists in subdirectory)

We cannot assume EXTENSION_NAME is always present in extension CMakeLists.txt.
Instead we check if a given CMakeLists.txt is associated with an
extension if it has the variable EXTENSION_HOMEPAGE.

See https://www.slicer.org/wiki/Documentation/Nightly/Developers/Extensions/DescriptionFile

git-svn-id: http://svn.slicer.org/Slicer4/trunk@26473 3bd1e089-480b-0410-8dfb-8563597acbee

Utilities/Scripts/SlicerWizard/ExtensionProject.py
@@ -52,16 +52,15 @@ class ExtensionProject(object):
       raise IOError("CMakeLists.txt not found")
 
     self._scriptContents, self._encoding = self._parse(cmakeFile, encoding=encoding)
-
     try:
       self._scriptPath = cmakeFile
-      self.getValue("EXTENSION_NAME")
+      self.getValue("EXTENSION_HOMEPAGE")
     except KeyError:
       for cmakeFile in self._collect_cmakefiles(path):
         self._scriptContents, self._encoding = self._parse(cmakeFile, encoding=encoding)
         try:
           self._scriptPath = cmakeFile
-          self.getValue("EXTENSION_NAME")
+          self.getValue("EXTENSION_HOMEPAGE")
           break
         except KeyError:
           continue