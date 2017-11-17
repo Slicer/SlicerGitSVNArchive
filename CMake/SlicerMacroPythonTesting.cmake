Slicer/SlicerGitSVNArchive

commit 36fad5de7d59676e10b6795d1493b2c47d3720c3

BUG: Teach 'slicer_add_python_unittest' to add module search paths
This commit allows scripted module test within extension to successfully
load dependent modules.

Fixes http://www.na-mic.org/Bug/view.php?id=4242

From: Isaiah Norton <inorton@bwh.harvard.edu>

git-svn-id: http://svn.slicer.org/Slicer4/trunk@25783 3bd1e089-480b-0410-8dfb-8563597acbee

CMake/SlicerMacroPythonTesting.cmake
@@ -18,6 +18,12 @@
 #
 ################################################################################
 
+set(UNITTEST_LIB_PATHS
+  "--additional-module-paths"
+  ${CMAKE_BINARY_DIR}/${Slicer_QTSCRIPTEDMODULES_LIB_DIR}
+  ${CMAKE_BINARY_DIR}/${Slicer_CLIMODULES_LIB_DIR}
+  ${CMAKE_BINARY_DIR}/${Slicer_QTLOADBLEMODULES_LIB_DIR}
+  )
 
 macro(slicer_add_python_test)
   set(options)
@@ -33,7 +39,9 @@ macro(slicer_add_python_test)
     COMMAND ${Slicer_LAUNCHER_EXECUTABLE}
     --no-splash
     --testing
-    ${Slicer_ADDITIONAL_LAUNCHER_SETTINGS} ${MY_SLICER_ARGS}
+    ${Slicer_ADDITIONAL_LAUNCHER_SETTINGS}
+    ${UNITTEST_LIB_PATHS}
+    ${MY_SLICER_ARGS}
     --python-script ${MY_SCRIPT} ${MY_SCRIPT_ARGS}
     )
   set_property(TEST py_${MY_TESTNAME_PREFIX}${test_name} PROPERTY RUN_SERIAL TRUE)
@@ -55,8 +63,9 @@ macro(slicer_add_python_unittest)
     --no-splash
     --testing
     ${Slicer_ADDITIONAL_LAUNCHER_SETTINGS}
-    --python-code "import slicer.testing\\; slicer.testing.runUnitTest(['${CMAKE_CURRENT_BINARY_DIR}', '${_script_source_dir}'], '${test_name}')"
     ${MY_SLICER_ARGS}
+    ${UNITTEST_LIB_PATHS}
+    --python-code "import slicer.testing\\; slicer.testing.runUnitTest(['${CMAKE_CURRENT_BINARY_DIR}', '${_script_source_dir}'], '${test_name}')"
     )
   set_property(TEST py_${MY_TESTNAME_PREFIX}${test_name} PROPERTY RUN_SERIAL TRUE)
 endmacro()

Modules/Scripted/SegmentEditor/CMakeLists.txt
@@ -37,6 +37,5 @@ slicer_add_python_unittest(
   # TODO: slicer_add_python_unittest should take care of adding the additional module paths
   SLICER_ARGS
     --disable-cli-modules
-    --additional-module-paths ${MODULE_BUILD_DIR} ${CMAKE_BINARY_DIR}/${Slicer_QTSCRIPTEDMODULES_LIB_DIR}
   )
 

commit 4edfa38fe94abf5b8c88ea336682e55efbf906a6

COMP: SlicerMacroPythonTesting: Fix typo in CMake variable
This commit fixes a regression introduced in r25783 (BUG: Teach
'slicer_add_python_unittest' to add module search paths) fixing
Slicer issue 4242 through PR #610.

From: Isaiah Norton <inorton@bwh.harvard.edu>

git-svn-id: http://svn.slicer.org/Slicer4/trunk@26645 3bd1e089-480b-0410-8dfb-8563597acbee

CMake/SlicerMacroPythonTesting.cmake
@@ -22,7 +22,7 @@ set(UNITTEST_LIB_PATHS
   "--additional-module-paths"
   ${CMAKE_BINARY_DIR}/${Slicer_QTSCRIPTEDMODULES_LIB_DIR}
   ${CMAKE_BINARY_DIR}/${Slicer_CLIMODULES_LIB_DIR}
-  ${CMAKE_BINARY_DIR}/${Slicer_QTLOADBLEMODULES_LIB_DIR}
+  ${CMAKE_BINARY_DIR}/${Slicer_QTLOADABLEMODULES_LIB_DIR}
   )
 
 macro(slicer_add_python_test)