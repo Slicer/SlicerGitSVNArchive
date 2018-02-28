Slicer/SlicerGitSVNArchive

commit 69829db155db8e5b887005767fbce8e7bb218518

ENH: Speedup app startup time by lazily instantiating extension dialog
On Ubuntu workstation with SSD harddrive and 64GB, this allows to reduce
startup time by 0.5s.

With the introduction of Qt5, this became an issue because instantiating
a QWebEngineView also means that QtWebEngineProcess is started.

Without change:

$ time ./Slicer --no-main-window --disable-python --disable-modules  --exit-after-startup
real	0m1.767s

With change:

$ time ./Slicer --no-main-window --disable-python --disable-modules  --exit-after-startup
real	0m1.157s


References:

https://doc.qt.io/qt-5/qtwebengine-overview.html#qt-webengine-process
https://www.youtube.com/watch?v=3CTFxhBc_1I

git-svn-id: http://svn.slicer.org/Slicer4/trunk@26960 3bd1e089-480b-0410-8dfb-8563597acbee

Base/QTGUI/qSlicerApplication.cxx
@@ -161,8 +161,11 @@ qSlicerApplicationPrivate::~qSlicerApplicationPrivate()
   delete this->SettingsDialog;
   this->SettingsDialog = 0;
 #ifdef Slicer_BUILD_EXTENSIONMANAGER_SUPPORT
-  delete this->ExtensionsManagerDialog;
-  this->ExtensionsManagerDialog =0;
+  if(this->ExtensionsManagerDialog)
+    {
+    delete this->ExtensionsManagerDialog;
+    this->ExtensionsManagerDialog =0;
+    }
 #endif
 #ifdef Slicer_USE_QtTesting
   delete this->TestingUtility;
@@ -284,14 +287,6 @@ void qSlicerApplicationPrivate::init()
 
   QObject::connect(this->SettingsDialog, SIGNAL(restartRequested()),
                    q, SLOT(restart()));
-
-  //----------------------------------------------------------------------------
-  // Dialogs
-  //----------------------------------------------------------------------------
-#ifdef Slicer_BUILD_EXTENSIONMANAGER_SUPPORT
-  this->ExtensionsManagerDialog = new qSlicerExtensionsManagerDialog(0);
-  this->ExtensionsManagerDialog->setExtensionsManagerModel(q->extensionsManagerModel());
-#endif
 
   //----------------------------------------------------------------------------
   // Test Utility
@@ -669,6 +664,10 @@ ctkSettingsDialog* qSlicerApplication::settingsDialog()const
 void qSlicerApplication::openExtensionsManagerDialog()
 {
   Q_D(qSlicerApplication);
+  if(!d->ExtensionsManagerDialog)
+    {
+    d->ExtensionsManagerDialog = new qSlicerExtensionsManagerDialog(0);
+    }
   if (!d->ExtensionsManagerDialog->extensionsManagerModel() &&
       this->mainWindow())
     {

commit 8f094e289dcee2ededa28dab5b08f9b49d0f68ff

BUG: Initialize ivar to prevent crash on application shutdown
This commit fixes a regression introduced in r26960 (ENH: Speedup
app startup time by lazily instantiating extension dialog) ensuring
the ExtensionsManagerDialog reference is initialized.

From: Dženan Zukić <dzenan.zukic@kitware.com>

git-svn-id: http://svn.slicer.org/Slicer4/trunk@26969 3bd1e089-480b-0410-8dfb-8563597acbee

Base/QTGUI/qSlicerApplication.cxx
@@ -150,6 +150,9 @@ qSlicerApplicationPrivate::qSlicerApplicationPrivate(
 {
   this->ToolTipTrapper = 0;
   this->SettingsDialog = 0;
+#ifdef Slicer_BUILD_EXTENSIONMANAGER_SUPPORT
+  this->ExtensionsManagerDialog = 0;
+#endif
 #ifdef Slicer_USE_QtTesting
   this->TestingUtility = 0;
 #endif