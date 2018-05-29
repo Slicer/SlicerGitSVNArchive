4092c0f73b6c108b0774a12cacce29cce9bc5f9b
Pablo Hernandez-Cerdan <pablo.hernandez@kitware.com>
2018-05-29 10:09:21 -0400

    BUG: Add missing typename keywords.
    Also remove unused type.

--- Libs/vtkITK/vtkITKImageThresholdCalculator.cxx
+++ Libs/vtkITK/vtkITKImageThresholdCalculator.cxx
@@ -52,10 +52,9 @@ vtkStandardNewMacro(vtkITKImageThresholdCalculator);
 template <class TPixelType>
 void ITKComputeThresholdFromVTKImage(vtkITKImageThresholdCalculator *self, vtkImageData *inputImage, double& computedThreshold)
 {
-  typedef itk::Size<3> SizeType;
   typedef itk::Image<TPixelType, 3> ImageType;
   typedef itk::Statistics::ImageToHistogramFilter<ImageType> HistogramGeneratorType;
-  typedef HistogramGeneratorType::HistogramType HistogramType;
+  typedef typename HistogramGeneratorType::HistogramType HistogramType;
   typedef itk::HistogramThresholdCalculator<HistogramType, double> CalculatorType;
 
   // itk import for input itk images
@@ -69,15 +68,15 @@ void ITKComputeThresholdFromVTKImage(vtkITKImageThresholdCalculator *self, vtkIm
 
   ConnectPipelines(vtkExporter.GetPointer(), itkImporter);
 
-  HistogramGeneratorType::Pointer histGenerator = HistogramGeneratorType::New();
+  typename HistogramGeneratorType::Pointer histGenerator = HistogramGeneratorType::New();
   histGenerator->SetInput(itkImporter->GetOutput());
-  HistogramGeneratorType::HistogramSizeType hsize(1);
+  typename HistogramGeneratorType::HistogramSizeType hsize(1);
   hsize[0] = 64;
   histGenerator->SetHistogramSize( hsize );
   histGenerator->SetAutoMinimumMaximum( true );
 
   // Create and initialize the calculator
-  CalculatorType::Pointer calculator;
+  typename CalculatorType::Pointer calculator;
   switch (self->GetMethod())
   {
     case vtkITKImageThresholdCalculator::METHOD_HUANG: calculator = itk::HuangThresholdCalculator<HistogramType>::New(); break;
