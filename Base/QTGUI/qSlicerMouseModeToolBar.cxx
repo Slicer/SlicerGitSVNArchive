Slicer/SlicerGitSVNArchive

commit 526640bebe85b9868aa3f71f3656ea3baf402e2a

BUG: Ensure mouse mode icon is always visible when placing annotations
This commit fixes a regression introduced by r27574 (BUG: Update VTK backporting
fixes) integrating VTK commit vtk/vtk@a9af5ea23b (Allow widgets to change
cursor when using QVTKOpenGLNativeWidget). See [1]

[1] https://gitlab.kitware.com/vtk/vtk/commit/a9af5ea23ba6831193267487b66f7bbaf5f42b55

Associated merge request is https://gitlab.kitware.com/vtk/vtk/merge_requests/4956

List of VTK changes:

$ git shortlog a3a5b779..4ab10d5674 --no-merges
Jean-Christophe Fillion-Robin (1):
      ENH: Support setting custom default cursor for QVTKOpenGL[Native]Widget

git-svn-id: http://svn.slicer.org/Slicer4/trunk@27590 3bd1e089-480b-0410-8dfb-8563597acbee

Base/QTGUI/qSlicerMouseModeToolBar.cxx
@@ -27,6 +27,7 @@
 #include "qSlicerLayoutManager.h"
 #include "qMRMLThreeDView.h"
 #include "qMRMLThreeDWidget.h"
+#include "qMRMLSliceView.h"
 #include "qMRMLSliceWidget.h"
 #include "qSlicerMouseModeToolBar_p.h"
 
@@ -463,6 +464,9 @@ void qSlicerMouseModeToolBar::changeCursorTo(QCursor cursor)
   for (int i=0; i < layoutManager->threeDViewCount(); ++i)
     {
     layoutManager->threeDWidget(i)->threeDView()->setCursor(cursor);
+#if VTK_MAJOR_VERSION >= 9 || (VTK_MAJOR_VERSION >= 8 && VTK_MINOR_VERSION >= 2)
+    layoutManager->threeDWidget(i)->threeDView()->VTKWidget()->setQVTKCursor(cursor);
+#endif
     }
 
   // the slice viewers
@@ -482,10 +486,13 @@ void qSlicerMouseModeToolBar::changeCursorTo(QCursor cursor)
     vtkMRMLSliceNode *sliceNode = vtkMRMLSliceNode::SafeDownCast(visibleViews->GetItemAsObject(v));
     if (sliceNode)
       {
-      qMRMLSliceWidget *sliceView = layoutManager->sliceWidget(sliceNode->GetName());
+      qMRMLSliceView *sliceView = layoutManager->sliceWidget(sliceNode->GetName())->sliceView();
       if (sliceView)
         {
         sliceView->setCursor(cursor);
+#if VTK_MAJOR_VERSION >= 9 || (VTK_MAJOR_VERSION >= 8 && VTK_MINOR_VERSION >= 2)
+        sliceView->VTKWidget()->setQVTKCursor(cursor);
+#endif
         }
       }
     }

SuperBuild/External_VTK.cmake
@@ -118,7 +118,7 @@ set(_git_tag)
 if("${Slicer_VTK_VERSION_MAJOR}" STREQUAL "7")
   set(_git_tag "43f6ee36f6e28c8347768bd97df4d767da6b4ce7")
 elseif("${Slicer_VTK_VERSION_MAJOR}" STREQUAL "8")
-  set(_git_tag "a3a5b7798efd37e12219d80bfadd96286dfa2c7e")
+  set(_git_tag "4ab10d5674090961d3fc48ac23319b3da97defec")
 else()
   message(FATAL_ERROR "error: Unsupported Slicer_VTK_VERSION_MAJOR: ${Slicer_VTK_VERSION_MAJOR}")
 endif()

commit 37520e8646ca0a515f322f871a95a1a508c0e3ea

BUG: Fix crash when loading MRB file in RSNA2012ProstateDemo test
This commit fixes a regression introduced in r27590 (BUG: Ensure mouse
mode icon is always visible when placing annotations).

git-svn-id: http://svn.slicer.org/Slicer4/trunk@27599 3bd1e089-480b-0410-8dfb-8563597acbee

Base/QTGUI/qSlicerMouseModeToolBar.cxx
@@ -486,12 +486,12 @@ void qSlicerMouseModeToolBar::changeCursorTo(QCursor cursor)
     vtkMRMLSliceNode *sliceNode = vtkMRMLSliceNode::SafeDownCast(visibleViews->GetItemAsObject(v));
     if (sliceNode)
       {
-      qMRMLSliceView *sliceView = layoutManager->sliceWidget(sliceNode->GetName())->sliceView();
-      if (sliceView)
+      qMRMLSliceWidget *sliceWidget = layoutManager->sliceWidget(sliceNode->GetName());
+      if (sliceWidget && sliceWidget->sliceView())
         {
-        sliceView->setCursor(cursor);
+        sliceWidget->sliceView()->setCursor(cursor);
 #if VTK_MAJOR_VERSION >= 9 || (VTK_MAJOR_VERSION >= 8 && VTK_MINOR_VERSION >= 2)
-        sliceView->VTKWidget()->setQVTKCursor(cursor);
+        sliceWidget->sliceView()->VTKWidget()->setQVTKCursor(cursor);
 #endif
         }
       }