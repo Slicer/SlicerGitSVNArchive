Slicer/SlicerGitSVNArchive

commit 4d4b63d4a0a00f8af0ca9ffa82140e17387729a2

ENH: Move UI to update self-test message delay to the developer settings
This free valuable space in all module GUIs (when developer mode is
enabled). Assuming this settings would not be changed often, open
the application settings to adjust it is reasonable.

Suggested-by: Andras Lasso <lasso@queensu.ca>

git-svn-id: http://svn.slicer.org/Slicer4/trunk@27618 3bd1e089-480b-0410-8dfb-8563597acbee

Base/Python/slicer/ScriptedLoadableModule.py
@@ -153,18 +153,6 @@ class ScriptedLoadableModuleWidget:
 
     reloadFormLayout.addWidget(createHLayout([self.reloadButton, self.reloadAndTestButton, self.editSourceButton, self.restartButton]))
 
-    # message delay
-    self.messageDelaySlider = ctk.ctkSliderWidget()
-    self.messageDelaySlider.decimals = 0
-    self.messageDelaySlider.minimum = 0
-    self.messageDelaySlider.maximum = 2000
-    self.messageDelaySlider.singleStep = 10
-    self.messageDelaySlider.pageStep = 100
-    self.messageDelaySlider.suffix = "ms"
-    self.messageDelaySlider.value = 750
-
-    reloadFormLayout.addRow("Message delay:", self.messageDelaySlider)
-
   def setup(self):
     # Instantiate and connect default widgets ...
     self.setupDeveloperSection()
@@ -190,7 +178,7 @@ class ScriptedLoadableModuleWidget:
     try:
       self.onReload()
       test = slicer.selfTests[self.moduleName]
-      test(msec=int(self.messageDelaySlider.value))
+      test(msec=slicer.app.userSettings().value("Developer/SelfTestDisplayMessageDelay"))
     except Exception, e:
       import traceback
       traceback.print_exc()

Base/QTGUI/Resources/UI/qSlicerSettingsDeveloperPanel.ui
@@ -45,6 +45,35 @@
      </property>
     </widget>
    </item>
+   <item row="3" column="0">
+    <widget class="QLabel" name="SelfTestMessageDelayLabel">
+     <property name="text">
+      <string>Self-test message delay:</string>
+     </property>
+    </widget>
+   </item>
+   <item row="3" column="1">
+    <widget class="ctkSliderWidget" name="SelfTestMessageDelaySlider">
+     <property name="toolTip">
+      <string>Time to wait before resuming self-test execution and hiding messages displayed to the user</string>
+     </property>
+     <property name="decimals">
+      <number>0</number>
+     </property>
+     <property name="singleStep">
+      <double>10.000000000000000</double>
+     </property>
+     <property name="pageStep">
+      <double>100.000000000000000</double>
+     </property>
+     <property name="maximum">
+      <double>2000.000000000000000</double>
+     </property>
+     <property name="suffix">
+      <string>ms</string>
+     </property>
+    </widget>
+   </item>
   </layout>
  </widget>
  <customwidgets>
@@ -53,6 +82,11 @@
    <extends>QWidget</extends>
    <header>ctkSettingsPanel.h</header>
    <container>1</container>
+  </customwidget>
+  <customwidget>
+   <class>ctkSliderWidget</class>
+   <extends>QWidget</extends>
+   <header>ctkSliderWidget.h</header>
   </customwidget>
  </customwidgets>
  <resources/>

Base/QTGUI/qSlicerSettingsDeveloperPanel.cxx
@@ -60,6 +60,7 @@ void qSlicerSettingsDeveloperPanelPrivate::init()
 
   // Default values
   this->DeveloperModeEnabledCheckBox->setChecked(false);
+  this->SelfTestMessageDelaySlider->setValue(750);
   this->QtTestingEnabledCheckBox->setChecked(false);
 #ifndef Slicer_USE_QtTesting
   this->QtTestingEnabledCheckBox->hide();
@@ -71,7 +72,10 @@ void qSlicerSettingsDeveloperPanelPrivate::init()
                       "checked", SIGNAL(toggled(bool)),
                       "Enable/Disable developer mode", ctkSettingsPanel::OptionRequireRestart);
 
-  // Register settings
+  q->registerProperty("Developer/SelfTestDisplayMessageDelay", this->SelfTestMessageDelaySlider,
+                      "value", SIGNAL(valueChanged(double)),
+                      "Time to wait before resuming self-test execution and hiding messages displayed to the user");
+
   q->registerProperty("QtTesting/Enabled", this->QtTestingEnabledCheckBox,
                       "checked", SIGNAL(toggled(bool)),
                       "Enable/Disable QtTesting", ctkSettingsPanel::OptionRequireRestart);
@@ -81,7 +85,6 @@ void qSlicerSettingsDeveloperPanelPrivate::init()
                    q, SLOT(enableDeveloperMode(bool)));
   QObject::connect(this->QtTestingEnabledCheckBox, SIGNAL(toggled(bool)),
                    q, SLOT(enableQtTesting(bool)));
-
 }
 
 // --------------------------------------------------------------------------

commit e6b96c94fcf5824ccc5a7987b8de8caeda2d8f33

BUG: Fix runtime error when using "Reload and Test" button
This commit fixes a regression introduced in r27618 (ENH: Move UI to
update self-test message delay to the developer setting)

It addresses the following error:

  Reload and Test: Exception!

  Could not find matching overload for given arguments:
  (u'750',)
   The following slots are available:
  start(int msec) -> void
  start() -> void

git-svn-id: http://svn.slicer.org/Slicer4/trunk@27619 3bd1e089-480b-0410-8dfb-8563597acbee

Base/Python/slicer/ScriptedLoadableModule.py
@@ -178,7 +178,7 @@ class ScriptedLoadableModuleWidget:
     try:
       self.onReload()
       test = slicer.selfTests[self.moduleName]
-      test(msec=slicer.app.userSettings().value("Developer/SelfTestDisplayMessageDelay"))
+      test(msec=int(slicer.app.userSettings().value("Developer/SelfTestDisplayMessageDelay")))
     except Exception, e:
       import traceback
       traceback.print_exc()