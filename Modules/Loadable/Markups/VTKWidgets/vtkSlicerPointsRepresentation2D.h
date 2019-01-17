5669bf1eb12116efdb1ff280935156a7a6b841d8
Davide Punzo <punzodavide@hotmail.it>
2019-01-17 18:08:48 +0100

    STYLE: update copyright for new files

--- Modules/Loadable/Markups/VTKWidgets/vtkSlicerPointsRepresentation2D.h
+++ Modules/Loadable/Markups/VTKWidgets/vtkSlicerPointsRepresentation2D.h
@@ -1,17 +1,21 @@
 /*=========================================================================
 
-  Program:   Visualization Toolkit
-  Module:    vtkSlicerPointsRepresentation2D.h
+ Copyright (c) ProxSim ltd., Kwun Tong, Hong Kong. All Rights Reserved.
 
-  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
-  All rights reserved.
-  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+ See COPYRIGHT.txt
+ or http://www.slicer.org/copyright/copyright.txt for details.
 
-     This software is distributed WITHOUT ANY WARRANTY; without even
-     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-     PURPOSE.  See the above copyright notice for more information.
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+
+ This file was originally developed by Davide Punzo, punzodavide@hotmail.it,
+ and development was supported by ProxSim ltd.
 
 =========================================================================*/
+
 /**
  * @class   vtkSlicerPointsRepresentation2D
  * @brief   Default representation for the points widget
