1834ffc3421c0fbba6f68c4050e4f8312c1d08bf
Pablo Hernandez-Cerdan <pablo.hernandez.cerdan@outlook.com>
2019-03-14 22:50:28 -0400

    STYLE: ResampleDTIVolume: Update code to use override and nullptr

--- Modules/CLI/ResampleDTIVolume/itkSeparateComponentsOfADiffusionTensorImage.h
+++ Modules/CLI/ResampleDTIVolume/itkSeparateComponentsOfADiffusionTensorImage.h
@@ -63,12 +63,9 @@ protected:
 
   void DynamicThreadedGenerateData( const OutputImageRegionType & outputRegionForThread) override;
 
-  void GenerateOutputInformation() ITK_OVERRIDE;
+  void GenerateOutputInformation() override;
 
-  void GenerateInputRequestedRegion() ITK_OVERRIDE;
-
-private:
-
+  void GenerateInputRequestedRegion() override;
 };
 
 } // end namespace itk
