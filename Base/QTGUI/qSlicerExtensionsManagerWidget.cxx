Slicer/SlicerGitSVNArchive

commit 18508592c35f7a811a7410878182d34fbaccd591

ENH: BuildSystem: Add Slicer_BUILD_WEBENGINE_SUPPORT option
This new option allows to build custom Slicer applications without
WebEngine libraries. It reduces the install size by ~120MB

If Slicer_BUILD_WEBENGINE_SUPPORT option is OFF, the following happens:

  - exclude the following classes:

    - qSlicerWebWidget, qSlicerWebDownloadWidget and qSlicerWebPythonProxy
    - qMRMLExpandingWebView and qMRMLExpandingWebViewPlugin
    - qMRMLChartView, qMRMLChartWidget and qMRMLChartViewControllerWidget

  - views:

    - skip registration of qMRMLLayoutChartViewFactory in layout manager
       and return sensible values for method like viewNode(), viewWidget()

    - skip download of jqPlot only used in qMRMLChartView class

    - vtkMRMLChart nodes are still available and registered.

  - disable tab showing shortcuts in  qSlicerActionsDialog

  - datastore module is not available

  - extension manager:

    - enabling Slicer_BUILD_EXTENSIONMANAGER_SUPPORT while having
      Slicer_BUILD_WEBENGINE_SUPPORT off is supported.

    - installing from file is still supported.

    - in "Manage Extensons" tab, clicking on an extension link will NOT
      show a web-browser inside the manager.

    - in "Install Extensions" tab, nothing will be shown.


This commit also enables sharing of GL context. This is required when
the application is build with optionSlicer_BUILD_WEBENGINE_SUPPORT  set
to OFF and the designer plugin "qwebengineview" is attempted to be loaded.
It addressed the following warning reported during application startup:

  Qt WebEngine seems to be initialized from a plugin. Please set Qt::AA_ShareOpenGLContexts using QCoreApplication::setAttribute before constructing QGuiApplication.
  loaded library "/path/to/Qt5.10.0/5.10.0/gcc_64/plugins/designer/libqwebengineview.so"

git-svn-id: http://svn.slicer.org/Slicer4/trunk@28172 3bd1e089-480b-0410-8dfb-8563597acbee

Base/QTGUI/qSlicerExtensionsManagerWidget.cxx
@@ -18,6 +18,8 @@
 
 ==============================================================================*/
 
+#include "vtkSlicerConfigure.h" // For Slicer_BUILD_WEBENGINE_SUPPORT
+
 // Qt includes
 #include <QFileDialog>
 #include <QMenu>
@@ -25,9 +27,11 @@
 #include <QTimerEvent>
 #include <QToolButton>
 #include <QUrlQuery>
+#ifdef Slicer_BUILD_WEBENGINE_SUPPORT
 #include <QWebEngineHistory>
 #include <QWebEnginePage>
 #include <QWebEngineView>
+#endif
 
 // CTK includes
 #include <ctkSearchBox.h>
@@ -43,6 +47,7 @@
 namespace
 {
 
+#ifdef Slicer_BUILD_WEBENGINE_SUPPORT
 QString jsQuote(QString text)
 {
   // NOTE: This assumes that 'text' does not contain '\r' or other control characters
@@ -50,6 +55,7 @@ QString jsQuote(QString text)
   text.replace(reSpecialCharacters, "\\\\1").replace("\n", "\\n");
   return QString("\'%1\'").arg(text);
 }
+#endif
 
 // --------------------------------------------------------------------------
 void invalidateSizeHint(QToolButton * button)
@@ -172,6 +178,11 @@ public:
   qSlicerExtensionsManagerWidgetPrivate(qSlicerExtensionsManagerWidget& object);
   void init();
 
+#ifdef Slicer_BUILD_WEBENGINE_SUPPORT
+  qSlicerExtensionsInstallWidget* ExtensionsManageBrowser;
+  qSlicerExtensionsInstallWidget* ExtensionsInstallWidget
+#endif
+
   qSlicerExtensionsToolsWidget* toolsWidget;
   QString lastSearchText;
   int searchTimerId;
@@ -181,6 +192,10 @@ public:
 qSlicerExtensionsManagerWidgetPrivate::qSlicerExtensionsManagerWidgetPrivate(qSlicerExtensionsManagerWidget& object)
   :q_ptr(&object), searchTimerId(0)
 {
+#ifdef Slicer_BUILD_WEBENGINE_SUPPORT
+  this->ExtensionsManageBrowser = nullptr;
+  this->ExtensionsInstallWidget = nullptr;
+#endif
 }
 
 // --------------------------------------------------------------------------
@@ -190,15 +205,29 @@ void qSlicerExtensionsManagerWidgetPrivate::init()
 
   this->setupUi(q);
 
+#ifdef Slicer_BUILD_WEBENGINE_SUPPORT
+  // Setup browser for "Install Extensions" tab
+  this->ExtensionsInstallWidget = new qSlicerExtensionsInstallWidget(installExtensionsTab);
+  this->ExtensionsInstallWidget->setObjectName("ExtensionsInstallWidget");
+  this->InstallExtensionsTabLayout->addWidget(this->ExtensionsInstallWidget);
+
+  // Setup browser for "Manage Extensions" tab
+  this->ExtensionsManageBrowser = new qSlicerExtensionsInstallWidget();
+  this->ExtensionsManageBrowser->setObjectName("ExtensionsManageBrowser");
+  this->ManageExtensionsPager->addWidget(this->ExtensionsManageBrowser);
   this->ExtensionsManageBrowser->setBrowsingEnabled(false);
   this->ExtensionsManageBrowser->webView()->load(QUrl("about:"));
+#endif
+
+  qSlicerExtensionsActionsWidget * actionsWidget = new qSlicerExtensionsActionsWidget;
 
   // Back and forward buttons
-  qSlicerExtensionsActionsWidget * actionsWidget = new qSlicerExtensionsActionsWidget;
+#ifdef Slicer_BUILD_WEBENGINE_SUPPORT
   actionsWidget->ManageBackButton->setDefaultAction(this->ExtensionsManageBrowser->webView()->pageAction(QWebEnginePage::Back));
   actionsWidget->ManageForwardButton->setDefaultAction(this->ExtensionsManageBrowser->webView()->pageAction(QWebEnginePage::Forward));
   actionsWidget->InstallBackButton->setDefaultAction(this->ExtensionsInstallWidget->webView()->pageAction(QWebEnginePage::Back));
   actionsWidget->InstallForwardButton->setDefaultAction(this->ExtensionsInstallWidget->webView()->pageAction(QWebEnginePage::Forward));
+#endif
 
   this->tabWidget->setCornerWidget(actionsWidget, Qt::TopLeftCorner);
 
@@ -223,6 +252,7 @@ void qSlicerExtensionsManagerWidgetPrivate::init()
   QObject::connect(this->ExtensionsManageWidget, SIGNAL(linkActivated(QUrl)),
                    q, SLOT(onManageLinkActivated(QUrl)));
 
+#ifdef Slicer_BUILD_WEBENGINE_SUPPORT
   QObject::connect(this->ExtensionsManageBrowser->webView(),
                    SIGNAL(urlChanged(QUrl)),
                    q, SLOT(onManageUrlChanged(QUrl)));
@@ -234,6 +264,7 @@ void qSlicerExtensionsManagerWidgetPrivate::init()
   QObject::connect(this->ExtensionsInstallWidget->webView(),
                    SIGNAL(urlChanged(QUrl)),
                    q, SLOT(onInstallUrlChanged(QUrl)));
+#endif
 
   QObject::connect(this->tabWidget, SIGNAL(currentChanged(int)),
                    q, SLOT(onCurrentTabChanged(int)));
@@ -289,8 +320,10 @@ void qSlicerExtensionsManagerWidget::setExtensionsManagerModel(qSlicerExtensions
   disconnect(this, SLOT(onModelUpdated()));
 
   d->ExtensionsManageWidget->setExtensionsManagerModel(model);
+#ifdef Slicer_BUILD_WEBENGINE_SUPPORT
   d->ExtensionsManageBrowser->setExtensionsManagerModel(model);
   d->ExtensionsInstallWidget->setExtensionsManagerModel(model);
+#endif
   d->ExtensionsRestoreWidget->setExtensionsManagerModel(model);
 
   if (model)
@@ -309,7 +342,9 @@ void qSlicerExtensionsManagerWidget::setExtensionsManagerModel(qSlicerExtensions
 void qSlicerExtensionsManagerWidget::refreshInstallWidget()
 {
   Q_D(qSlicerExtensionsManagerWidget);
+#ifdef Slicer_BUILD_WEBENGINE_SUPPORT
   d->ExtensionsInstallWidget->refresh();
+#endif
 }
 
 // --------------------------------------------------------------------------
@@ -348,21 +383,25 @@ void qSlicerExtensionsManagerWidget::onCurrentTabChanged(int index)
 {
   Q_D(qSlicerExtensionsManagerWidget);
   Q_UNUSED(index);
-
+#ifdef Slicer_BUILD_WEBENGINE_SUPPORT
   QWebEngineHistory* history = d->ExtensionsManageBrowser->webView()->history();
   if (history->canGoBack())
     {
     history->goToItem(history->items().first());
     }
+#endif
 }
 
 // --------------------------------------------------------------------------
 void qSlicerExtensionsManagerWidget::onManageLinkActivated(const QUrl& link)
 {
   Q_D(qSlicerExtensionsManagerWidget);
-
+#ifdef Slicer_BUILD_WEBENGINE_SUPPORT
   d->ManageExtensionsPager->setCurrentIndex(1);
   d->ExtensionsManageBrowser->webView()->load(link);
+#else
+  Q_UNUSED(link);
+#endif
 }
 
 // --------------------------------------------------------------------------
@@ -397,6 +436,7 @@ void qSlicerExtensionsManagerWidget::onInstallUrlChanged(const QUrl& newUrl)
 // --------------------------------------------------------------------------
 void qSlicerExtensionsManagerWidget::onSearchTextChanged(const QString& newText)
 {
+#ifdef Slicer_BUILD_WEBENGINE_SUPPORT
   Q_D(qSlicerExtensionsManagerWidget);
   if (d->searchTimerId)
     {
@@ -407,11 +447,15 @@ void qSlicerExtensionsManagerWidget::onSearchTextChanged(const QString& newText)
     {
     d->searchTimerId = this->startTimer(500);
     }
+#else
+  Q_UNUSED(newText);
+#endif
 }
 
 // --------------------------------------------------------------------------
 void qSlicerExtensionsManagerWidget::timerEvent(QTimerEvent* e)
 {
+#ifdef Slicer_BUILD_WEBENGINE_SUPPORT
   Q_D(qSlicerExtensionsManagerWidget);
   if (e->timerId() == d->searchTimerId)
     {
@@ -426,6 +470,7 @@ void qSlicerExtensionsManagerWidget::timerEvent(QTimerEvent* e)
     this->killTimer(d->searchTimerId);
     d->searchTimerId = 0;
     }
+#endif
   QObject::timerEvent(e);
 }
 

Base/QTApp/qSlicerApplicationHelper.cxx
@@ -121,6 +121,9 @@ void qSlicerApplicationHelper::preInitializeApplication(
   // Enable automatic scaling based on the pixel density of the monitor
   QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
+  // Enables resource sharing between the OpenGL contexts used by classes like QOpenGLWidget and QQuickWidget
+  QApplication::setAttribute(Qt::AA_ShareOpenGLContexts);
+
   // Allow a custom application name so that the settings
   // can be distinct for differently named applications
   QString applicationName("Slicer");

Base/QTGUI/CMakeLists.txt
@@ -93,13 +93,6 @@ set(KIT_SRCS
   qSlicerViewersToolBar.cxx
   qSlicerViewersToolBar.h
   qSlicerViewersToolBar_p.h
-  qSlicerWebPythonProxy.cxx
-  qSlicerWebPythonProxy.h
-  qSlicerWebDownloadWidget.cxx
-  qSlicerWebDownloadWidget.h
-  qSlicerWebWidget.cxx
-  qSlicerWebWidget.h
-  qSlicerWebWidget_p.h
   qSlicerWidget.cxx
   qSlicerWidget.h
 
@@ -109,9 +102,6 @@ set(KIT_SRCS
 
 if(Slicer_BUILD_EXTENSIONMANAGER_SUPPORT)
   list(APPEND KIT_SRCS
-    qSlicerExtensionsInstallWidget.cxx
-    qSlicerExtensionsInstallWidget.h
-    qSlicerExtensionsInstallWidget_p.h
     qSlicerExtensionsRestoreWidget.cxx
     qSlicerExtensionsRestoreWidget.h
     qSlicerExtensionsManageWidget.cxx
@@ -123,12 +113,31 @@ if(Slicer_BUILD_EXTENSIONMANAGER_SUPPORT)
     qSlicerSettingsExtensionsPanel.cxx
     qSlicerSettingsExtensionsPanel.h
     )
+  if(Slicer_BUILD_WEBENGINE_SUPPORT)
+    list(APPEND KIT_SRCS
+      qSlicerExtensionsInstallWidget.cxx
+      qSlicerExtensionsInstallWidget.h
+      qSlicerExtensionsInstallWidget_p.h
+      )
+  endif()
 endif()
 
 if(Slicer_BUILD_I18N_SUPPORT)
   list(APPEND KIT_SRCS
     qSlicerSettingsInternationalizationPanel.cxx
     qSlicerSettingsInternationalizationPanel.h
+    )
+endif()
+
+if(Slicer_BUILD_WEBENGINE_SUPPORT)
+  list(APPEND KIT_SRCS
+    qSlicerWebPythonProxy.cxx
+    qSlicerWebPythonProxy.h
+    qSlicerWebDownloadWidget.cxx
+    qSlicerWebDownloadWidget.h
+    qSlicerWebWidget.cxx
+    qSlicerWebWidget.h
+    qSlicerWebWidget_p.h
     )
 endif()
 
@@ -206,10 +215,6 @@ set(KIT_MOC_SRCS
   qSlicerStyle.h
   qSlicerViewersToolBar.h
   qSlicerViewersToolBar_p.h
-  qSlicerWebDownloadWidget.h
-  qSlicerWebPythonProxy.h
-  qSlicerWebWidget.h
-  qSlicerWebWidget_p.h
   qSlicerWidget.h
 
   qSlicerSingletonViewFactory.h
@@ -217,19 +222,32 @@ set(KIT_MOC_SRCS
 
 if(Slicer_BUILD_EXTENSIONMANAGER_SUPPORT)
   list(APPEND KIT_MOC_SRCS
-    qSlicerExtensionsInstallWidget.h
-    qSlicerExtensionsInstallWidget_p.h
     qSlicerExtensionsRestoreWidget.h
     qSlicerExtensionsManageWidget.h
     qSlicerExtensionsManagerDialog.h
     qSlicerExtensionsManagerWidget.h
     qSlicerSettingsExtensionsPanel.h
     )
+  if(Slicer_BUILD_WEBENGINE_SUPPORT)
+    list(APPEND KIT_MOC_SRCS
+      qSlicerExtensionsInstallWidget.h
+      qSlicerExtensionsInstallWidget_p.h
+      )
+  endif()
 endif()
 
 if(Slicer_BUILD_I18N_SUPPORT)
   list(APPEND KIT_MOC_SRCS
     qSlicerSettingsInternationalizationPanel.h
+    )
+endif()
+
+if(Slicer_BUILD_WEBENGINE_SUPPORT)
+  list(APPEND KIT_MOC_SRCS
+    qSlicerWebDownloadWidget.h
+    qSlicerWebPythonProxy.h
+    qSlicerWebWidget.h
+    qSlicerWebWidget_p.h
     )
 endif()
 

Base/QTGUI/Resources/UI/qSlicerExtensionsManagerWidget.ui
@@ -43,7 +43,6 @@
        <item>
         <widget class="QStackedWidget" name="ManageExtensionsPager">
          <widget class="qSlicerExtensionsManageWidget" name="ExtensionsManageWidget"/>
-         <widget class="qSlicerExtensionsInstallWidget" name="ExtensionsManageBrowser"/>
         </widget>
        </item>
       </layout>
@@ -56,17 +55,10 @@
       <attribute name="title">
        <string>Install Extensions</string>
       </attribute>
-      <layout class="QVBoxLayout" name="verticalLayout_3">
+      <layout class="QVBoxLayout" name="InstallExtensionsTabLayout">
        <property name="margin">
         <number>0</number>
        </property>
-       <item>
-        <widget class="qSlicerExtensionsInstallWidget" name="ExtensionsInstallWidget" native="true">
-         <property name="enabled">
-          <bool>true</bool>
-         </property>
-        </widget>
-       </item>
       </layout>
      </widget>
      <widget class="QWidget" name="restoreExtensionsTab">
@@ -92,12 +84,6 @@
    <class>qSlicerExtensionsManageWidget</class>
    <extends>QWidget</extends>
    <header>qSlicerExtensionsManageWidget.h</header>
-   <container>1</container>
-  </customwidget>
-  <customwidget>
-   <class>qSlicerExtensionsInstallWidget</class>
-   <extends>QWidget</extends>
-   <header>qSlicerExtensionsInstallWidget.h</header>
    <container>1</container>
   </customwidget>
   <customwidget>

Base/QTGUI/Testing/Cxx/qSlicerWidgetTest2.cxx
@@ -11,6 +11,9 @@
 
 // this is a test of the slicer slice logic resampling pipeline
 
+// Slicer includes
+#include "vtkSlicerConfigure.h" // For Slicer_VTK_USE_QVTKOPENGLWIDGET, Slicer_BUILD_WEBENGINE_SUPPORT
+
 // Qt includes
 #include <QApplication>
 #include <QProcessEnvironment>
@@ -18,10 +21,9 @@
 #include <QString>
 #include <QTimer>
 #include <QVBoxLayout>
+#ifdef Slicer_BUILD_WEBENGINE_SUPPORT
 #include <QWebEngineView>
-
-// Slicer includes
-#include "vtkSlicerConfigure.h"
+#endif
 
 // SlicerQt includes
 #include "qSlicerWidget.h"
@@ -186,10 +188,12 @@ int qSlicerWidgetTest2(int argc, char * argv[] )
   vbox.addWidget(vtkWidget);
   vtkWidget->GetRenderWindow()->Render();
 
+#ifdef Slicer_BUILD_WEBENGINE_SUPPORT
   QWebEngineView webView;
   webView.setParent(&parentWidget);
   webView.setUrl(QUrl("http://pyjs.org/examples"));
   vbox.addWidget(&webView);
+#endif
 
   vtkMRMLScene* scene = vtkMRMLScene::New();
   widget->setMRMLScene(scene);

Base/QTGUI/qSlicerActionsDialog.cxx
@@ -18,10 +18,14 @@
 
 ==============================================================================*/
 
+#include "vtkSlicerConfigure.h" // For Slicer_BUILD_WEBENGINE_SUPPORT
+
 // Qt includes
 #include <QGridLayout>
 #include <QtGlobal>
+#ifdef Slicer_BUILD_WEBENGINE_SUPPORT
 #include <QWebEngineView>
+#endif
 
 // SlicerQt includes
 #include "qSlicerActionsDialog.h"
@@ -40,7 +44,9 @@ public:
   qSlicerActionsDialogPrivate(qSlicerActionsDialog& object);
   void init();
 
+#ifdef Slicer_BUILD_WEBENGINE_SUPPORT
   QWebEngineView* WebView;
+#endif
 
 };
 
@@ -56,6 +62,7 @@ void qSlicerActionsDialogPrivate::init()
   Q_Q(qSlicerActionsDialog);
 
   this->setupUi(q);
+#ifdef Slicer_BUILD_WEBENGINE_SUPPORT
   this->WebView = new QWebEngineView();
   this->WebView->setObjectName("WebView");
   this->gridLayout->addWidget(this->WebView, 0, 0);
@@ -69,6 +76,9 @@ void qSlicerActionsDialogPrivate::init()
     QString("http://wiki.slicer.org/slicerWiki/index.php/Documentation/%1/").arg(wikiVersion);
   shortcutsUrl += "SlicerApplication/MouseandKeyboardShortcuts";
   this->WebView->setUrl( shortcutsUrl );
+#else
+  this->tabWidget->setTabEnabled(this->tabWidget->indexOf(this->WikiTab), false);
+#endif
 }
 
 //------------------------------------------------------------------------------

CMake/SlicerBlockInstallQt.cmake
@@ -110,10 +110,12 @@ set(QT_INSTALL_LIB_DIR ${Slicer_INSTALL_LIB_DIR})
       )
 
     # Install webengine translations
-    set(translations_dir "${qt_root_dir}/translations/qtwebengine_locales")
-    install(DIRECTORY ${translations_dir}
-      DESTINATION ${Slicer_INSTALL_ROOT}/share/QtTranslations/ COMPONENT Runtime
-      )
+    if("Qt5::WebEngine" IN_LIST QT_LIBRARIES)
+      set(translations_dir "${qt_root_dir}/translations/qtwebengine_locales")
+      install(DIRECTORY ${translations_dir}
+        DESTINATION ${Slicer_INSTALL_ROOT}/share/QtTranslations/ COMPONENT Runtime
+        )
+    endif()
 
     # Configure and install qt.conf
     set(qt_conf_contents "[Paths]\nPrefix = ..\nPlugins = ${Slicer_INSTALL_QtPlugins_DIR}\nTranslations = share/QtTranslations")

CMake/SlicerCPack.cmake
@@ -36,9 +36,11 @@ if(NOT Slicer_USE_SYSTEM_QT)
     imageformats
     sqldrivers
     )
-    list(APPEND SlicerBlockInstallQtPlugins_subdirectories
-      designer:webengineview
-      )
+    if(Slicer_BUILD_WEBENGINE_SUPPORT)
+      list(APPEND SlicerBlockInstallQtPlugins_subdirectories
+        designer:webengineview
+        )
+    endif()
     if(APPLE)
       list(APPEND SlicerBlockInstallQtPlugins_subdirectories
         platforms:cocoa

commit 0295b070dde96cb6c7bdb34b5d8642d28639d30a

COMP: Fix qSlicerExtensionsManagerWidget build error
This commit fixes regression introduced in r28172 (ENH: BuildSystem: Add
Slicer_BUILD_WEBENGINE_SUPPORT option)

git-svn-id: http://svn.slicer.org/Slicer4/trunk@28183 3bd1e089-480b-0410-8dfb-8563597acbee

Base/QTGUI/qSlicerExtensionsManagerWidget.cxx
@@ -39,6 +39,9 @@
 // QtGUI includes
 #include "qSlicerExtensionsManagerWidget.h"
 #include "qSlicerExtensionsManagerModel.h"
+#ifdef Slicer_BUILD_WEBENGINE_SUPPORT
+#include "qSlicerExtensionsInstallWidget.h"
+#endif
 #include "ui_qSlicerExtensionsActionsWidget.h"
 #include "ui_qSlicerExtensionsManagerWidget.h"
 #include "ui_qSlicerExtensionsToolsWidget.h"
@@ -180,7 +183,7 @@ public:
 
 #ifdef Slicer_BUILD_WEBENGINE_SUPPORT
   qSlicerExtensionsInstallWidget* ExtensionsManageBrowser;
-  qSlicerExtensionsInstallWidget* ExtensionsInstallWidget
+  qSlicerExtensionsInstallWidget* ExtensionsInstallWidget;
 #endif
 
   qSlicerExtensionsToolsWidget* toolsWidget;
@@ -207,7 +210,7 @@ void qSlicerExtensionsManagerWidgetPrivate::init()
 
 #ifdef Slicer_BUILD_WEBENGINE_SUPPORT
   // Setup browser for "Install Extensions" tab
-  this->ExtensionsInstallWidget = new qSlicerExtensionsInstallWidget(installExtensionsTab);
+  this->ExtensionsInstallWidget = new qSlicerExtensionsInstallWidget(this->InstallExtensionsTab);
   this->ExtensionsInstallWidget->setObjectName("ExtensionsInstallWidget");
   this->InstallExtensionsTabLayout->addWidget(this->ExtensionsInstallWidget);
 