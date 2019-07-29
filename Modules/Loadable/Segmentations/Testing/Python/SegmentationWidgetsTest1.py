Slicer/SlicerGitSVNArchive

commit cb6b36b0314fff6f03febf83fd2bf6afea928908

COMP: Fix -Wunused and -Wsign-compare warnings
git-svn-id: http://svn.slicer.org/Slicer4/trunk@28412 3bd1e089-480b-0410-8dfb-8563597acbee

Modules/Loadable/Segmentations/Widgets/qMRMLSegmentEditorWidget.cxx
@@ -3125,7 +3125,7 @@ void qMRMLSegmentEditorWidget::onSelectSegmentShortcut()
     }
   QSet<QString> displayedSegmentIDsSet = d->SegmentsTableView->displayedSegmentIDs().toSet();
   QStringList iterableSegmentIds = visibleSegmentIDsSet.intersect(displayedSegmentIDsSet).toList();
-  for (unsigned int segmentIndex = 0; segmentIndex < iterableSegmentIds.size(); segmentIndex++)
+  for (int segmentIndex = 0; segmentIndex < iterableSegmentIds.size(); segmentIndex++)
     {
     QString segmentID = iterableSegmentIds[segmentIndex];
     if (currentSegmentID == segmentID)

Modules/Loadable/Segmentations/Widgets/qMRMLSegmentsTableView.cxx
@@ -970,7 +970,6 @@ void qMRMLSegmentsTableView::contextMenuEvent(QContextMenuEvent* event)
   if (d->AdvancedSegmentVisibility && index.isValid())
     {
     QString segmentID = d->SortFilterModel->segmentIDFromIndex(index);
-    vtkSegment* segment = d->SegmentationNode->GetSegmentation()->GetSegment(segmentID.toStdString());
 
     // Get segment display properties
     vtkMRMLSegmentationDisplayNode::SegmentDisplayProperties properties;

Modules/Loadable/Segmentations/Widgets/qMRMLSegmentsModel.cxx
@@ -330,7 +330,6 @@ QModelIndexList qMRMLSegmentsModel::indexes(QString segmentID) const
     }
   // Add the QModelIndexes from the other columns
   const int row = itemIndexes[0].row();
-  QModelIndex itemParentIndex = itemIndexes[0].parent();
   for (int col = 1; col < this->columnCount(); ++col)
     {
     itemIndexes << this->index(row, col);

Modules/Loadable/VolumeRendering/MRML/vtkMRMLGPURayCastVolumeRenderingDisplayNode.cxx
@@ -59,8 +59,8 @@ void vtkMRMLGPURayCastVolumeRenderingDisplayNode::Copy(vtkMRMLNode *anode)
   int wasModifying = this->StartModify();
   this->Superclass::Copy(anode);
 
-  vtkMRMLCopyBeginMacro(anode);
-  vtkMRMLCopyEndMacro();
+//  vtkMRMLCopyBeginMacro(anode);
+//  vtkMRMLCopyEndMacro();
 
   this->EndModify(wasModifying);
 }

commit 71b0431c17eff2b158774d0ef92dc69667a5ebce

BUG: Fix crash in Segment table prev/next shortcut
Crash introduced in cb6b36b. Previously negative indices were handled through cast to unsigned int.
New implementation should correctly iterate through visible segment IDs in the table without crashing.

Other changes:
* Update failing test SegmentationWidgetsTest1
* Add testing for prev/next functions in qMRMLSegmentEditorWidget
* Add testing for filtering options in qMRMLSegmentsTableView
* Fix bug preventing the saving of filtering options in the vtkMRMLSegmentationNode

Modules/Loadable/Segmentations/Widgets/qMRMLSegmentEditorWidget.cxx
@@ -103,6 +103,7 @@
 #include <QPointer>
 #include <QScrollArea>
 #include <QShortcut>
+#include <QTableView>
 #include <QToolButton>
 #include <QVBoxLayout>
 #include <QWidgetAction>
@@ -3103,39 +3104,10 @@ void qMRMLSegmentEditorWidget::onSelectSegmentShortcut()
     {
     return;
     }
-  int segmentIndexOffset = shortcut->property("segmentIndexOffset").toInt();
 
-  vtkMRMLSegmentationNode* segmentationNode = vtkMRMLSegmentationNode::SafeDownCast(this->segmentationNode());
-  if (segmentationNode == nullptr || segmentationNode->GetDisplayNode() == nullptr)
-    {
-    return;
-    }
-  vtkMRMLSegmentationDisplayNode* displayNode = vtkMRMLSegmentationDisplayNode::SafeDownCast(segmentationNode->GetDisplayNode());
-  if (displayNode == nullptr)
-    {
-    return;
-    }
-  std::vector<std::string> visibleSegmentIDs;
-  displayNode->GetVisibleSegmentIDs(visibleSegmentIDs);
-  QString currentSegmentID = this->currentSegmentID();
-  QSet<QString> visibleSegmentIDsSet;
-  for (std::string visibleSegmentID : visibleSegmentIDs)
-    {
-    visibleSegmentIDsSet << QString::fromStdString(visibleSegmentID);
-    }
-  QSet<QString> displayedSegmentIDsSet = d->SegmentsTableView->displayedSegmentIDs().toSet();
-  QStringList iterableSegmentIds = visibleSegmentIDsSet.intersect(displayedSegmentIDsSet).toList();
-  for (int segmentIndex = 0; segmentIndex < iterableSegmentIds.size(); segmentIndex++)
-    {
-    QString segmentID = iterableSegmentIds[segmentIndex];
-    if (currentSegmentID == segmentID)
-      {
-      // this is the current segment, determine which one is the previous/next and select that
-      int newSegmentIndex = (segmentIndex + segmentIndexOffset) % iterableSegmentIds.size(); // wrap around
-      this->setCurrentSegmentID(iterableSegmentIds[newSegmentIndex].toStdString().c_str());
-      return;
-      }
-    }
+  // Direction that the segment selection should be moved (-1/+1 is previous/next)
+  int segmentIndexOffset = shortcut->property("segmentIndexOffset").toInt();
+  this->selectSegmentAtOffset (segmentIndexOffset);
 }
 
 //---------------------------------------------------------------------------
@@ -3681,4 +3653,58 @@ void qMRMLSegmentEditorWidget::showSegmentationGeometryDialog()
       this->setMasterVolumeNode(masterVolumeNode);
       }
     }
+}
+
+//---------------------------------------------------------------------------
+void qMRMLSegmentEditorWidget::selectPreviousSegment()
+{
+  this->selectSegmentAtOffset (-1);
+}
+
+//---------------------------------------------------------------------------
+void qMRMLSegmentEditorWidget::selectNextSegment()
+{
+  this->selectSegmentAtOffset (1);
+}
+
+//---------------------------------------------------------------------------
+void qMRMLSegmentEditorWidget::selectSegmentAtOffset (int offset)
+{
+  Q_D(qMRMLSegmentEditorWidget);
+
+  vtkMRMLSegmentationNode* segmentationNode = vtkMRMLSegmentationNode::SafeDownCast(this->segmentationNode());
+  if (segmentationNode == nullptr || segmentationNode->GetDisplayNode() == nullptr)
+    {
+    return;
+    }
+  vtkMRMLSegmentationDisplayNode* displayNode = vtkMRMLSegmentationDisplayNode::SafeDownCast(segmentationNode->GetDisplayNode());
+  if (displayNode == nullptr)
+    {
+    return;
+    }
+  std::vector<std::string> visibleSegmentIDs;
+  displayNode->GetVisibleSegmentIDs(visibleSegmentIDs);
+  QString currentSegmentID = this->currentSegmentID();
+
+  QModelIndexList selectedRows = d->SegmentsTableView->tableWidget()->selectionModel()->selectedRows();
+  if (selectedRows.empty())
+    {
+    return;
+    }
+
+  int selectedRow = selectedRows[0].row();
+  int rowCount = d->SegmentsTableView->tableWidget()->model()->rowCount();
+  QString selectedSegmentID = currentSegmentID;
+  for (int i = 0; i < rowCount; ++i)
+    {
+    // 'offset' represents the direction that the segment selection should be moved (-1/+1 is previous/next)
+    selectedRow = std::max(0, (selectedRow + offset) % rowCount);
+    std::string segmentID = d->SegmentsTableView->segmentIDForRow(selectedRow).toStdString();
+    if (std::find(visibleSegmentIDs.begin(), visibleSegmentIDs.end(), segmentID) != visibleSegmentIDs.end())
+      {
+      selectedSegmentID = QString::fromStdString(segmentID);
+      break;
+      }
+    }
+  this->setCurrentSegmentID(selectedSegmentID.toStdString().c_str());
 }

Modules/Loadable/Segmentations/Widgets/qMRMLSegmentEditorWidget.h
@@ -295,6 +295,17 @@ public slots:
   /// \sa interactionNode()
   void setInteractionNode(vtkMRMLInteractionNode* interactionNode);
 
+  /// Select the segment above the currently selected one in the table (skipping segments that are not visible)
+  void selectPreviousSegment();
+
+  /// Select the segment below the currently selected one in the table (skipping segments that are not visible)
+  void selectNextSegment();
+
+  /// Select the segment offset from the currently selected one in the table (skipping segments that are not visible)
+  /// Positive offset will move down the table
+  /// Negative offset will move up the table
+  void selectSegmentAtOffset (int offset);
+
 signals:
   /// Emitted if different segment is selected in the segment list.
   void currentSegmentIDChanged(const QString&);

Modules/Loadable/Segmentations/Widgets/qMRMLSegmentsTableView.cxx
@@ -275,7 +275,7 @@ void qMRMLSegmentsTableViewPrivate::init()
   QObject::connect(this->SegmentsTable->selectionModel(), &QItemSelectionModel::selectionChanged, q, &qMRMLSegmentsTableView::onSegmentSelectionChanged);
   QObject::connect(this->Model, &qMRMLSegmentsModel::segmentAboutToBeModified, q, &qMRMLSegmentsTableView::segmentAboutToBeModified);
   QObject::connect(this->SegmentsTable, &QTableView::clicked, q, &qMRMLSegmentsTableView::onSegmentsTableClicked);
-  QObject::connect(this->FilterLineEdit, &QLineEdit::textChanged, this->SortFilterModel, &qMRMLSortFilterSegmentsProxyModel::setTextFilter);
+  QObject::connect(this->FilterLineEdit, &ctkSearchBox::textEdited, this->SortFilterModel, &qMRMLSortFilterSegmentsProxyModel::setTextFilter);
   for (QPushButton* button : this->ShowStatusButtons)
     {
     if (!button)
@@ -284,8 +284,7 @@ void qMRMLSegmentsTableViewPrivate::init()
       }
     QObject::connect(button, &QToolButton::clicked, q, &qMRMLSegmentsTableView::onShowStatusButtonClicked);
     }
-
-  QObject::connect(this->FilterLineEdit, &QLineEdit::textEdited, this->SortFilterModel, &qMRMLSortFilterSegmentsProxyModel::setTextFilter);
+  QObject::connect(this->SortFilterModel, &qMRMLSortFilterSegmentsProxyModel::filterModified, q, &qMRMLSegmentsTableView::onSegmentsFilterModified);
 
   // Set item delegate to handle color and opacity changes
   qMRMLItemDelegate* itemDelegate = new qMRMLItemDelegate(this->SegmentsTable);
@@ -927,6 +926,19 @@ void qMRMLSegmentsTableView::setStatusShown(int status, bool shown)
 }
 
 //------------------------------------------------------------------------------
+int qMRMLSegmentsTableView::rowForSegmentID(QString segmentID)
+{
+  return this->sortFilterProxyModel()->indexFromSegmentID(segmentID).row();
+}
+
+//------------------------------------------------------------------------------
+QString qMRMLSegmentsTableView::segmentIDForRow(int row)
+{
+  QModelIndex index = this->sortFilterProxyModel()->index(row, 0);
+  return this->sortFilterProxyModel()->segmentIDFromIndex(index);
+}
+
+//------------------------------------------------------------------------------
 void qMRMLSegmentsTableView::contextMenuEvent(QContextMenuEvent* event)
 {
   Q_D(qMRMLSegmentsTableView);
@@ -1056,6 +1068,7 @@ void qMRMLSegmentsTableView::setSelectedSegmentsStatus(int aStatus)
     return;
     }
 
+  MRMLNodeModifyBlocker blocker(d->SegmentationNode);
   QStringList selectedSegmentIDs = this->selectedSegmentIDs();
   for (QString segmentID : selectedSegmentIDs)
     {

Modules/Loadable/Segmentations/Widgets/qMRMLSegmentsTableView.h
@@ -113,6 +113,11 @@ public:
   /// \sa setStatusShown
   Q_INVOKABLE bool statusShown(int status);
 
+  /// Get the row for the specified segment ID
+  int rowForSegmentID(QString segmentID);
+  /// Get the segment ID for the specified row
+  QString segmentIDForRow(int row);
+
 public slots:
   /// Set segmentation MRML node
   void setSegmentationNode(vtkMRMLNode* node);
@@ -152,7 +157,7 @@ public slots:
 
   /// Set the text used to filter the segments in the table
   /// \sa textFilter
-  void setTextFilter(QString);
+  void setTextFilter(QString textFilter);
   /// Set if the specified status should be shown in the table
   /// \sa statusShown
   void setStatusShown(int status, bool shown);

Modules/Loadable/Segmentations/Widgets/qMRMLSortFilterSegmentsProxyModel.cxx
@@ -348,6 +348,8 @@ void qMRMLSortFilterSegmentsProxyModel::setHideSegments(const QStringList& segme
 {
   Q_D(qMRMLSortFilterSegmentsProxyModel);
   d->HideSegments = segmentIDs;
+  this->invalidateFilter();
+  emit filterModified();
 }
 
 // --------------------------------------------------------------------------

Modules/Loadable/Segmentations/Widgets/qMRMLSortFilterSegmentsProxyModel.h
@@ -93,7 +93,7 @@ public:
   /// If the flags for all states are false, than no filtering is performed
   /// The list of availiable status is in vtkSlicerSegmentationsModuleLogic::SegmentStatus
   /// \sa showStatus
-  void setShowStatus(int status, bool shown);
+  Q_INVOKABLE void setShowStatus(int status, bool shown);
 
 public slots:
   void setFilterEnabled(bool filterEnabled);

Modules/Loadable/Segmentations/Testing/Python/SegmentationWidgetsTest1.py
@@ -27,18 +27,20 @@ class SegmentationWidgetsTest1(ScriptedLoadableModuleTest):
 
     self.TestSection_00_SetupPathsAndNames()
     self.TestSection_01_GenerateInputData()
-    self.TestSection_1_qMRMLSegmentsTableView()
-    self.TestSection_2_qMRMLSegmentationGeometryWidget()
+    self.TestSection_02_qMRMLSegmentsTableView()
+    self.TestSection_03_qMRMLSegmentationGeometryWidget()
+    self.TestSection_04_qMRMLSegmentatEditorWidget()
 
     logging.info('Test finished')
 
   #------------------------------------------------------------------------------
   def TestSection_00_SetupPathsAndNames(self):
+    logging.info('Test section 0: SetupPathsAndNames')
     self.inputSegmentationNode = None
 
   #------------------------------------------------------------------------------
   def TestSection_01_GenerateInputData(self):
-
+    logging.info('Test section 1: GenerateInputData')
     self.inputSegmentationNode = slicer.mrmlScene.AddNewNodeByClass('vtkMRMLSegmentationNode')
 
     # Create new segments
@@ -62,8 +64,12 @@ class SegmentationWidgetsTest1(ScriptedLoadableModuleTest):
     self.assertEqual(self.inputSegmentationNode.GetSegmentation().GetNumberOfSegments(), 3)
 
   #------------------------------------------------------------------------------
-  def TestSection_1_qMRMLSegmentsTableView(self):
-    logging.info('Test section 1: qMRMLSegmentsTableView')
+  def TestSection_02_qMRMLSegmentsTableView(self):
+    logging.info('Test section 2: qMRMLSegmentsTableView')
+
+    self.inputSegmentationNode.CreateDefaultDisplayNodes()
+    displayNode = self.inputSegmentationNode.GetDisplayNode()
+    self.assertIsNotNone(displayNode)
 
     segmentsTableView = slicer.qMRMLSegmentsTableView()
     segmentsTableView.setMRMLScene(slicer.mrmlScene)
@@ -78,6 +84,27 @@ class SegmentationWidgetsTest1(ScriptedLoadableModuleTest):
     self.assertEqual(len(segmentsTableView.displayedSegmentIDs()), 2)
     slicer.app.processEvents()
     slicer.util.delayDisplay("Hidden 'second'")
+
+    segmentsTableView.setHideSegments([])
+
+    sortFilterProxyModel = segmentsTableView.sortFilterProxyModel()
+    sortFilterProxyModel.filterEnabled = True
+    sortFilterProxyModel.textFilter = "third"
+    slicer.app.processEvents()
+    slicer.util.delayDisplay("All but 'third' filtered")
+    self.assertEqual(len(segmentsTableView.displayedSegmentIDs()), 1)
+
+    sortFilterProxyModel.textFilter = ""
+
+    firstSegment = self.inputSegmentationNode.GetSegmentation().GetSegment("first")
+
+    logic = slicer.modules.segmentations.logic()
+    logic.SetSegmentStatus(firstSegment, logic.InProgress)
+    sortFilterProxyModel.setShowStatus(logic.NotStarted, True)
+
+    slicer.app.processEvents()
+    slicer.util.delayDisplay("'NotStarted' shown")
+    self.assertEqual(len(segmentsTableView.displayedSegmentIDs()), 2)
 
   #------------------------------------------------------------------------------
   def compareOutputGeometry(self, orientedImageData, spacing, origin, directions):
@@ -124,7 +151,7 @@ class SegmentationWidgetsTest1(ScriptedLoadableModuleTest):
     return imageAccumulate.GetVoxelCount()
 
   #------------------------------------------------------------------------------
-  def TestSection_2_qMRMLSegmentationGeometryWidget(self):
+  def TestSection_03_qMRMLSegmentationGeometryWidget(self):
     logging.info('Test section 2: qMRMLSegmentationGeometryWidget')
 
     import vtkSegmentationCore
@@ -307,3 +334,52 @@ class SegmentationWidgetsTest1(ScriptedLoadableModuleTest):
     slicer.util.delayDisplay('Model and ROI source cases - OK')
 
     slicer.util.delayDisplay('Segmentation geometry widget test passed')
+
+  #------------------------------------------------------------------------------
+  def TestSection_04_qMRMLSegmentatEditorWidget(self):
+    logging.info('Test section 4: qMRMLSegmentatEditorWidget')
+
+    self.segmentEditorNode = slicer.mrmlScene.AddNewNodeByClass('vtkMRMLSegmentEditorNode')
+    self.assertIsNotNone(self.segmentEditorNode)
+
+    displayNode = self.inputSegmentationNode.GetDisplayNode()
+    self.assertIsNotNone(displayNode)
+
+    segmentEditorWidget = slicer.qMRMLSegmentEditorWidget()
+    segmentEditorWidget.setMRMLSegmentEditorNode(self.segmentEditorNode)
+    segmentEditorWidget.setMRMLScene(slicer.mrmlScene)
+    segmentEditorWidget.setSegmentationNode(self.inputSegmentationNode)
+    segmentEditorWidget.installKeyboardShortcuts(segmentEditorWidget)
+    segmentEditorWidget.setFocus(qt.Qt.OtherFocusReason)
+    segmentEditorWidget.show()
+
+    self.segmentEditorNode.SetSelectedSegmentID('first')
+
+    segmentEditorWidget.selectNextSegment()
+    selectedSegmentID = self.segmentEditorNode.GetSelectedSegmentID()
+    self.assertEqual(selectedSegmentID, 'second')
+
+    segmentEditorWidget.selectPreviousSegment()
+    selectedSegmentID = self.segmentEditorNode.GetSelectedSegmentID()
+    self.assertEqual(selectedSegmentID, 'first')
+
+    displayNode.SetSegmentVisibility('second', False)
+    segmentEditorWidget.selectNextSegment()
+    selectedSegmentID = self.segmentEditorNode.GetSelectedSegmentID()
+    self.assertEqual(selectedSegmentID, 'third')
+
+    # Trying to go out of bounds past first segment
+    segmentEditorWidget.selectPreviousSegment() #First
+    selectedSegmentID = self.segmentEditorNode.GetSelectedSegmentID()
+    self.assertEqual(selectedSegmentID, 'first')
+    segmentEditorWidget.selectPreviousSegment() #First
+    selectedSegmentID = self.segmentEditorNode.GetSelectedSegmentID()
+    self.assertEqual(selectedSegmentID, 'first')
+    segmentEditorWidget.selectPreviousSegment() #First
+    selectedSegmentID = self.segmentEditorNode.GetSelectedSegmentID()
+    self.assertEqual(selectedSegmentID, 'first')
+
+    # Wrap around
+    self.segmentEditorNode.SetSelectedSegmentID('third')
+    segmentEditorWidget.selectNextSegment()
+    self.assertEqual(selectedSegmentID, 'first')