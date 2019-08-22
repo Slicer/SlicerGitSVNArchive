Slicer/SlicerGitSVNArchive

commit e781d5feaec47aecb2d9c22c6862650f0a037fcd

COMP: Update CTK to fix handling of python libraries
List of changes:

$ git shortlog acd419d5c..3e7286ff5 --no-merges
Jean-Christophe Fillion-Robin (1):
      STYLE: Remove trailing spaces

Stefan Dinkelacker (1):
      BUG: Fix handling of PythonLibs

git-svn-id: http://svn.slicer.org/Slicer4/trunk@28453 3bd1e089-480b-0410-8dfb-8563597acbee

SuperBuild/External_CTK.cmake
@@ -57,7 +57,7 @@ if(NOT DEFINED CTK_DIR AND NOT Slicer_USE_SYSTEM_${proj})
 
   ExternalProject_SetIfNotDefined(
     Slicer_${proj}_GIT_TAG
-    "acd419d5c6e27c6e13811dd508ab8cf17ec76c7a"
+    "3e7286ff5677a123f31c63337b5faded8d48d823"
     QUIET
     )
 

commit 77a66cd863b59497c86880c1637993ead06671fe

COMP: Update CTK to fix regression introduced in r28453
This commit fixes a regression introduced in commontk/CTK@ff1800e (BUG: Fix
handling of PythonLibs)

See https://discourse.slicer.org/t/no-preview-builds-today-error-with-recent-ctk-update/8133


List of changes:

$ git shortlog 3e7286ff..6a8d584d --no-merges
Jean-Christophe Fillion-Robin (1):
      COMP: Ensure ctkFunctionExtractOptimizedLibrary can be used in dependent project

git-svn-id: http://svn.slicer.org/Slicer4/trunk@28457 3bd1e089-480b-0410-8dfb-8563597acbee

SuperBuild/External_CTK.cmake
@@ -57,7 +57,7 @@ if(NOT DEFINED CTK_DIR AND NOT Slicer_USE_SYSTEM_${proj})
 
   ExternalProject_SetIfNotDefined(
     Slicer_${proj}_GIT_TAG
-    "3e7286ff5677a123f31c63337b5faded8d48d823"
+    "6a8d584d3eb0ed750f6a850ab4f9c1089820f701"
     QUIET
     )
 