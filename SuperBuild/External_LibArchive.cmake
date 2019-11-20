Slicer/SlicerGitSVNArchive

commit 8522d2f4233c3ca84d1bbd1066706a86296f1d33

COMP: Update LibArchive to fix build issue with GCC 8.2.0
Update to latest LibArchive release (v3.4.0) to address https://issues.slicer.org/view.php?id=4616

git-svn-id: http://svn.slicer.org/Slicer4/trunk@28627 3bd1e089-480b-0410-8dfb-8563597acbee

SuperBuild/External_LibArchive.cmake
@@ -42,13 +42,11 @@ if((NOT DEFINED LibArchive_INCLUDE_DIR
     QUIET
     )
 
-  # master (v3.3.3) with patches:
+  # master (v3.4.0) with patches:
   # - disabling LHA (See #4407)
-  # - fixing GCC7 build errors
-  # - fix CMP0074 warning
   ExternalProject_SetIfNotDefined(
     Slicer_${proj}_GIT_TAG
-    "0737ce70653716995508f4613338410b2eb7f6ec" # slicer-v3.3.3-2018-06-10-d7dea508
+    "92ad4c79e46805ce2dfbc46746e10d204108655e" # slicer-v3.4.0-2019-06-11-614110e7
     QUIET
     )
 

commit 0b26d0f93e9ad73c0a268e78336cd2db2804eb5e

COMP: Fix macOS packaging to account for LibArchive version update
This commit fixes a regression introduced in r28627 (COMP: Update
LibArchive to fix build issue with GCC 8.2.0)

git-svn-id: http://svn.slicer.org/Slicer4/trunk@28638 3bd1e089-480b-0410-8dfb-8563597acbee

SuperBuild/External_LibArchive.cmake
@@ -89,7 +89,7 @@ if((NOT DEFINED LibArchive_INCLUDE_DIR
     )
   if(APPLE)
     ExternalProject_Add_Step(${proj} fix_rpath
-      COMMAND install_name_tool -id ${EP_INSTALL_DIR}/lib/libarchive.16.dylib ${EP_INSTALL_DIR}/lib/libarchive.16.dylib
+      COMMAND install_name_tool -id ${EP_INSTALL_DIR}/lib/libarchive.17.dylib ${EP_INSTALL_DIR}/lib/libarchive.17.dylib
       DEPENDEES install
       )
   endif()