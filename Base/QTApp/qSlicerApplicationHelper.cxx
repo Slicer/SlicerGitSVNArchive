Slicer/SlicerGitSVNArchive

commit 49df340fc046d0479be8ada53523d92643cbac6d

ENH: Add importFromDICOMWeb method to DICOMUtils
importFromDICOMWeb downloads data sets from a DICOMweb server and imports them into Slicer's DICOM database.

registerSlicerURLHandler helper function associates slicer:// URLs with current Slicer executable (will be used in a future to allow direct data download from Kheops, OHIF viewer, etc.).

Co-authored-by: Kyle Sunderland <sunderlandkyl@gmail.com>
Co-authored-by: Andras Lasso <lasso@queensu.ca>

git-svn-id: http://svn.slicer.org/Slicer4/trunk@28819 3bd1e089-480b-0410-8dfb-8563597acbee

Base/QTApp/qSlicerApplicationHelper.cxx
@@ -279,20 +279,7 @@ bool qSlicerApplicationHelper::checkRenderingCapabilities()
 
     // Run tscon system tool to create a new session, which terminates
     // the existing session (closes remote desktop connection).
-    SHELLEXECUTEINFO shExecInfo;
-    shExecInfo.cbSize = sizeof(SHELLEXECUTEINFO);
-    shExecInfo.fMask = SEE_MASK_NOCLOSEPROCESS;
-    shExecInfo.hwnd = nullptr;
-    // tscon requires administrator access, therefore "runas" verb is needed.
-    // UAC popup will be displayed.
-    shExecInfo.lpVerb = "runas";
-    shExecInfo.lpFile = "tscon.exe";
-    shExecInfo.lpParameters = "1 /dest:console";
-    shExecInfo.lpDirectory = nullptr;
-    shExecInfo.nShow = SW_MAXIMIZE;
-    shExecInfo.hInstApp = nullptr;
-    ShellExecuteEx(&shExecInfo);
-    WaitForSingleObject(shExecInfo.hProcess, INFINITE);
+    qSlicerApplicationHelper::runAsAdmin("tscon.exe", "1 /dest:console");
 
     QApplication::processEvents();
 
@@ -303,4 +290,42 @@ bool qSlicerApplicationHelper::checkRenderingCapabilities()
 #endif
 
   return (result == QMessageBox::Ignore);
+}
+
+//----------------------------------------------------------------------------
+int qSlicerApplicationHelper::runAsAdmin(QString executable, QString parameters/*=QString()*/, QString workingDir/*=QString()*/)
+{
+#if defined(_WIN32)
+  // Run tscon system tool to create a new session, which terminates
+  // the existing session (closes remote desktop connection).
+  SHELLEXECUTEINFO shExecInfo;
+  shExecInfo.cbSize = sizeof(SHELLEXECUTEINFO);
+  shExecInfo.fMask = SEE_MASK_NOCLOSEPROCESS;
+  shExecInfo.hwnd = nullptr;
+  // tscon requires administrator access, therefore "runas" verb is needed.
+  // UAC popup will be displayed.
+  shExecInfo.lpVerb = "runas";
+  shExecInfo.lpFile = executable.toUtf8().constData();
+  shExecInfo.lpParameters = nullptr;
+  if (!parameters.isEmpty())
+    {
+    shExecInfo.lpParameters = parameters.toUtf8().constData();
+    }
+  shExecInfo.lpDirectory = nullptr;
+  if (!workingDir.isEmpty())
+    {
+    shExecInfo.lpDirectory = workingDir.toUtf8().constData();
+    }
+  shExecInfo.nShow = SW_MAXIMIZE;
+  shExecInfo.hInstApp = nullptr;
+  ShellExecuteEx(&shExecInfo);
+  WaitForSingleObject(shExecInfo.hProcess, INFINITE);
+  DWORD exitCode = 0;
+  GetExitCodeProcess(shExecInfo.hProcess, &exitCode);
+  CloseHandle(shExecInfo.hProcess);
+  return exitCode;
+#else
+  qFatal() << Q_FUNC_INFO << ": not implemented for Linux and MacOSX."
+  return -1;
+#endif
 }

Base/QTApp/qSlicerApplicationHelper.h
@@ -65,6 +65,11 @@ public:
   /// See more information at https://issues.slicer.org/view.php?id=4252
   Q_INVOKABLE static bool checkRenderingCapabilities();
 
+  /// Run command as administrator (UAC on Windows, sudo on linux)
+  /// Currently only works on Windows.
+  /// Returns error code (0=success).
+  Q_INVOKABLE static int runAsAdmin(QString executable, QString parameters = QString(), QString workingDir = QString());
+
 private:
   Q_DISABLE_COPY(qSlicerApplicationHelper);
 };

Modules/Scripted/DICOMLib/DICOMUtils.py
@@ -678,3 +678,85 @@ def loadLoadables(loadablesByPlugin, messages=None, progressCallback=None):
   slicer.mrmlScene.RemoveObserver(sceneObserverTag)
 
   return loadedNodeIDs
+
+def importFromDICOMWeb(dicomWebEndpoint, studyInstanceUID, seriesInstanceUID=None, accessToken=None):
+  """
+  Downloads and imports DICOM series from a DICOMweb instance.
+  Example usage:
+    from DICOMLib import DICOMUtils
+
+    loadedUIDs = DICOMUtils.importFromDICOMWeb(dicomWebEndpoint="https://yourdicomweburl/dicomWebEndpoint",
+                                             studyInstanceUID="2.16.840.1.113669.632.20.1211.10000509338")
+                                             accessToken="YOUR_ACCESS_TOKEN")
+  :param dicomWebEndpoint: Endpoint URL for retrieving the study/series from DICOMweb
+  :param studyInstanceUID: UID for the study to be downloaded
+  :param seriesInstanceUID: UID for the series to be downloaded. If not specified, all series will be downloaded from the study
+  :param accessToken: Optional access token for the query
+  :return: List of imported study UIDs
+  """
+
+  from dicomweb_client.api import DICOMwebClient
+  import random
+
+  if accessToken is None:
+    client = DICOMwebClient(url = dicomWebEndpoint)
+  else:
+    client = DICOMwebClient(
+              url = dicomWebEndpoint,
+              headers = { "Authorization": "Bearer {}".format(accessToken) },
+              )
+
+  seriesList = client.search_for_series(study_instance_uid=studyInstanceUID)
+  seriesInstanceUIDs = []
+  if not seriesInstanceUID is None:
+    seriesInstanceUIDs = [seriesInstanceUID]
+  else:
+    for series in seriesList:
+      currentSeriesInstanceUID = series['0020000E']['Value'][0]
+      seriesInstanceUIDs.append(currentSeriesInstanceUID)
+
+  fileNumber = 0
+  for currentSeriesInstanceUID in seriesInstanceUIDs:
+    instances = client.retrieve_series(
+      study_instance_uid=studyInstanceUID,
+      series_instance_uid=currentSeriesInstanceUID)
+
+    outputDirectoryBase = slicer.dicomDatabase.databaseDirectory + "/DICOMweb/" + qt.QDateTime.currentDateTime().toString("yyyyMMdd-hhmmss")
+    outputDirectory = qt.QTemporaryDir(outputDirectoryBase) # Add unique substring to directory
+    outputDirectory.setAutoRemove(False)
+    outputDirectoryPath = outputDirectory.path()
+    if not os.access(outputDirectoryPath, os.F_OK):
+      os.makedirs(outputDirectoryPath)
+
+    for instance in instances:
+      filename = outputDirectoryPath + "/" + str(fileNumber) + ".dcm"
+      instance.save_as(filename)
+      fileNumber += 1
+    importDicom(outputDirectoryPath)
+
+  return seriesInstanceUIDs
+
+def registerSlicerURLHandler():
+  """
+  Registers slicer:// protocol with this executable.
+  For now, only implemented on Windows.
+  """
+  if os.name == 'nt':
+    slicerLauncherPath = os.path.abspath(slicer.app.launcherExecutableFilePath)
+    urlHandlerRegFile = r"""Windows Registry Editor Version 5.00
+[HKEY_CLASSES_ROOT\Slicer]
+@="URL:Slicer Slicer Protocol"
+"URL Protocol"=""
+[HKEY_CLASSES_ROOT\Slicer\DefaultIcon]
+@="Slicer.exe,1"
+[HKEY_CLASSES_ROOT\Slicer\shell]
+[HKEY_CLASSES_ROOT\Slicer\shell\open]
+[HKEY_CLASSES_ROOT\Slicer\shell\open\command]
+@="\"{0}\" \"%1\""
+""".format(slicerLauncherPath.replace("\\","\\\\"))
+    urlHandlerRegFilePath = slicer.app.temporaryPath+"registerSlicerUrlHandler.reg"
+    with open(urlHandlerRegFilePath, "wt") as f:
+      f.write(urlHandlerRegFile)
+    slicer.qSlicerApplicationHelper().runAsAdmin("Regedt32.exe", "/s "+urlHandlerRegFilePath)
+  else:
+    raise NotImplementedError()

commit 3c2af402891d861b33c93ca262d0ea6c81ed1329

COMP: Fix unix build error related to qSlicerApplicationHelper::runAsAdmin
This commit fixes a regression introduced in r28819 (ENH: Add
importFromDICOMWeb method to DICOMUtils)

From: Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>

git-svn-id: http://svn.slicer.org/Slicer4/trunk@28823 3bd1e089-480b-0410-8dfb-8563597acbee

Base/QTApp/qSlicerApplicationHelper.cxx
@@ -325,7 +325,10 @@ int qSlicerApplicationHelper::runAsAdmin(QString executable, QString parameters/
   CloseHandle(shExecInfo.hProcess);
   return exitCode;
 #else
-  qFatal() << Q_FUNC_INFO << ": not implemented for Linux and MacOSX."
+  Q_UNUSED(executable);
+  Q_UNUSED(parameters);
+  Q_UNUSED(workingDir);
+  qFatal("%s: not implemented for Linux and macOS.", Q_FUNC_INFO);
   return -1;
 #endif
 }